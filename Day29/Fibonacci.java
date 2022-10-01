f81686c6c503ceb4d51507b7b78b6a2ac863d801
geekqq <jinwei1014@gmail.com>
2022-09-30 19:41:53 -0700

    Day29

--- Day29/Fibonacci.java
+++ Day29/Fibonacci.java
@@ -1,2 +1,26 @@
-package Day29;public class Fibonacci {
+package Day29;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(5));
+        System.out.println(fibonacciOne(5));
+    }
+
+    public static int fibonacci(int n) {
+        int f0 = 0;
+        int f1 = 1;
+        int next = 0;
+
+        for (int i = 3; i <= n ; i++) {
+            next = f0 + f1;
+            f0 = f1;
+            f1 = next;
+        }
+        return next;
+    }
+
+    public static int fibonacciOne(int n) {
+        if (n < 2) return n;
+        return fibonacci(n - 1) + fibonacciOne(n - 2);
+    }
 }
---
+++ Day29/GuessingNumber.java
@@ -0,0 +1,29 @@
+package Day29;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingNumber {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int target = rand.nextInt(101);
+        int guess;
+        int numberOfTries = 0;
+        boolean win = false;
+
+        System.out.println("猜数字游戏，请在0-100猜一个数字：");
+        while (win == false) {
+            guess = new Scanner(System.in).nextInt();
+            if (guess == target) {
+                win = true;
+                break;
+            } else if (guess < target) {
+                System.out.println("太小了：");
+            } else {
+                System.out.println("太大了：");
+            }
+            numberOfTries++;
+        }
+        System.out.println("猜对了。你一共猜了" + numberOfTries + "次。你好厉害哦！");
+    }
+}
