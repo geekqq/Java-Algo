9c3c0608a93bd1e0de615846367dfb46b855203c
geekqq <jinwei1014@gmail.com>
2022-10-02 22:29:54 -0700

    new code

---
+++ Day31/LeastCommonMultiple.java
@@ -0,0 +1,26 @@
+package Day31;
+
+public class LeastCommonMultiple {
+    public static void main(String[] args) {
+        System.out.println(lcm(5, 10));
+        System.out.println(zxgbs(6,8));
+    }
+
+    public static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return a % b == 0 ? b : gcd(b, a % b);
+    }
+
+    public static int zxgbs(int a, int b) {
+        int max = Math.max(a, b);
+        for (int i = max; ; i += max) {
+            if (i % a == 0 && i % b == 0) {
+                return i;
+            }
+        }
+    }
+}
