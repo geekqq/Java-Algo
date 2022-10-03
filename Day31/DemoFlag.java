b100ad9cac558312f7147c3490c2cce9c5e5ec10
geekqq <jinwei1014@gmail.com>
2022-10-02 22:02:26 -0700

    flag

---
+++ Day31/DemoFlag.java
@@ -0,0 +1,9 @@
+package Day31;
+
+public class DemoFlag {
+    public static void main(String[] args) {
+        boolean flag = false;
+        System.out.println(!flag);
+        System.out.println(flag);
+    }
+}
