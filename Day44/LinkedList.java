5c80c04677aafe28c2e963f20395f70a86e55eee
geekqq <jinwei1014@gmail.com>
2022-10-15 08:19:57 -0700

    mergeSort

---
+++ Day44/LinkedList.java
@@ -0,0 +1,48 @@
+package Day44;
+
+public class LinkedList {
+    ListNode head;
+
+    static class ListNode {
+        int value;
+        ListNode next;
+
+        ListNode(int value) {
+            this.value = value;
+            next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(2);
+        LinkedList ob = new LinkedList();
+        ob.printList(head);
+    }
+
+    public void printList(ListNode head) {
+        if (head == null) {
+            System.out.println("the list is empty");
+        }
+        ListNode cur = head;
+        while (cur != null) {
+            System.out.print(head.value + " ");
+            cur = cur.next;
+        }
+    }
+
+    public void addNode(int value) {
+        ListNode newNode = new ListNode(value);
+        if (head == null) {
+            head = newNode;
+            newNode.next = null;
+        } else {
+            ListNode cur = head;
+            while (cur != null) {
+                cur = cur.next;
+            }
+            cur = newNode;
+            cur.next = null;
+        }
+    }
+}
