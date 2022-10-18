ba55162920bb73d36c0223533c2ca12b62d7a657
geekqq <jinwei1014@gmail.com>
2022-10-17 23:03:18 -0700

    add new code

--- Day46/ReverseArray.java
+++ Day46/ReverseArray.java
@@ -4,14 +4,18 @@ import java.util.Arrays;
 
 public class ReverseArray {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,3,23,4,};
+        int[] nums = {1,2,3,3,23,4,};
         System.out.println(Arrays.toString(reverseArray(nums)));
     }
     public static int[] reverseArray(int[] nums) {
-        int n = nums.length - 1;
+        if (nums == null || nums.length <= 1) {
+            return nums;
+        }
+
         int[] newArr = new int[nums.length];
+        int n = nums.length - 1;
         for (int i = 0; i < nums.length; i++) {
-            nums[n--] = nums[i];
+            newArr[n--] = nums[i];
         }
         return newArr;
     }
