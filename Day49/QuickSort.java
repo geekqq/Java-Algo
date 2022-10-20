dc5cfb326475231b4f12ba05ea4dddad066d57d8
geekqq <jinwei1014@gmail.com>
2022-10-20 10:54:07 -0700

    add new quickSort

---
+++ Day49/QuickSort.java
@@ -0,0 +1,47 @@
+package Day49;
+
+public class QuickSort {
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        //define the last element as pivot
+        int pivot= arr[high];
+
+        //initialize the smaller element index -1, it does not exit yet
+        int i = (low - 1);
+
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,0,3,4,3,23};
+        quickSort(nums, 0, nums.length - 1);
+        printArray(nums);
+    }
+
+}
