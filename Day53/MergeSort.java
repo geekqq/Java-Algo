225c485d30f06a2120f8bcaa3aac907cbafd4fd3
geekqq <jinwei1014@gmail.com>
2022-10-24 22:38:21 -0700

    Day 53

---
+++ Day53/MergeSort.java
@@ -0,0 +1,54 @@
+package Day53;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void merge(int[] nums, int l, int m, int r) {
+        int leftLen = m - l + 1;
+        int rightLen = r - m;
+
+        int[] leftArr = new int[leftLen];
+        int[] rightArr = new int[rightLen];
+
+        for (int i = 0; i < leftLen; i++) {
+            leftArr[i] = nums[l + i];
+        }
+        for (int i = 0; i < rightLen; i++) {
+            rightArr[i] = nums[m + 1 + i];
+        }
+
+        int i = 0;
+        int j = 0;
+        int k = l;
+
+        while (i < leftLen && j < rightLen) {
+            if (leftArr[i] < rightArr[j]) {
+                nums[k++] = leftArr[i++];
+            } else {
+                nums[k++] = rightArr[j++];
+            }
+        }
+        while (i < leftLen) {
+            nums[k++] = leftArr[i++];
+        }
+        while (j < rightLen) {
+            nums[k++] = rightArr[j++];
+        }
+
+    }
+
+    public static void sort(int[] nums, int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            sort(nums, l, m);
+            sort(nums, m + 1, r);
+            merge(nums, l, m , r);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,0,3,6,3,5,1,-3,4,7};
+        sort(nums, 0, nums.length - 1);
+        System.out.println(Arrays.toString(nums));
+    }
+}
