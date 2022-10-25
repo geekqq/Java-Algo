be8b33858bc83c6c8eb6fe156cc06505e6f562ef
geekqq <jinwei1014@gmail.com>
2022-10-24 23:20:34 -0700

    add new code

--- Day53/ReverseLinkedList.java
+++ Day53/ReverseLinkedList.java
@@ -1,18 +1,19 @@
-package Day52;
+package Day53;
 
 public class ReverseLinkedList {
     static ListNode head;
+
     static class ListNode {
         int val;
         ListNode next;
 
-        public ListNode(int val) {
+        ListNode(int val) {
             this.val = val;
             next = null;
         }
     }
 
-    public ListNode reverse(ListNode head) {
+    public static ListNode reverse(ListNode head) {
         ListNode prev = null;
         ListNode cur = head;
         ListNode next = null;
@@ -27,7 +28,7 @@ public class ReverseLinkedList {
         return head;
     }
 
-    public void printList(ListNode head) {
+    public static void printList(ListNode head) {
         ListNode cur = head;
         while (cur != null) {
             System.out.print(cur.val + " ");
@@ -39,12 +40,13 @@ public class ReverseLinkedList {
     public static void main(String[] args) {
         ReverseLinkedList list = new ReverseLinkedList();
         list.head = new ListNode(2);
-        list.head.next = new ListNode(4);
-        list.head.next.next = new ListNode(6);
-        list.head.next.next.next = new ListNode(8);
-        list.head.next.next.next.next = new ListNode(10);
+        list.head.next = new ListNode(3);
+        list.head.next.next = new ListNode(5);
+        list.head.next.next.next = new ListNode(7);
         list.printList(head);
         head = list.reverse(head);
         list.printList(head);
     }
+
 }
+
