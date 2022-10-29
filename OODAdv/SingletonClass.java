f0030e150e09e8ddfe78a107096e673ceaee9bfe
geekqq <jinwei1014@gmail.com>
2022-10-29 10:09:24 -0700

    singletonClass

--- OODAdv/SingletonClass.java
+++ OODAdv/SingletonClass.java
@@ -2,7 +2,7 @@ package OODAdv;
 
 //设计一个class，该class只能创建一个instance。最常用的场景是创建过程的cost比较大的时候比如cache在load时候数据可能会非常大
 public class SingletonClass {
-    private static SingletonClass instance;
+    private static SingletonClass instance = null;
     private SingletonClass() {
     }
 
