004470cf21a287dd1d5c6dea5e3bac6d14fc266b
geekqq <jinwei1014@gmail.com>
2022-10-29 08:03:47 -0700

    getInstance()

---
+++ OODAdv/SingletonClass.java
@@ -0,0 +1,15 @@
+package OODAdv;
+
+//设计一个class，该class只能创建一个instance。最常用的场景是创建过程的cost比较大的时候比如cache在load时候数据可能会非常大
+public class SingletonClass {
+    private static SingletonClass instance;
+    private SingletonClass() {
+    }
+
+    public static SingletonClass getInstance() {
+        if (instance == null) {
+            instance = new SingletonClass();
+        }
+        return instance;
+    }
+}
