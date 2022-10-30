7a9724b56dfd1cd51dbe44a2025ad2f60fae765d
geekqq <jinwei1014@gmail.com>
2022-10-29 20:03:01 -0700

    new code for Saturday

---
+++ Day58/PrimeNumber.java
@@ -0,0 +1,24 @@
+package Day58;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static Boolean isPrime(int n) {
+        if (n <= 3) return n > 1;
+        if (n % 6 != 1 && n % 6 != 5) return false;
+        for (int i = 5; i <= Math.sqrt(n) ; i += 6) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
+
