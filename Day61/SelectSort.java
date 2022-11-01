bd0868c5bcb00e0b212e0dfcfc53f63e61c87efd
geekqq <jinwei1014@gmail.com>
2022-11-01 15:23:38 -0700

    selectSort

---
+++ Day61/SelectSort.java
@@ -0,0 +1,29 @@
+package Day61;
+
+import java.util.Arrays;
+
+public class SelectSort {
+    public static void main(String[] args) {
+        int[] nums = {23, 12, 1, 8, -3, 0, 90};
+        selectSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void selectSort(int[] nums) {
+        int minIndex;
+        for (int i = 0; i < nums.length - 1; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[minIndex] > nums[j]) {
+                    minIndex = j;
+                }
+            }
+            swap(nums, minIndex, i);
+        }
+    }
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
