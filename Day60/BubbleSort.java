503f324539d57cd969d41502435dd5d73244be90
geekqq <jinwei1014@gmail.com>
2022-10-31 22:28:06 -0700

    recursion version of bubble sort

--- Day60/BubbleSort.java
+++ Day60/BubbleSort.java
@@ -7,6 +7,10 @@ public class BubbleSort {
         int[] nums = {64, 34,25,12,22,11,90};
         bubbleSort(nums);
         System.out.println(Arrays.toString(nums));
+        System.out.println("----------------------");
+        int[] arr = {23, 12, 1, 8, -3, 0, 90};
+        bubbleSort2(arr, arr.length);
+        System.out.println(Arrays.toString(arr));
     }
 
     public static void bubbleSort(int[] nums) {
@@ -20,5 +24,24 @@ public class BubbleSort {
                 }
             }
         }
+    }
+    //recursion version of bubble sort
+    public static void bubbleSort2(int[] nums, int n) {
+        if (n == 0 || n == 1) {
+            return;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                swap(nums, i, i + 1);
+            }
+        }
+        bubbleSort2(nums, n - 1);
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
