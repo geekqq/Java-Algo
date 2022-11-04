89388c432064d5e687698f7550c99455b2cc68d9
geekqq <jinwei1014@gmail.com>
2022-11-04 12:43:31 -0700

    Day 64

--- Day64/InsertionSort.java
+++ Day64/InsertionSort.java
@@ -1,18 +1,16 @@
-package Day63;
+package Day64;
 
 import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String[] args) {
         int[] nums = {23, 12, -3, 1, 8, -3, 0, 90, 90};
-        sort(nums);
+        selectSort(nums);
         System.out.println(Arrays.toString(nums));
-
     }
 
-    public static void sort(int[] nums) {
-        int n = nums.length;
-        for (int i = 1; i < n; i++) {
+    public static void selectSort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
             int key = nums[i];
             int j = i - 1;
 
---
+++ Day64/SelectSort.java
@@ -0,0 +1,30 @@
+package Day64;
+
+import java.util.Arrays;
+
+public class SelectSort {
+    public static void main(String[] args) {
+        int[] nums = {23, 12, -3, 1, 8, -3, 0, 90, 90};
+        selectSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void selectSort(int[] nums) {
+        int minIndex;
+        for (int i = 0; i < nums.length - 1; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[minIndex] > nums[j]) {
+                    minIndex = j;
+                }
+            }
+            swap(nums, i, minIndex);
+        }
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
