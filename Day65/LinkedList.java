fc620674781f94427c347708ba9eb097d4dba1b6
geekqq <jinwei1014@gmail.com>
2022-11-05 18:54:25 -0700

    new code linked list

---
+++ Day65/LinkedList.java
@@ -0,0 +1,76 @@
+package Day65;
+
+public class LinkedList {
+    private static ListNode head;
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+            next = null;
+        }
+    }
+
+    public static void add(int val) {
+        ListNode node = new ListNode(val);
+        if (head == null) head = node;
+        ListNode cur = head;
+        while (cur.next != null) {
+            cur = cur.next;
+        }
+        cur.next = node;
+    }
+
+    public static void print(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode prev = null;
+        ListNode cur = head;
+        ListNode next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        head = prev;
+        return head;
+    }
+
+    public static ListNode reverseRecursion(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode rest = reverseRecursion(head.next);
+        head.next.next = head;
+        head.next = null;
+        return rest;
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.head = new ListNode(21);
+        list.add(-3);
+        list.add(9);
+        list.add(3);
+        list.add(3);
+        list.add(10);
+        list.print(head);
+        head = reverse(head);
+        list.print(head);
+        head = reverseRecursion(head);
+        list.print(head);
+    }
+
+}
