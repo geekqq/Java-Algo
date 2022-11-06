4f0256716259a5fa0568f0f9a7f2a51e0ad97a98
geekqq <jinwei1014@gmail.com>
2022-11-05 22:15:34 -0700

    new code MyQueue

---
+++ Day65/MyQueue.java
@@ -0,0 +1,49 @@
+package Day65;
+
+import java.util.Stack;
+
+public class MyQueue<E> {
+    //using stacks to implement queue
+    private Stack<E> stackIn;
+    private Stack<E> stackOut;
+
+    public MyQueue() {
+        stackIn = new Stack<E>();
+        stackOut = new Stack<E>();
+    }
+
+    public void offer(E val) {
+        while (!stackIn.isEmpty()) {
+            stackOut.push(stackIn.pop());
+        }
+        stackIn.push(val);
+        while (!stackOut.isEmpty()) {
+            stackIn.push(stackOut.pop());
+        }
+    }
+
+    public E poll() {
+        if (stackIn.isEmpty()) {
+            System.out.println("underflow!!");
+            System.exit(0);
+        }
+        return stackIn.pop();
+    }
+
+    public static void main(String[] args) {
+        MyQueue q = new MyQueue();
+        for (int i = 0; i < 10; i++) {
+            q.offer(i);
+        }
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+    }
+}
