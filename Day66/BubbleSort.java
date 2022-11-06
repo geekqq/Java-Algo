0a308ca5165137b23a81079f863a75e4f4a2ea66
geekqq <jinwei1014@gmail.com>
2022-11-06 08:44:27 -0800

    new code bubble sort

---
+++ Day66/BubbleSort.java
@@ -0,0 +1,28 @@
+package Day66;
+
+import java.util.Arrays;
+
+import static Day65.SelectSort.swap;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] nums = {23, 12, -3, 1, 8, -3, 0, 90, 90};
+        bubbleSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void bubbleSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            boolean swap = false;
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    swap(nums, j + 1, j);
+                    swap = true;
+                }
+            }
+            if (swap = false) {
+                break;
+            }
+        }
+    }
+}
