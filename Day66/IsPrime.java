3b6e08491d8304123baafa974ebe3ff78e6f3baa
geekqq <jinwei1014@gmail.com>
2022-11-06 20:52:48 -0800

    new code day66

--- Day66/IsPrime.java
+++ Day66/IsPrime.java
@@ -1,4 +1,4 @@
-package Day65;
+package Day66;
 
 public class IsPrime {
     public static void main(String[] args) {
@@ -7,13 +7,14 @@ public class IsPrime {
                 System.out.print(i + " ");
             }
         }
-        System.out.println();
     }
 
     public static boolean isPrime(int n) {
         if (n <= 3) return n > 1;
-        if (n % 6 != 5 && n %6 != 1) return false;
-        for (int i = 5; i <= Math.sqrt(n) ; i+=6) {
+        if (n % 6 != 5 && n % 6 != 1) {
+            return false;
+        }
+        for (int i = 5; i <= Math.sqrt(n) ; i += 6) {
             if (n % i == 0 || n % (i + 2) == 0) {
                 return false;
             }
