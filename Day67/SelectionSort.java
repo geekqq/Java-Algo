408c048919e9bb8a17b0dcf9a2587d0c362e6109
geekqq <jinwei1014@gmail.com>
2022-11-07 19:24:13 -0800

    new code

---
+++ Day67/SelectionSort.java
@@ -0,0 +1,26 @@
+package Day67;
+
+import java.util.Arrays;
+
+import static Day44.SelectSort.swap;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] nums = {23, 12, -3, 1, 8, -3, 0, 90, 90};
+        selectSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void selectSort(int[] nums) {
+        int minIndex;
+        for (int i = 0; i < nums.length - 1; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[minIndex] > nums[j]) {
+                    minIndex = j;
+                }
+            }
+            swap(nums, minIndex, i);
+        }
+    }
+}
