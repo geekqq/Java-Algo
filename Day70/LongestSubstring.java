c11c1cf447e0522b46e54de4ebed90f4b3f3f791
geekqq <jinwei1014@gmail.com>
2022-11-10 15:08:58 -0800

    new code longest substring

---
+++ Day70/LongestSubstring.java
@@ -0,0 +1,46 @@
+package Day70;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstring {
+    public static void main(String[] args) {
+        System.out.println(getLengthOfLongestSubstring("stonehoo"));
+        System.out.println(getLongestSubstring("stonehoo"));
+    }
+
+    public static int getLongestSubstring(String s) {
+        if (s == null || s.isEmpty()) return 0;
+        int left = 0;
+        int right = 0;
+        int maxLen = 0;
+        Set<Character> set = new HashSet<>();
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            if (set.add(c)) {
+                maxLen = Math.max(maxLen, set.size());
+                right++;
+            } else {
+                set.remove(s.charAt(left));
+                left++;
+            }
+        }
+        return maxLen;
+    }
+
+    public static int getLengthOfLongestSubstring(String s) {
+        if (s == null || s.isEmpty()) return 0;
+        int maxLen = 0;
+        String test = "";
+        char[] chars = s.toCharArray();
+        for(char c : chars) {
+            String cur = String.valueOf(c);
+            if (test.contains(cur)) {
+                test = test.substring(test.indexOf(cur) + 1);
+            }
+            test += cur;
+            maxLen = Math.max(maxLen, test.length());
+        }
+        return maxLen;
+    }
+}
