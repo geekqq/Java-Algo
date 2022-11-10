b581033701d546bbb88f6d02db558fbb2a9edc85
geekqq <jinwei1014@gmail.com>
2022-11-10 11:26:58 -0800

    SingletonClass

---
+++ Day70/SingletonClass.java
@@ -0,0 +1,19 @@
+package Day70;
+
+public class SingletonClass {
+
+    private static SingletonClass instance = null;
+
+    private SingletonClass() {};
+
+    public static SingletonClass getInstance() {
+        if (instance == null) {
+            synchronized (SingletonClass.class) {
+                if (instance == null) {
+                    instance = new SingletonClass();
+                }
+            }
+        }
+        return instance;
+    }
+}
