015be09edf409364fb7fba45a64f535044cb7106
geekqq <jinwei1014@gmail.com>
2022-11-12 21:10:59 -0800

    new code set zeros in matrix

---
+++ Day72/SetZerosMatrix.java
@@ -0,0 +1,51 @@
+package Day72;
+
+public class SetZerosMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {0,3,1,5},
+                {2,4,0,1},
+                {1,2,5,10},
+                {5,6,1,8},
+        };
+        setZeros(matrix);
+        print(matrix);
+    }
+
+    public static int[][] setZeros(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
+            return null;
+        }
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+        boolean[] rows = new boolean[n];
+        boolean[] cols = new boolean[m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (rows[i] || cols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        return matrix;
+    }
+
+    public static void print(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
