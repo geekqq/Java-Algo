b6a9234b7334619f88d9451f0d432b313c49182b
geekqq <jinwei1014@gmail.com>
2022-11-15 11:09:16 -0800

    new code reverse linked list

---
+++ Day75/ReverseLinkedList.java
@@ -0,0 +1,88 @@
+package Day75;
+
+public class ReverseLinkedList {
+
+    private ListNode head;
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+            next = null;
+        }
+    }
+
+    public void addLast(int val) {
+        ListNode newNode = new ListNode(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            ListNode cur = head;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+    }
+
+    public void print() {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode prev = null;
+        ListNode cur = head;
+        ListNode next = null;
+
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        head = prev;
+        return head;
+    }
+
+    public static ListNode reverseRecursion(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode rest = reverseRecursion(head.next);
+        head.next.next = head;
+        head.next = null;
+        return rest;
+    }
+
+    public static void main(String[] args) {
+        ReverseLinkedList list = new ReverseLinkedList();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        list.addLast(5);
+        list.addLast(6);
+        list.addLast(7);
+        list.addLast(8);
+        list.addLast(7);
+        list.addLast(7);
+        list.addLast(9);
+        list.print();
+        list.head = reverse(list.head);
+        list.print();
+        list.head = reverseRecursion(list.head);
+        list.print();
+    }
+}
