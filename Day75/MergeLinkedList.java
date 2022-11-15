8b5fa97179da7a8b0d0449ed6f34f80e55da7621
geekqq <jinwei1014@gmail.com>
2022-11-15 11:02:03 -0800

    merge linked list

---
+++ Day75/MergeLinkedList.java
@@ -0,0 +1,72 @@
+package Day75;
+
+public class MergeLinkedList {
+    private ListNode head;
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+            next = null;
+        }
+    }
+
+    public void addLast(int val) {
+        ListNode temp = new ListNode(val);
+        if (head == null) {
+            head = temp;
+        } else {
+            ListNode cur = head;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = temp;
+        }
+    }
+
+    public void print() {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static ListNode mergeList(ListNode h1, ListNode h2) {
+        if (h1 == null) {
+            return h2;
+        }
+        if (h2 == null) {
+            return h1;
+        }
+
+        if (h1.val < h2.val) {
+            h1.next = mergeList(h1.next, h2);
+            return h1;
+        } else {
+            h2.next = mergeList(h1, h2.next);
+            return h2;
+        }
+    }
+
+    public static void main(String[] args) {
+        MergeLinkedList list1 = new MergeLinkedList();
+        MergeLinkedList list2 = new MergeLinkedList();
+        MergeLinkedList list3 = new MergeLinkedList();
+        list1.addLast(0);
+        list1.addLast(1);
+        list1.addLast(2);
+        list1.addLast(3);
+        list1.addLast(4);
+        list2.addLast(5);
+        list2.addLast(6);
+        list2.addLast(7);
+        list2.addLast(8);
+        list2.addLast(9);
+        list3.head = mergeList(list1.head, list2.head);
+        list3.print();
+    }
+}
