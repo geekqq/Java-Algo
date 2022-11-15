5c0cff82a0d9fd0fb9624c2f0470462acb1b75a8
geekqq <jinwei1014@gmail.com>
2022-11-15 10:48:03 -0800

    new code day 75

---
+++ Day75/BubbleSort.java
@@ -0,0 +1,28 @@
+package Day75;
+
+import java.util.Arrays;
+
+import static Day44.SelectSort.swap;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] nums = {23, 12, -3, 1, 8, -3, 0, 90, 90};
+        bubbleSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void bubbleSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            boolean flag = true;
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    swap(nums, j + 1, j);
+                    flag = false;
+                }
+            }
+            if (flag) {
+                break;
+            }
+        }
+    }
+}
---
+++ Day75/InsertSort.java
@@ -0,0 +1,24 @@
+package Day75;
+
+import java.util.Arrays;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] nums = {23, 12, -3, 1, 8, -3, 0, 90, 90};
+        insertSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void insertSort(int[] nums) {
+        for (int i = 1; i < nums.length - 1; i++) {
+            int key = nums[i];
+            int j = i - 1;
+
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j + 1] = key;
+        }
+    }
+}
