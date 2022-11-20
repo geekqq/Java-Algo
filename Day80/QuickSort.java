5d0e21da00e21d5381f720235877679de2b9c578
geekqq <jinwei1014@gmail.com>
2022-11-20 10:50:26 -0800

    new code add twonumbers

---
+++ Day80/QuickSort.java
@@ -0,0 +1,34 @@
+package Day80;
+
+import java.util.Arrays;
+
+import static Day44.SelectSort.swap;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] nums = {30,10,20,40,50,10};
+        sort(nums, 0, nums.length - 1);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static int partition(int[] nums, int low, int high) {
+        int pivot = nums[high];
+        int i = low - 1;
+        for (int j = low; j < nums.length - 1; j++) {
+            if (nums[j] < pivot) {
+                i++;
+                swap(nums, i, j);
+            }
+        }
+        swap(nums, i + 1, high);
+        return i + 1;
+    }
+
+    public static void sort(int[] nums, int low, int high) {
+        if (low < high) {
+            int pi = partition(nums, low, high);
+            sort(nums, low, pi - 1);
+            sort(nums, pi + 1, high);
+        }
+    }
+}
