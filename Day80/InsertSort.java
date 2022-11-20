1c1a5f271bd17a9da3dcf59a68eb82c326e36d57
geekqq <jinwei1014@gmail.com>
2022-11-20 10:36:39 -0800

    day 80 new code

---
+++ Day80/InsertSort.java
@@ -0,0 +1,26 @@
+package Day80;
+
+import java.util.Arrays;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] nums = {30,10,20,40,50,10};
+        insertSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void insertSort(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            int key = nums[i];
+            int j = i - 1;
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j + 1] = key;
+        }
+    }
+}
