96c9c7faaf48ff8e7e59b796a13df19e5b090f74
geekqq <jinwei1014@gmail.com>
2022-11-19 22:13:01 -0800

    new code merge linked list

---
+++ Day79/BubbleSort.java
@@ -0,0 +1,28 @@
+package Day79;
+
+import java.util.Arrays;
+
+import static Day44.SelectSort.swap;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] nums = {23, 12, -3, 1, 90, 8, -3, 0, 90};
+        bubbleSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void bubbleSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            boolean flag = true;
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j + 1] < nums[j]) {
+                    swap(nums, j + 1, j);
+                    flag = false;
+                }
+            }
+            if (flag) {
+                break;
+            }
+        }
+    }
+}
