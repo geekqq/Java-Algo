49767e7bab9e147d7a3732b27fcd14dc60850bb6
geekqq <jinwei1014@gmail.com>
2022-11-22 14:58:34 -0800

    new code merge linkedlist

---
+++ Day82/GetArrayRank.java
@@ -0,0 +1,29 @@
+package Day82;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetArrayRank {
+    public static void main(String[] args) {
+        int[] nums = {30,10,20,40,50,10};
+        System.out.println(Arrays.toString(getRank(nums)));
+    }
+
+    public static int[] getRank(int[] nums) {
+        int[] newArr = Arrays.copyOfRange(nums, 0, nums.length);
+        Arrays.sort(newArr);
+        int rank = 1;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < newArr.length; i++) {
+            if (!map.containsKey(newArr[i])) {
+                map.put(newArr[i], rank);
+                rank++;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = map.get(nums[i]);
+        }
+        return nums;
+    }
+}
