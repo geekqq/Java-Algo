db819a311064627bd9679f79f646d3157f9ac266
geekqq <jinwei1014@gmail.com>
2022-11-27 14:40:37 -0800

    new code is even numbers & is fibonacci numbers

---
+++ Day87/IsEven.java
@@ -0,0 +1,15 @@
+package Day87;
+
+public class IsEven {
+    public static void main(String[] args) {
+        for (int i = 0; i < 25; i++) {
+            if (isEven(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isEven(int n) {
+        return (n & 1) == 0;
+    }
+}
