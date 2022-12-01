9590319d617f5410fbecee4167ee5462d7e12810
geekqq <jinwei1014@gmail.com>
2022-12-01 14:34:33 -0800

    new code day 91

---
+++ Day91/InsertSort.java
@@ -0,0 +1,28 @@
+package Day91;
+
+import java.util.Arrays;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] nums = {3,3,2,5,6,3,7,8,7};
+        insertSort(nums);
+        System.out.println(Arrays.toString(nums));
+        int[] nums2 = {3};
+        insertSort(nums2);
+        System.out.println(Arrays.toString(nums2));
+    }
+    public static void insertSort(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            int key = nums[i];
+            int j = i - 1;
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j + 1] = key;
+        }
+    }
+}
