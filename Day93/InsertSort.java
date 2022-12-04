a76777d46612cb9eb7ff8496aa8c3dc4f903648f
geekqq <jinwei1014@gmail.com>
2022-12-03 20:25:42 -0800

    new code

---
+++ Day93/InsertSort.java
@@ -0,0 +1,25 @@
+package Day93;
+
+import java.util.Arrays;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] nums = {3,3,2,5,6,3,7,8,7};
+        insertSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+    public static void insertSort(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            int key = nums[i];
+            int j = i - 1;
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j + 1] = key;
+        }
+    }
+}
---
+++ Day93/SelectSort.java
@@ -0,0 +1,28 @@
+package Day93;
+
+import java.util.Arrays;
+
+import static Day44.SelectSort.swap;
+
+public class SelectSort {
+    public static void main(String[] args) {
+        int[] nums = {3,3,2,5,6,3,7,8,7};
+        selectSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+    public static void selectSort(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int minIndex;
+        for (int i = 0; i < nums.length - 1; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[minIndex] > nums[j]) {
+                    minIndex = j;
+                }
+            }
+            swap(nums, i, minIndex);
+        }
+    }
+}
