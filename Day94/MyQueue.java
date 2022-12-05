5e584364d64f00b6adc8759552947cb3ae61afbb
geekqq <jinwei1014@gmail.com>
2022-12-04 19:13:25 -0800

    new code day 94

---
+++ Day94/MyQueue.java
@@ -0,0 +1,39 @@
+package Day94;
+
+import java.util.Stack;
+
+public class MyQueue<E> {
+    private Stack<E> stackIn;
+    private Stack<E> stackOut;
+
+    public MyQueue() {
+        stackIn = new Stack<E>();
+        stackOut = new Stack<E>();
+    }
+    public void offer(E val) {
+        while (!stackIn.isEmpty()) {
+            stackOut.push(stackIn.pop());
+        }
+        stackIn.push(val);
+        while (!stackOut.isEmpty()) {
+            stackIn.push(stackOut.pop());
+        }
+    }
+    public E poll() {
+        if (stackIn.isEmpty()) {
+            System.out.println("underflow!!");
+            System.exit(0);
+        }
+        return stackIn.pop();
+    }
+
+    public static void main(String[] args) {
+        MyQueue q = new MyQueue();
+        for (int i = 0; i < 11; i++) {
+            q.offer(i);
+        }
+        for (int i = 0; i < 15; i++) {
+            System.out.println(q.poll());
+        }
+    }
+}
