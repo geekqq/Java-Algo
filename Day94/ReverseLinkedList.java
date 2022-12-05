836cce1d457d1af802d4cf9526d2a1dbd755b3e0
geekqq <jinwei1014@gmail.com>
2022-12-04 19:21:13 -0800

    reverse linked list

---
+++ Day94/ReverseLinkedList.java
@@ -0,0 +1,77 @@
+package Day94;
+
+public class ReverseLinkedList {
+    private ListNode head;
+    class ListNode {
+        int val;
+        ListNode next;
+        public ListNode(int val) {
+            this.val = val;
+            next = null;
+        }
+    }
+    public void add(int val) {
+        ListNode newNode = new ListNode(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            ListNode temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+    public void print() {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public static ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode cur = head;
+        ListNode next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        head = prev;
+        return head;
+    }
+    public static ListNode reverse2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode rest = reverse2(head.next);
+        head.next.next = head;
+        head.next = null;
+        return rest;
+    }
+
+    public static void main(String[] args) {
+        ReverseLinkedList list = new ReverseLinkedList();
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(5);
+        list.add(3);
+        list.add(2);
+        list.add(1);
+        list.print();
+        list.head = reverse(list.head);
+        list.print();
+        list.head = reverse2(list.head);
+        list.print();
+    }
+}
