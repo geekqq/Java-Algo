cddd19a74a9b5d63346f1f4edf3c050df863f325
stone <geekqq>
2022-12-11 21:42:20 -0800

    new code

---
+++ Day101/GetArrayRank.java
@@ -0,0 +1,31 @@
+package Day101;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetArrayRank {
+    public static void main(String[] args) {
+        int[] nums = {20, 10, 20, 30, 10, 40, 50};
+        System.out.println(Arrays.toString(getArrayRank(nums)));
+    }
+    public static int[] getArrayRank(int[] nums) {
+        if (nums == null) {
+            return null;
+        }
+        int[] newArr = Arrays.copyOfRange(nums, 0, nums.length);
+        Arrays.sort(newArr);
+        int rank = 1;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < newArr.length; i++) {
+            if (!map.containsKey(newArr[i])) {
+                map.put(newArr[i], rank);
+                rank++;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = map.get(nums[i]);
+        }
+        return nums;
+    }
+}
