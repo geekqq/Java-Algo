6db779c5d65100a1765b998fe9eb8f3c2268a79c
stone <geekqq>
2022-12-19 10:18:14 -0800

    new code sorting

---
+++ Day109/PrintArrays.java
@@ -0,0 +1,10 @@
+package Day109;
+
+public class PrintArrays {
+    public static void print(int[] nums){
+        for (int i : nums) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ Day109/SelectSort.java
@@ -0,0 +1,28 @@
+package Day109;
+
+import static Day37.MergeSortTwo.printArray;
+import static Day37.SortedArray.swap;
+
+public class SelectSort {
+    public static void main(String[] args) {
+        int[] nums = {1,4,0,2,7,3,2,5,0,9};
+        selectSort(nums);
+        printArray(nums);
+    }
+
+    public static void selectSort(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int minIndex;
+        for (int i = 0; i < nums.length - 1; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[minIndex] > nums[j]) {
+                    minIndex = j;
+                }
+            }
+            swap(nums, i, minIndex);
+        }
+    }
+}
