63825381a98bd08e0ec6b479ed7d823bfdce1be1
stone <geekqq>
2022-12-18 20:25:24 -0800

    day 107

---
+++ Day107/SetZeroesMatrix.java
@@ -0,0 +1,47 @@
+package Day107;
+
+public class SetZeroesMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {0,3,1,5},
+                {2,4,0,1},
+                {1,2,5,10},
+                {5,6,1,8}
+        };
+        setZeroes(matrix);
+        print(matrix);
+    }
+    public static int[][] setZeroes(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
+            return null;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] rows = new boolean[m];
+        boolean[] cols = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rows[i] || cols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        return matrix;
+    }
+    public static void print(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Day107/SingletonClass.java
@@ -0,0 +1,17 @@
+package Day107;
+
+public class SingletonClass {
+    private static SingletonClass instance = null;
+    private SingletonClass() {
+    }
+    public static SingletonClass getInstance() {
+        if (instance == null) {
+            synchronized (SingletonClass.class) {
+                if (instance == null) {
+                    instance = new SingletonClass();
+                }
+            }
+        }
+        return instance;
+    }
+}
