3addf8093c6196f63b02405fa065b8784212deaa
stone <geekqq>
2022-12-20 11:06:21 -0800

    new code

---
+++ Day110/InsertSort.java
@@ -0,0 +1,25 @@
+package Day110;
+
+import static Day104.CountSort.printArray;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] nums = {1,4,0,2,7,3,2,5,0,9};
+        insertSort(nums);
+        printArray(nums);
+    }
+    public static void insertSort(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            int key = nums[i];
+            int j = i - 1;
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j + 1] = key;
+        }
+    }
+}
