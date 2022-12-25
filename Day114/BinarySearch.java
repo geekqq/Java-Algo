e379fec6eefccf87648fc6261f626b0df6a575c2
stone <geekqq>
2022-12-24 19:50:34 -0800

    new code binary search

---
+++ Day114/BinarySearch.java
@@ -0,0 +1,28 @@
+package Day114;
+
+public class BinarySearch {
+    public static int binarySearch(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = null;
+        int target = 0;
+        System.out.println(binarySearch(nums, target));
+    }
+}
