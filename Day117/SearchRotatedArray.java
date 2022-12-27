2777bb3b687b925a0a9b49fc7d34e8b0262f97ef
stone <geekqq>
2022-12-27 13:15:02 -0800

    new code day 107 search rotated sorted array

---
+++ Day117/SearchRotatedArray.java
@@ -0,0 +1,49 @@
+package Day117;
+
+public class SearchRotatedArray {
+    //find the min index in the rotated sorted array
+    public static int findMinIndex(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    public static int searchRotatedArray(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = findMinIndex(nums);
+        int left = 0;
+        int right = nums.length - 1;
+        if (target >= nums[start] && target <= nums[right]) {
+            left = start;
+        } else {
+            right = start - 1;
+        }
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,8,9,10,0,1,2,2,2,3};
+        System.out.println(findMinIndex(nums));
+        System.out.println(searchRotatedArray(nums, 10));
+    }
+}
