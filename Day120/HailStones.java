0bae9290cacd6b7bd6557d99aa62c3d7dcead523
geekqq <jinwei1014@gmail.com>
2022-12-30 23:18:29 -0800

    hailstone

---
+++ Day120/HailStones.java
@@ -0,0 +1,20 @@
+package Day120;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HailStones {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        int n = 15;
+        while (n != 1) {
+            list.add(n);
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = 3*n + 1;
+            }
+        }
+        System.out.println(list);
+    }
+}
