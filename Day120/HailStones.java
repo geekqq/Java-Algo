ebb709d6221ccf4a1ce70b58370f8fdc93cc4447
geekqq <jinwei1014@gmail.com>
2022-12-31 00:50:28 -0800

    new

--- Day120/HailStones.java
+++ Day120/HailStones.java
@@ -15,6 +15,20 @@ public class HailStones {
                 n = 3*n + 1;
             }
         }
+        list.add(n);
         System.out.println(list);
+    }
+    public static List<Integer> hailStone(int n) {
+        List<Integer> list = new ArrayList<>();
+        while (n != 1) {
+            list.add(n);
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = n * 3 + 1;
+            }
+        }
+        list.add(n);
+        return list;
     }
 }
