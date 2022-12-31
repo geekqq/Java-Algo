0182a66f01c6fc8cb5bffeb303a4c64de8c3d05f
geekqq <jinwei1014@gmail.com>
2022-12-30 22:51:35 -0800

    new code

---
+++ Day120/UseArgement.java
@@ -0,0 +1,7 @@
+public class UseArgement {
+    public static void main(String[] args) {
+	System.out.print("Hi, ");
+	System.out.print(args[0]);
+        System.out.print(". How are you?");
+    }
+}
