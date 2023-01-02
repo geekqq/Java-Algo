ddd245dcd876441f88393776656d75d4524b169c
geekqq <jinwei1014@gmail.com>
2023-01-02 13:27:19 -0800

    new code day 122

---
+++ Day122/MyPow.java
@@ -0,0 +1,25 @@
+package Day122;
+
+public class MyPow {
+    public static void main(String[] args) {
+        System.out.println(myPow(5,0));
+        System.out.println(myPow2(5,2));
+    }
+    public static int myPow(int n, int x) {
+        if (x == 0) {
+            return 1;
+        }
+        int res = 1;
+        for (int i = 0; i < x; i++) {
+            res *= n;
+        }
+        return res;
+    }
+    public static int myPow2(int n, int x) {
+        if (x == 0) {
+            return 1;
+        }
+        int half = myPow2(n, x / 2);
+        return (x & 1) == 0 ? half * half : half * half * n;
+    }
+}
---
+++ Day122/SelectSort.java
@@ -0,0 +1,25 @@
+package Day122;
+
+import java.util.Arrays;
+
+import static Day44.SelectSort.swap;
+
+public class SelectSort {
+    public static void main(String[] args) {
+        int[] nums = {1,4,2,7,2,0,9};
+        selectSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+    public static void selectSort(int[] nums) {
+        int minIndex;
+        for (int i = 0; i < nums.length - 1; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[minIndex] > nums[j]) {
+                    minIndex = j;
+                }
+            }
+            swap(nums, i, minIndex);
+        }
+    }
+}
