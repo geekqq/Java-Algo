23baa82f0e95d7e755b40a8fd64e2906c559e8f7
geekqq <jinwei1014@gmail.com>
2023-01-05 19:45:29 -0800

    new code day 126

---
+++ Day126/FindKClosestElements.java
@@ -0,0 +1,69 @@
+package Day126;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindKClosestElements {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,3,3,4,5,5,6};
+        int k = 5;
+        int x = 4;
+        System.out.println(findKClosestElements(nums, k, x));
+    }
+
+    public static List<Integer> findKClosestElements(int[] nums, int k, int x) {
+        List<Integer> res = new ArrayList<>();
+        if(nums == null || nums.length == 0 || k <= 0) {
+            return res;
+        }
+        k = Math.min(nums.length, k);
+        int pos = findClosestIndex(nums, x);
+        res.add(nums[pos]);
+        addRemaining(res, nums, pos, k - 1, x);
+        return res;
+    }
+    public static int findClosestIndex(int[] nums, int x) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == x) {
+                return mid;
+            } else if (nums[mid] > x) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        return Math.abs(nums[left] - x) <= Math.abs(nums[right] - x) ? left : right;
+    }
+
+    public static void addRemaining(List<Integer> res, int[] nums, int pos, int count, int x) {
+        if (count == 0) {
+            return;
+        }
+        int left = pos - 1;
+        int right = pos + 1;
+        while (left <= right) {
+            if (count == 0) {
+                return;
+            }
+            int mid = left + (right - left) / 2;
+            if (Math.abs(nums[left] - x) <= Math.abs(nums[right] - x)) {
+                res.add(0, nums[left--]);
+            } else {
+                res.add(nums[right++]);
+            }
+            count--;
+        }
+        while (count > 0) {
+            if (left < 0) {
+                res.add(nums[right++]);
+            } else {
+                res.add(0, nums[left--]);
+            }
+            count--;
+        }
+    }
+
+}
