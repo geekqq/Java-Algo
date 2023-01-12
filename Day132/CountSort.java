2ea6f18553ee5ad3cd0f0e0dc216efdd42980f0e
geekqq <jinwei1014@gmail.com>
2023-01-11 21:23:59 -0800

    new code

---
+++ Day132/CountSort.java
@@ -0,0 +1,31 @@
+package Day132;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class CountSort {
+    public static void main(String[] args) {
+        int[] nums = {1,0,1,4,3,5,2,0};
+        System.out.println(Arrays.toString(countSort(nums)));
+    }
+    public static int[] countSort(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        int[] newArr = Arrays.copyOf(nums, nums.length);
+        int max = Arrays.stream(newArr).max().getAsInt();
+        int bucketLen = max + 1;
+        int[] bucket = new int[bucketLen];
+        for (int value : newArr) {
+            bucket[value]++;
+        }
+        int sortIndex = 0;
+        for (int i = 0; i < bucketLen; i++) {
+            while (bucket[i] > 0) {
+                newArr[sortIndex++] = i;
+                bucket[i]--;
+            }
+        }
+        return newArr;
+    }
+}
