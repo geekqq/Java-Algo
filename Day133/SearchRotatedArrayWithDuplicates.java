68c19c6f359c4b9f9372650356c0f8e8fbbab098
geekqq <jinwei1014@gmail.com>
2023-01-13 11:23:36 -0800

    new code day 134

--- Day133/SearchRotatedArrayWithDuplicates.java
+++ Day133/SearchRotatedArrayWithDuplicates.java
@@ -3,7 +3,10 @@ package Day133;
 public class SearchRotatedArrayWithDuplicates {
     public static void main(String[] args) {
         int[] nums = {4,4,5,5,6,6,7,1,2,3,3};
-        System.out.println(findTargetInRotatedArrayWithDuplicates(nums, 3));
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + " = " + findTargetInRotatedArrayWithDuplicates(nums, i));
+
+        }
     }
 
     public static boolean findTargetInRotatedArrayWithDuplicates(int[] nums, int target) {
@@ -17,10 +20,22 @@ public class SearchRotatedArrayWithDuplicates {
             if (nums[left] == target) return true;
             if (nums[right] == target) return true;
             if (nums[mid] == target) return true;
-            else if (nums[mid] < target) {
-                left = mid + 1;
+            if (nums[left] == nums[mid]) {
+                left++;
+            } else if (nums[right] == nums[mid]) {
+                right--;
+            } else if (nums[left] < nums[mid]) {
+                if (target > nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
             } else {
-                right = mid - 1;
+                if (target > nums[mid] && target < nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
         return false;
---
+++ Day134/RomanToInteger2.java
@@ -0,0 +1,35 @@
+package Day134;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger2 {
+    public static void main(String[] args) {
+        System.out.println(romanToInt("CMXCIV"));
+    }
+    private static int romanToInt(String s) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("M", 1000);
+        map.put("D", 500);
+        map.put("C", 100);
+        map.put("L", 50);
+        map.put("X", 10);
+        map.put("V", 5);
+        map.put("I", 1);
+
+        String lastSym = s.substring(s.length() - 1);
+        int lastVal = map.get(lastSym);
+        int sum = lastVal;
+        for (int i = s.length() - 2; i >= 0; i--) {
+            String curSym = s.substring(i, i + 1);
+            int curVal = map.get(curSym);
+            if (curVal < lastVal) {
+                sum -= curVal;
+            } else {
+                sum += curVal;
+            }
+            lastVal = curVal;
+        }
+        return sum;
+    }
+}
