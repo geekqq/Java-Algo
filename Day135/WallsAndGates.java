0879fe0d5cb90d50254888af7bf43d7c116d621c
geekqq <jinwei1014@gmail.com>
2023-01-14 20:40:54 -0800

    walls and gates

---
+++ Day135/WallsAndGates.java
@@ -0,0 +1,40 @@
+package Day135;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class WallsAndGates {
+    public void wallsAndGates(int[][] rooms) {
+        if (rooms == null || rooms.length == 0) return;
+        int row = rooms.length;
+        int col = rooms[0].length;
+
+        int[][] directions = new int[][] {{0,1}, {0,-1}, {1,0}, {-1,0}};
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < rooms.length; i++) {
+            for (int j = 0; j < rooms[0].length; j++) {
+                if (rooms[i][j] == 0) {
+                    queue.offer(i * col + j);
+                }
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int p = queue.poll();
+            int pRowIndex = p / col;
+            int pColIndex = p % col;
+            for (int[] d : directions) {
+                int x = pRowIndex + d[0];
+                int y = pColIndex + d[1];
+                if (x < 0 || x >= rooms.length || y < 0 || y >= rooms[0].length
+                        || rooms[x][y] <= rooms[pColIndex][pRowIndex] + 1) {
+                    continue;
+                }
+                rooms[x][y] = rooms[pRowIndex][pColIndex] + 1;
+                queue.offer(x * col + y);
+            }
+        }
+    }
+}
