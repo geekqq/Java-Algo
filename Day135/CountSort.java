170205535c92a6ddd1f3e9e84a235610c10f0431
geekqq <jinwei1014@gmail.com>
2023-01-14 20:27:40 -0800

    singletonclass

---
+++ Day135/CountSort.java
@@ -0,0 +1,30 @@
+package Day135;
+
+import java.util.Arrays;
+
+public class CountSort {
+    public static void main(String[] args) {
+        int[] nums = {1,0,1,4,3,5,2,0};
+        System.out.println(Arrays.toString(countSort(nums)));
+    }
+
+    private static int[] countSort(int[] nums) {
+        if (nums == null || nums.length == 0) return null;
+        int[] newArr = Arrays.copyOf(nums, nums.length);
+        int max = Arrays.stream(newArr).max().getAsInt();
+        int bucketLen = max + 1;
+        int[] bucket = new int[bucketLen];
+
+        for (int value : newArr) {
+            bucket[value]++;
+        }
+        int sortedIndex = 0;
+        for (int i = 0; i < bucketLen; i++) {
+            while (bucket[i] > 0) {
+                newArr[sortedIndex++] = i;
+                bucket[i]--;
+            }
+        }
+        return newArr;
+    }
+}
