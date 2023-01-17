1c6c61e68191233d29f63b5afaa5d3cc96ff06b0
geekqq <jinwei1014@gmail.com>
2023-01-17 12:57:28 -0800

    find h index

---
+++ Day138/FindInsertPos.java
@@ -0,0 +1,25 @@
+package Day138;
+
+public class FindInsertPos {
+    public static void main(String[] args) {
+        int[] nums = {2,4,5,6,7,9};
+        System.out.println(findInsertPos(nums,8));
+    }
+
+    private static int findInsertPos(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
---
+++ Day138/FindMinValueInRotatedArray.java
@@ -0,0 +1,28 @@
+package Day138;
+
+import java.util.Arrays;
+
+import static Day138.RandomArray.randomArray;
+
+public class FindMinValueInRotatedArray {
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,8,9,-4,0,2};
+        System.out.println(findMinIndex(nums));
+    }
+    private static int findMinIndex(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[left] < nums[mid]) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return Math.min(nums[left], nums[right]);
+    }
+}
