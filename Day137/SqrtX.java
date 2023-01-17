eecfab40e614af5995507b4a97b60b54d00975d6
geekqq <jinwei1014@gmail.com>
2023-01-16 18:12:27 -0800

    sqrt x

---
+++ Day137/SqrtX.java
@@ -0,0 +1,27 @@
+package Day137;
+
+public class SqrtX {
+    public static void main(String[] args) {
+        for (int i = 0; i < 27; i++) {
+            System.out.println(i + " sqrt = " + mySqrt(i));
+        }
+    }
+
+    private static int mySqrt(int x) {
+        if (x < 2) return x;
+        int left = 2;
+        int right = x / 2;
+
+        while (left <= right) {
+            int res = left + (right - left) / 2;
+            if (res == x / res) {
+                return res;
+            } else if (res < x / res) {
+                left = res + 1;
+            } else {
+                right = res - 1;
+            }
+        }
+        return right;
+    }
+}
