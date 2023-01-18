426921e5ee0d1299a750969846dc87d4e201e339
geekqq <jinwei1014@gmail.com>
2023-01-18 15:08:56 -0800

    square root and find h index

---
+++ Day139/FindHIndex.java
@@ -0,0 +1,29 @@
+package Day139;
+
+public class FindHIndex {
+    public static void main(String[] args) {
+        int[] citations = {1,2,3,4,5,7,10,15};
+        System.out.println(findHIndex(citations));
+    }
+    private static int findHIndex(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) {
+            return nums[0] == 0 ? 0 : 1;
+        }
+        int left = 0;
+        int n = nums.length;
+        int right = n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == n - mid) {
+                return n - mid;
+            } else if (nums[mid] < n - mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return n - left;
+    }
+}
