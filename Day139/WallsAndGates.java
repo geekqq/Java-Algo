eaff8efd018c269d410f99ab2fd88003a4f2c6fc
geekqq <jinwei1014@gmail.com>
2023-01-18 14:58:54 -0800

    walls and gates

---
+++ Day139/WallsAndGates.java
@@ -0,0 +1,64 @@
+package Day139;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class WallsAndGates {
+    private static final int[][] DIRECTIONS = {
+            {1,0}, {-1,0}, {0,1}, {0,-1}
+    };
+
+    private static void wallsAndGates(int[][] rooms) {
+        if (rooms == null || rooms.length == 0
+        || rooms[0].length == 0 || rooms[0] == null) {
+            return;
+        }
+
+        int rows = rooms.length;
+        int cols = rooms[0].length;
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (rooms[i][j] == 0) {
+                    queue.offer(i * cols + j);
+                }
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int p = queue.poll();
+            int pRowIndex = p / cols;
+            int pColIndex = p % cols;
+
+            for (int[] dir : DIRECTIONS) {
+                int ii = pRowIndex + dir[0];
+                int jj = pColIndex + dir[1];
+
+                if (ii < 0 || ii >= rows || jj < 0 || jj >= cols
+                || rooms[ii][jj] <= rooms[pRowIndex][pColIndex] + 1) {
+                    continue;
+                }
+                rooms[ii][jj] = rooms[pRowIndex][pColIndex] + 1;
+                queue.offer(ii * cols + jj);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] rooms = {
+                {2147483647,-1,0,2147483647},
+                {2147483647,2147483647,2147483647,-1},
+                {2147483647,-1,2147483647,-1},
+                {0,-1,2147483647,2147483647}
+        };
+        wallsAndGates(rooms);
+        for (int i = 0; i < rooms.length; i++) {
+            for (int j = 0; j < rooms[i].length; j++) {
+                System.out.print(rooms[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
