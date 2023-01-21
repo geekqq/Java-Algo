7d76a7fc0c334b7ec73ecb0c9ac7b984ddfbecc2
stone <geekqq>
2023-01-20 20:35:18 -0800

    count primes

--- Day141/CountPrimes.java
+++ Day141/CountPrimes.java
@@ -1,2 +1,28 @@
-package Day141;public class CountPrimes {
+package Day141;
+
+import java.util.Arrays;
+
+public class CountPrimes {
+    public static void main(String[] args) {
+        System.out.println(isPrime(2));
+    }
+
+    private static int isPrime(int n) {
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+        for (int i = 2; i < Math.sqrt(n); i++){
+            if (isPrime[i]) {
+                for (int j = i * i; j <n; j+= i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
