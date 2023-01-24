5f1bd1f6e9d58c79603c0adc610db12ae3846250
geekqq <jinwei1014@gmail.com>
2023-01-24 12:29:57 -0800

    roman to integer

---
+++ Day145/RomanToInteger.java
@@ -0,0 +1,59 @@
+package Day145;
+
+import java.util.Map;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        System.out.println(romanToInt("CMXCIV"));
+        System.out.println(romanToInt2("CMXCIV"));
+
+    }
+
+    private static final Map<String, Integer> MAP = Map.of(
+            "M", 1000,
+            "D", 500,
+            "C", 100,
+            "L", 50,
+            "X", 10,
+            "V", 5,
+            "I", 1
+    );
+    private static int romanToInt(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int i = 0;
+        int sum = 0;
+        while (i < s.length()) {
+            String curSym = s.substring(i, i + 1);
+            int curVal = MAP.get(curSym);
+            if (i + 1 < s.length()) {
+                String nextSym = s.substring(i + 1, i + 2);
+                int nextVal = MAP.get(nextSym);
+                if (nextVal > curVal) {
+                    sum += nextVal - curVal;
+                    i += 2;
+                } else {
+                    sum += curVal;
+                    i++;
+                }
+            }
+        }
+        return sum;
+    }
+    private static int romanToInt2(String s) {
+        if (s == null || s.length() == 0) return 0;
+        String lastSym = s.substring(s.length() - 1);
+        int lastVal = MAP.get(lastSym);
+        int sum = lastVal;
+        for (int i = s.length() - 2; i >= 0; i--) {
+            String curSym = s. substring(i, i + 1);
+            int curVal = MAP.get(curSym);
+            if (curVal < lastVal) {
+                sum -= curVal;
+            } else {
+                sum += curVal;
+            }
+            lastVal = curVal;
+        }
+        return sum;
+    }
+}
