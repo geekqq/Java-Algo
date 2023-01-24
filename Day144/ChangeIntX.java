8321107c267a632a412864d3266ee5aeaba925db
geekqq <jinwei1014@gmail.com>
2023-01-23 20:37:03 -0800

    palindrome int

---
+++ Day144/ChangeIntX.java
@@ -0,0 +1,12 @@
+package Day144;
+
+public class ChangeIntX {
+    public static void main(String[] args) {
+        int x = 5;
+        changeIntValue(x);
+        System.out.println(x);
+    }
+    private static void changeIntValue(int x) {
+        x = 10;
+    }
+}
