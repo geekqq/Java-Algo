5354bd987114b0338f5c9170b61269dada2c6189
geekqq <jinwei1014@gmail.com>
2023-01-24 13:26:07 -0800

    find min value in rotated array

---
+++ Day145/FindMinValueInRotatedArray.java
@@ -0,0 +1,23 @@
+package Day145;
+
+public class FindMinValueInRotatedArray {
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,8,9,-1,0,2,3};
+        System.out.println(findMinValueInRotatedArray(nums));
+    }
+    private static int findMinValueInRotatedArray(int[] nums) {
+        if (nums == null || nums.length == 0) return -1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[left] < nums[mid]) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return Math.min(nums[left], nums[right]);
+    }
+}
+
