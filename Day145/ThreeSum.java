7d10c1e2d5a320e5ac831b264038916eece7f043
geekqq <jinwei1014@gmail.com>
2023-01-24 12:23:39 -0800

    three sum

---
+++ Day145/ThreeSum.java
@@ -0,0 +1,39 @@
+package Day145;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        int[] nums = {-1, 2, 1, 0, -4, -1, 4};
+        System.out.println(threeSum(nums));
+    }
+
+    private static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int temp = nums[left] + nums[right];
+                if (nums[i] + temp == 0) {
+                    res.add(Arrays.asList(nums[left], nums[right], nums[i]));
+                    left++;
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                } else if (nums[i] + temp < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return res;
+    }
+}
