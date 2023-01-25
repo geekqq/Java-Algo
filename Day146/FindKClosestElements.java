5ac9d3a0d2208ffad924d87ae1894b4a902c9e1f
geekqq <jinwei1014@gmail.com>
2023-01-25 10:30:42 -0800

    find k closest elements

---
+++ Day146/FindKClosestElements.java
@@ -0,0 +1,54 @@
+package Day146;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class FindKClosestElements {
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,3,3,3,4,4,5,5,5,5,5};
+        System.out.println(findKClosestElements(nums, 4,4));
+    }
+
+    private static List<Integer> findKClosestElements(int[] nums, int k, int x) {
+        List<Integer> res = new LinkedList<>();
+        if (nums == null || nums.length == 0 || k <= 0) return res;
+        k = Math.min(k, nums.length);
+        int pos = findClosestIndex(nums, x);
+        res.add(nums[pos]);
+        addRemaning(res, nums, pos, k - 1, x);
+        return res;
+    }
+
+    private static int findClosestIndex(int[] nums, int x) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == x) return mid;
+            else if (nums[mid] < x) left = mid;
+            else right = mid;
+        }
+        return Math.abs(nums[left] - x) <= Math.abs(nums[right] - x) ? left : right;
+    }
+    private static void addRemaning(List<Integer> res, int [] nums, int pos, int count, int x) {
+        if (count == 0) return;
+        int left = pos - 1;
+        int right = pos + 1;
+        while (left >= 0 && right < nums.length && count > 0) {
+            if (Math.abs(nums[left] - x) <= Math.abs(nums[right] - x)) {
+                res.add(0, nums[left--]);
+            } else {
+                res.add(nums[right++]);
+            }
+            count--;
+        }
+        while (count > 0) {
+            if (left < 0) {
+                res.add(nums[right++]);
+            } else {
+                res.add(0, nums[left--]);
+            }
+            count--;
+        }
+    }
+}
