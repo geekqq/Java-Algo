6c69aada3320756a37f7e3265eb151c57964520a
geekqq <jinwei1014@gmail.com>
2023-01-26 13:37:56 -0800

    my power

---
+++ Day147/MyPow.java
@@ -0,0 +1,19 @@
+package Day147;
+
+public class MyPow {
+    public static void main(String[] args) {
+        System.out.println(myPow(2,-3));
+    }
+
+    private static double myPow(double x, int n) {
+        if (n < 0) {
+            n = -n;
+            x = 1 / x;
+        }
+        double res = 1;
+        for (int i = 0; i < n; i++) {
+            res *= x;
+        }
+        return res;
+    }
+}
