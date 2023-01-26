f31c98056298532532c4242b5f301cdc1603ed95
geekqq <jinwei1014@gmail.com>
2023-01-26 13:06:00 -0800

    sqrtX and three sum

---
+++ Day147/SqrtX.java
@@ -0,0 +1,26 @@
+package Day147;
+
+public class SqrtX {
+    public static void main(String[] args) {
+        for (int i = 0; i <26; i++) {
+            System.out.println(i + " sqrt = " + mySqurt(i));
+        }
+    }
+
+    private static int mySqurt(int x) {
+        if (x < 2) return x;
+        int left = 2;
+        int right = x / 2;
+        while (left <= right) {
+            int res = left + (right - left) / 2;
+            if (res == x / res) {
+                return res;
+            } else if (res < x / res) {
+                left = res + 1;
+            } else {
+                right = res - 1;
+            }
+        }
+        return right;
+    }
+}
