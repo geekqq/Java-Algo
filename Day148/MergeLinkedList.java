3776d06d1c0e09891b3f52fce01b4a3f1941e352
geekqq <jinwei1014@gmail.com>
2023-01-27 13:27:28 -0800

    merge linked list

---
+++ Day148/MergeLinkedList.java
@@ -0,0 +1,91 @@
+package Day148;
+
+public class MergeLinkedList {
+    private ListNode head;
+    static class ListNode {
+        int val;
+        ListNode next;
+        public ListNode(int val) {
+            this.val = val;
+            next = null;
+        }
+    }
+    public void add(int val) {
+        ListNode newNode = new ListNode(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            ListNode cur = head;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+    }
+    public void print() {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    private static ListNode merge1(ListNode head1, ListNode head2) {
+        if (head1 == null) {
+            return head2;
+        }
+        if (head2 == null) {
+            return head1;
+        }
+        if (head1.val < head2.val) {
+            head1.next = merge1(head1.next, head2);
+            return head1;
+        } else {
+            head2.next = merge1(head1, head2.next);
+            return head2;
+        }
+    }
+    private static ListNode merge2(ListNode head1, ListNode head2) {
+        ListNode dummyNode = new ListNode(0);
+        ListNode tail = dummyNode;
+        while (true) {
+            if (head1 == null) {
+                tail.next = head2;
+                break;
+            }
+            if (head2 == null) {
+                tail.next = head1;
+                break;
+            }
+            if (head1.val < head2.val) {
+                tail.next = head1;
+                head1 = head1.next;
+            } else {
+                tail.next = head2;
+                head2 = head2.next;
+            }
+            tail = tail.next;
+        }
+        return dummyNode.next;
+    }
+
+    public static void main(String[] args) {
+        MergeLinkedList list1 = new MergeLinkedList();
+        MergeLinkedList list2 = new MergeLinkedList();
+        MergeLinkedList list3 = new MergeLinkedList();
+        list1.add(2);
+        list1.add(4);
+        list1.add(6);
+        list1.add(8);
+        list2.add(1);
+        list2.add(3);
+        list2.add(4);
+        list2.add(5);
+        list2.add(7);
+        list2.add(9);
+        list2.add(11);
+        list3.head = merge2(list1.head, list2.head);
+        list3.print();
+    }
+}
