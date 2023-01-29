2bf9dadedd4bccdd57102ef629823e99ca60abad
geekqq <jinwei1014@gmail.com>
2023-01-29 10:06:56 -0800

    roman to integer

---
+++ Day150/LongestIncreasingSubsequences.java
@@ -0,0 +1,36 @@
+package Day150;
+
+public class LongestIncreasingSubsequences {
+    public static void main(String[] args) {
+        int[] nums = {0,1,3,2,3,4,3,6,9};
+        System.out.println(getLIS(nums));
+    }
+    private static int getLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        int len = nums.length;
+        int[] increasingSeq = new int[len];
+        int index = 0;
+        increasingSeq[0] = nums[index++];
+
+        for (int i = 1; i < len; i++) {
+            if (nums[i] > nums[i - 1]) {
+                increasingSeq[index++] = nums[i];
+            } else {
+                int pos = findInsertPos(increasingSeq, 0, index - 1, nums[i]);
+                increasingSeq[pos] = nums[i];
+            }
+        }
+        return index;
+    }
+    private static int findInsertPos(int[] nums, int start, int end, int target) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) start = mid + 1;
+            else end = mid - 1;
+        }
+        return start;
+    }
+}
---
+++ Day150/RomanToInteger.java
@@ -0,0 +1,65 @@
+package Day150;
+
+import java.util.Map;
+
+import static Day149.RomanToInteger.MAP;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        System.out.println(romanToInt("CMXCIIV"));
+        System.out.println(romanToInt2("CMXCIIV"));
+
+    }
+
+//    public static final Map<Character, Integer> MAP = Map.of(
+//
+//    );
+
+    private static int romanToInt(String s) {
+        if (s == null || s.length() == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        int i = 0;
+        int sum = 0;
+        while (i < s.length()) {
+            String curSym = s.substring(i, i + 1);
+            int curVal = MAP.get(curSym);
+
+            if (i + 1 < s.length()) {
+                String nextSym = s.substring(i + 1, i + 2);
+                int nextVal = MAP.get(nextSym);
+
+                if (curVal < nextVal) {
+                    sum += nextVal - curVal;
+                    i += 2;
+                } else {
+                    sum += curVal;
+                    i++;
+                }
+            }
+        }
+        return sum;
+    }
+
+    private static int romanToInt2(String s) {
+        if (s == null || s.length() == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        String lastSym = s.substring(s.length() - 1);
+        int lastVal = MAP.get(lastSym);
+        int sum = lastVal;
+        for (int i = s.length() - 2; i >= 0; i--) {
+            String curSym = s.substring(i, i + 1);
+            int curVal = MAP.get(curSym);
+            if (curVal < lastVal) {
+                sum -= curVal;
+            } else {
+                sum += curVal;
+            }
+            lastVal = curVal;
+        }
+        return sum;
+    }
+}
