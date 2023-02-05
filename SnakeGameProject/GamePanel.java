c36e22963a48b3e895291799bfe3abad1d73e2e5
geekqq <jinwei1014@gmail.com>
2023-02-04 22:53:48 -0800

    calculator simulator

--- SnakeGameProject/GamePanel.java
+++ SnakeGameProject/GamePanel.java
@@ -8,7 +8,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
     static final int SCREEN_WIDTH = 600;
     static final int SCREEN_HEIGH = 600;
-    static final int UNIT_SIZE = 25;
+    static final int UNIT_SIZE = 15;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGH) / UNIT_SIZE;
     static final int DELAY = 75;
     final int[] x = new int[GAME_UNITS];
