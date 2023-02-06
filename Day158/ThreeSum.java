94928cf719183abd4199645195ca444bba1b346a
geekqq <jinwei1014@gmail.com>
2023-02-06 10:39:17 -0800

    three sum

---
+++ Day158/ThreeSum.java
@@ -0,0 +1,40 @@
+package Day158;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreeSum {
+    private static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (nums == null || nums.length == 0) return res;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int temp = nums[left] + nums[right];
+                if (temp + nums[i] == 0) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                } else if (temp + nums[i] < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,0,-1,4,1,2,0,-4};
+        System.out.println(threeSum(nums));
+    }
+}
