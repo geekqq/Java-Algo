79b24e96b7d0e5a72cb0b6ca9b5d5e4bfce9df1d
geekqq <jinwei1014@gmail.com>
2023-02-08 11:38:51 -0800

    is valid parentheses

---
+++ Day160/ValidParentheses.java
@@ -0,0 +1,35 @@
+package Day160;
+
+import java.util.Stack;
+
+import static Day152.ValidParentheses.MAP;
+
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+        System.out.println(validParentheses("(){}()[]"));
+    }
+
+    private static boolean validParentheses(String s) {
+        if (s == null || s.length() == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (MAP.containsKey(c)) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    char open = stack.pop();
+                    if (MAP.get(open) != c) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
