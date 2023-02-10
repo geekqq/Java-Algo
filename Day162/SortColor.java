aa750d65fe2834d85da2bba894d5ee36c1765a9d
geekqq <jinwei1014@gmail.com>
2023-02-10 11:51:58 -0800

    sort colors

---
+++ Day162/SortColor.java
@@ -0,0 +1,31 @@
+package Day162;
+
+import java.util.Arrays;
+
+import static Day138.RandomArray.randomArray;
+import static Day44.SelectSort.swap;
+
+public class SortColor {
+    public static void main(String[] args) {
+        int[] colors = randomArray(3,12);
+        System.out.println(Arrays.toString(colors));
+        sortColor(colors);
+        System.out.println(Arrays.toString(colors));
+
+    }
+
+    private static void sortColor(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        for (int i = 0; i <= right; i++) {
+            if (nums[i] == 0) {
+                swap(nums, left++, i);
+            } else if (nums[i] == 2) {
+                swap(nums, right--, i--);
+            }
+        }
+    }
+}
