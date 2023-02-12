0600ded19234e8364685311c7c7ff0642dd5a995
geekqq <jinwei1014@gmail.com>
2023-02-11 17:42:15 -0800

    sort list

---
+++ Day163/SortList.java
@@ -0,0 +1,86 @@
+package Day163;
+
+import Day156.SortingList;
+
+public class SortList {
+    private ListNode head;
+
+    public void add(int val) {
+        ListNode newNode = new ListNode(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            ListNode cur = head;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+    }
+
+    public void print() {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    private static ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+
+        return mergeList(left, right);
+    }
+
+    private static ListNode mergeList(ListNode leftHead, ListNode rightHead) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (leftHead != null && rightHead != null) {
+            if (leftHead.val <= rightHead.val) {
+                cur.next = leftHead;
+                leftHead = leftHead.next;
+            } else {
+                cur.next = rightHead;
+                rightHead = rightHead.next;
+            }
+            cur = cur.next;
+        }
+        if (leftHead != null) {
+            cur.next = leftHead;
+        }
+        if (rightHead != null) {
+            cur.next = rightHead;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        SortList list = new SortList();
+        list.add(3);
+        list.add(2);
+        list.add(4);
+        list.add(7);
+        list.add(5);
+        list.add(1);
+        list.add(8);
+        list.add(9);
+        list.add(6);
+        list.print();
+        list.head = sortList(list.head);
+        list.print();
+    }
+}
