6ee439d9588c65b2914d934cd42c45453c87786c
geekqq <jinwei1014@gmail.com>
2023-02-11 17:27:08 -0800

    move zeroes to end

---
+++ Day162/MoveZeroesToEnd.java
@@ -0,0 +1,25 @@
+package Day162;
+
+import java.util.Arrays;
+
+import static Day138.RandomArray.randomArray;
+import static Day44.SelectSort.swap;
+
+public class MoveZeroesToEnd {
+    public static void main(String[] args) {
+        int[] nums = randomArray(3, 10);
+        System.out.println(Arrays.toString(nums));
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private static void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        int zero = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                swap(nums, zero++, i);
+            }
+        }
+    }
+}
