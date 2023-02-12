b77e21581930941a305e1c6c7bb723397265aba8
geekqq <jinwei1014@gmail.com>
2023-02-12 09:25:30 -0800

    insert sort list

---
+++ Day164/InsertSortList.java
@@ -0,0 +1,54 @@
+package Day164;
+
+public class InsertSortList {
+    private ListNode head;
+    public void add(int val) {
+        ListNode newNode = new ListNode(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            ListNode temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+    public void print() {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    private static ListNode insertSortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(0);
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode prev = dummy;
+            while (prev.next != null && cur.val > prev.next.val) {
+                prev = prev.next;
+            }
+            ListNode insertNode = cur;
+            cur = cur.next;
+            insertNode.next = prev.next;
+            prev.next = insertNode;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        InsertSortList list = new InsertSortList();
+        list.add(2);
+        list.add(5);
+        list.add(1);
+        list.add(0);
+        list.add(4);
+        list.add(3);
+        list.add(6);
+        list.head = insertSortList(list.head);
+        list.print();
+    }
+}
