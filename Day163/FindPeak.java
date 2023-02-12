94063631eac4a2f5b384967bcdcb46a803f22835
geekqq <jinwei1014@gmail.com>
2023-02-11 17:02:26 -0800

    find peak index

---
+++ Day163/FindPeak.java
@@ -0,0 +1,28 @@
+package Day163;
+
+public class FindPeak {
+    public static void main(String[] args) {
+        int[] nums = {1,4,6,7,9,0,-1,-4,5};
+        System.out.println(findPeakIndex(nums));
+    }
+
+    private static int findPeakIndex(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (mid > 0 && nums[mid] > nums[mid - 1]){
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left] > nums[right] ? left : right;
+    }
+}
