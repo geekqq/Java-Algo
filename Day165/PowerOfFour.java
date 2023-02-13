345e6902490d014e894db548bf213a233686e020
geekqq <jinwei1014@gmail.com>
2023-02-13 11:51:28 -0800

    power of four

---
+++ Day165/PowerOfFour.java
@@ -0,0 +1,19 @@
+package Day165;
+
+public class PowerOfFour {
+    public static void main(String[] args) {
+        System.out.println(powerOfFour(4));
+        System.out.println(powerOfFour(0));
+        System.out.println(powerOfFour(1));
+        System.out.println(powerOfFour(2));
+        System.out.println(powerOfFour(16));
+        System.out.println(powerOfFour(64));
+    }
+
+    private static boolean powerOfFour(int n) {
+        if (n <= 0) return false;
+        if (n == 1) return true;
+        if (n % 4 != 0) return false;
+        return powerOfFour(n / 4);
+    }
+}
