76f43e51b402adf25c3ed4802d984c9330292cbb
geekqq <jinwei1014@gmail.com>
2023-02-16 13:24:44 -0800

    find peak

---
+++ Day168/FindPeak.java
@@ -0,0 +1,24 @@
+package Day168;
+
+public class FindPeak {
+    public static void main(String[] args) {
+        int[] nums = {2,3,4,5,7,5,4,3,1,0};
+        System.out.println(findPeakIndex(nums));
+    }
+    private static int findPeakIndex(int[] nums) {
+        if (nums == null || nums.length == 0) return -1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1]) {
+                return mid;
+            } else if (mid > 0 && nums[mid] > nums[mid - 1]) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return Math.max(nums[left], nums[right]);
+    }
+}
