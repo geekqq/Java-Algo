130702ae9592c2e5974939441f929887c9b27ca1
geekqq <jinwei1014@gmail.com>
2023-02-18 12:32:42 -0800

    sort colors

---
+++ Day170/SortColors.java
@@ -0,0 +1,29 @@
+package Day170;
+
+import java.util.Arrays;
+
+import static Day138.RandomArray.randomArray;
+import static Day44.SelectSort.swap;
+
+public class SortColors {
+    public static void main(String[] args) {
+        int[] colors = randomArray(3,10);
+        System.out.println(Arrays.toString(colors));
+        sortColors(colors);
+        System.out.println(Arrays.toString(colors));
+
+    }
+
+    public static void sortColors(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        int left = 0;
+        int right = nums.length - 1;
+        for (int i = 0; i <= right; i++) {
+            if (nums[i] == 0) {
+                swap(nums, left++, i);
+            } else if (nums[i] == 2) {
+                swap(nums, right--, i--);
+            }
+        }
+    }
+}
