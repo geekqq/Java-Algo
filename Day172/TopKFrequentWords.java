13dcf22d6675c4fd9f90276416f3910d47f260d5
geekqq <jinwei1014@gmail.com>
2023-02-21 14:51:02 -0800

    top k frequent words

---
+++ Day172/TopKFrequentWords.java
@@ -0,0 +1,25 @@
+package Day172;
+
+import java.util.*;
+
+public class TopKFrequentWords {
+    private static List<String> frequentWords(String[] words, int k) {
+        if (words == null || words.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        List<String> list = new ArrayList<>(map.keySet());
+        Collections.sort(list, (w1, w2) -> map.get(w1) == map.get(w2) ? w1.compareTo(w2) : map.get(w2) - map.get(w1));
+        return list.subList(0, k);
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"i","love","leetcode","i","love","coding"};
+        System.out.println(frequentWords(words, 2));
+        String[] words2 = {"the","day","is","sunny","the","the","the","sunny","is","is"};
+        System.out.println(frequentWords(words2, 4));
+    }
+}
