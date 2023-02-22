11dc5f62bd361bf67ce268be315da2ef2c055bb1
geekqq <jinwei1014@gmail.com>
2023-02-21 20:16:02 -0800

    palindrome number

---
+++ Day172/IsPalindrome.java
@@ -0,0 +1,21 @@
+package Day172;
+
+public class IsPalindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(0));
+        System.out.println(isPalindrome(100));
+        System.out.println(isPalindrome(101));
+        System.out.println(isPalindrome(12321));
+        System.out.println(isPalindrome(1000));
+    }
+
+    private static boolean isPalindrome(int x) {
+        if (x < 0 || x % 10 == 0 && x != 0) return false;
+        int n = 0;
+        while (x > n) {
+            n = n * 10 + x % 10;
+            x /= 10;
+        }
+        return x == n || x == n / 10;
+    }
+}
