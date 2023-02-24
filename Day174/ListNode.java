be6ff25e589431f685db1d0f14a9bd06bd27beb2
geekqq <jinwei1014@gmail.com>
2023-02-23 22:14:52 -0800

    reverse linked list by range

---
+++ Day174/ListNode.java
@@ -0,0 +1,10 @@
+package Day174;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    public ListNode(int val) {
+        this.val = val;
+        next = null;
+    }
+}
---
+++ Day174/ReverseLinkedListByRange.java
@@ -0,0 +1,79 @@
+package Day174;
+
+public class ReverseLinkedListByRange {
+    private ListNode head;
+
+    public void add(int val) {
+        ListNode newNode = new ListNode(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            ListNode cur = head;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+    }
+    private void print() {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    private static ListNode reverseByOne(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode prev = null;
+        ListNode cur = head;
+        ListNode next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+
+    private static ListNode reverseByRange(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode subStart = dummy;
+        ListNode subEnd = dummy;
+        ListNode next = dummy;
+        for (int i = 0; i < left - 1; i++) {
+            prev = prev.next;
+        }
+        subStart = prev.next;
+        prev.next = null;
+        subEnd = subStart;
+        for (int i = left; i < right; i++) {
+            subEnd = subEnd.next;
+        }
+        next = subEnd.next;
+        subEnd.next = null;
+        prev.next = reverseByOne(subStart);
+        subStart.next = next;
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ReverseLinkedListByRange list = new ReverseLinkedListByRange();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.print();
+        list.head = reverseByRange(list.head, 1, 3);
+        list.print();
+    }
+}
