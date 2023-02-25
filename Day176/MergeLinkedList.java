287508f5ea4380aa6dfe597fe4c43aae3c969e24
geekqq <jinwei1014@gmail.com>
2023-02-25 08:23:58 -0800

    Merge Linked List

---
+++ Day176/MergeLinkedList.java
@@ -0,0 +1,82 @@
+package Day176;
+
+import Day174.ListNode;
+
+public class MergeLinkedList {
+    private ListNode head;
+
+    public void add(int val) {
+        ListNode newNode = new ListNode(val);
+        if (head == null) {
+            head = newNode;
+        } else  {
+            ListNode cur = head;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+    }
+
+    public void print(ListNode head) {
+        if (head == null) return;
+        System.out.print(head.val + " ");
+        print(head.next);
+    }
+
+    private static ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+
+        return mergeList(left, right);
+    }
+
+    private static ListNode mergeList(ListNode leftHead, ListNode rightHead) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+
+        while (leftHead != null && rightHead != null) {
+            if (leftHead.val <= rightHead.val) {
+                cur.next = leftHead;
+                leftHead = leftHead.next;
+            } else {
+                cur.next = rightHead;
+                rightHead = rightHead.next;
+            }
+            cur = cur.next;
+            if (leftHead != null) {
+                cur.next = leftHead;
+            }
+            if (rightHead != null) {
+                cur.next = rightHead;
+            }
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        MergeLinkedList list = new MergeLinkedList();
+        list.add(3);
+        list.add(1);
+        list.add(0);
+        list.add(2);
+        list.add(5);
+        list.add(4);
+        list.add(6);
+        list.print(list.head);
+        System.out.println();
+        list.head = sortList(list.head);
+        list.print(list.head);
+    }
+}
