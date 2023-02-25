71374e8a7beca50f36ad60a56d2bf8ecb6d2061b
geekqq <jinwei1014@gmail.com>
2023-02-25 08:01:01 -0800

    integer min & max value

---
+++ Day176/IntegerMaxMinDemo.java
@@ -0,0 +1,11 @@
+package Day176;
+
+public class IntegerMaxMinDemo {
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE / 10);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MIN_VALUE / 10);
+    }
+
+}
