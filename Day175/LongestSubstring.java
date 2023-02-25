3f7adfc534fbb2997f151a387e79d831b9b9cf30
geekqq <jinwei1014@gmail.com>
2023-02-24 22:23:57 -0800

    get length of Longest Substring

---
+++ Day175/LongestSubstring.java
@@ -0,0 +1,51 @@
+package Day175;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstring {
+    public static void main(String[] args) {
+        System.out.println(getLengthOfLongestSubstring("jinweihuStonehoo"));
+        System.out.println(getLengthOfLongestSubstringII("jinweihuStonehoo"));
+    }
+
+    private static int getLengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            throw new IllegalArgumentException();
+        }
+        int left = 0;
+        int right = 0;
+        int maxLen = 0;
+        Set<Character> set = new HashSet<>();
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            if (set.add(c)) {
+                maxLen = Math.max(maxLen, set.size());
+                right++;
+            } else {
+                set.remove(s.charAt(left));
+                left++;
+            }
+        }
+        return maxLen;
+    }
+
+    private static int getLengthOfLongestSubstringII(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int maxLen = 0;
+        String test = "";
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            String cur = String.valueOf(c);
+            if (test.contains(cur)) {
+                test = test.substring(test.indexOf(cur) + 1);
+            }
+            test += cur;
+            maxLen = Math.max(maxLen, test.length());
+        }
+        return maxLen;
+    }
+}
