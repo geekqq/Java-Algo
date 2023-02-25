898066f050f1fbb0376442e638d2f525ab957045
geekqq <jinwei1014@gmail.com>
2023-02-24 21:53:27 -0800

    select sort

---
+++ Day175/SelectSort.java
@@ -0,0 +1,29 @@
+package Day175;
+
+import java.util.Arrays;
+
+import static Day138.RandomArray.randomArray;
+import static Day44.SelectSort.swap;
+
+public class SelectSort {
+    public static void main(String[] args) {
+        int[] nums = randomArray(10, 10);
+        System.out.println(Arrays.toString(nums));
+        selectSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private static void selectSort(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        int minIndex;
+        for (int i = 0; i < nums.length - 1; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[minIndex] > nums[j]) {
+                    minIndex = j;
+                }
+            }
+            swap(nums, i, minIndex);
+        }
+    }
+}
