721b4832d59ce8d4e39585291d7579fbc3c92573
geekqq <jinwei1014@gmail.com>
2023-02-27 12:21:44 -0800

    find kth lartest element in array

---
+++ Day178/FindKthLargestElement.java
@@ -0,0 +1,28 @@
+package Day178;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+import static Day138.RandomArray.randomArray;
+
+public class FindKthLargestElement {
+    public static void main(String[] args) {
+        int[] nums = randomArray(10, 10);
+        System.out.println(Arrays.toString(nums));
+        System.out.println(findKthLargest(nums, 4));
+    }
+
+    private static int findKthLargest(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < nums.length; i++) {
+            pq.offer(nums[i]);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+}
