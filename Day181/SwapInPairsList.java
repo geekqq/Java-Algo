b6b168803db426a7243dc4a20536899439a014d9
geekqq <jinwei1014@gmail.com>
2023-03-02 12:56:19 -0800

    swap in pairs

---
+++ Day181/SwapInPairsList.java
@@ -0,0 +1,49 @@
+package Day181;
+
+import Day174.ListNode;
+
+public class SwapInPairsList {
+    private ListNode head;
+
+    public void add(int val) {
+        ListNode newNode = new ListNode(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            ListNode cur = head;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+    }
+
+    public void print() {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    private static ListNode swapInPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode postHead = swapInPairs(head.next.next);
+        head.next.next = head;
+        ListNode newHead = head.next;
+        head.next = postHead;
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        SwapInPairsList list = new SwapInPairsList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.print();
+        list.head = swapInPairs(list.head);
+        list.print();
+    }
+}
