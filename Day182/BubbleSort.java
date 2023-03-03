af3aec76d255c6ad5033c69e0719f5067898adac
geekqq <jinwei1014@gmail.com>
2023-03-03 15:04:07 -0800

    bubble sort

---
+++ Day182/BubbleSort.java
@@ -0,0 +1,33 @@
+package Day182;
+
+import java.util.Arrays;
+
+import static Day138.RandomArray.randomArray;
+import static Day44.SelectSort.swap;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] nums = randomArray(10, 10);
+        System.out.println(Arrays.toString(nums));
+        bubbleSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private static void bubbleSort(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        for (int i = 0; i < nums.length - 1; i++) {
+            boolean flag = true;
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    swap(nums, j, j + 1);
+                    flag = false;
+                }
+            }
+            if (flag) {
+                break;
+            }
+        }
+    }
+}
