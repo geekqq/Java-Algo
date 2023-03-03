6527877e3cd729923ef95ef1e5f830016f977315
geekqq <jinwei1014@gmail.com>
2023-03-02 21:53:13 -0800

    find peak index

---
+++ Day181/FindPeakIndex.java
@@ -0,0 +1,25 @@
+package Day181;
+
+public class FindPeakIndex {
+    public static void main(String[] args) {
+        int[] nums = {2,3,4,5,1,2,3,0};
+        System.out.println(findPeakIndex(nums));
+    }
+
+    private static int findPeakIndex(int[] nums) {
+        if (nums == null || nums.length == 0) return -1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1]) {
+                return mid;
+            } else if (mid > 0 && nums[mid] > nums[mid - 1]) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left] > nums[right] ? left : right;
+    }
+}
