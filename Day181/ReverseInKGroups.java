dc6f74507f79a0067951412428176728d6046fe0
geekqq <jinwei1014@gmail.com>
2023-03-02 21:41:06 -0800

    reverse in k groups

--- Day181/ReverseInKGroups.java
+++ Day181/ReverseInKGroups.java
@@ -52,13 +52,41 @@ public class ReverseInKGroups {
         return prev;
     }
 
+    private static ListNode reverseKGroupII(ListNode head, int k) {
+        if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+        ListNode cur = dummy;
+        ListNode next = dummy;
+        int count = 0;
+        while (cur.next != null) {
+            count++;
+            cur = cur.next;
+        }
+        while (count >= k) {
+            cur = prev.next;
+            next = cur.next;
+            for (int i = 1; i < k; i++) {
+                cur.next = next.next;
+                next.next = prev.next;
+                prev.next = next;
+                next = cur.next;
+            }
+            prev = cur;
+            count -= k;
+        }
+        return dummy.next;
+    }
+
     public static void main(String[] args) {
         ReverseInKGroups list = new ReverseInKGroups();
         for (int i = 0; i < 15; i++) {
             list.add(i);
         }
         list.print();
-        list.head = reverseInKList(list.head, 6);
+        list.head = reverseKGroupII(list.head, 4);
         list.print();
     }
 }
