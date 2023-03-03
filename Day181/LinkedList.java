c3c402d30e920dfeb77c471675cf23eb86a1a967
geekqq <jinwei1014@gmail.com>
2023-03-03 12:13:34 -0800

    swap in pairs in list

--- Day181/LinkedList.java
+++ Day181/LinkedList.java
@@ -86,6 +86,51 @@ public class LinkedList {
         return dummy.next;
     }
 
+    private static ListNode reverseByOne(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode prev = null;
+        ListNode cur = head;
+        ListNode next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+
+    private static ListNode reverseByRange(ListNode head, int left, int right) {
+        if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        for (int i = 0; i < left - 1; i++) {
+            prev = prev.next;
+        }
+        ListNode subStart = prev.next;
+        prev.next = null;
+        ListNode subEnd = subStart;
+        for (int i = left; i < right; i++) {
+            subEnd = subEnd.next;
+        }
+        ListNode next = subEnd.next;
+        subEnd.next = null;
+        prev.next = reverseByOne(subStart);
+        subStart.next = next;
+
+        return dummy.next;
+    }
+
+    private static ListNode swapInPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode postHead = swapInPairs(head.next.next);
+        head.next.next = head;
+        ListNode newHead = head.next;
+        head.next = postHead;
+        return newHead;
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
         list.add(3);
@@ -97,6 +142,10 @@ public class LinkedList {
         list.add(5);
         list.print();
         list.head = sortList(list.head);
+        list.print();
+        list.head = reverseByRange(list.head, 1,3);
+        list.print();
+        list.head = swapInPairs(list.head);
         list.print();
     }
 }
