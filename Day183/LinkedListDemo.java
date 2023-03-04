2209acc9f5f74ea32f5763f2eb96ed12f852a4bd
geekqq <jinwei1014@gmail.com>
2023-03-04 10:15:33 -0800

    add two numbers

--- Day183/LinkedListDemo.java
+++ Day183/LinkedListDemo.java
@@ -81,6 +81,104 @@ public class LinkedListDemo {
         return dummy.next;
     }
 
+    private static ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+        return mergeList(left, right);
+    }
+
+    private static ListNode mergeList(ListNode leftHead, ListNode rightHead) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (leftHead != null && rightHead != null) {
+            if (leftHead.val <= rightHead.val) {
+                cur.next = leftHead;
+                leftHead = leftHead.next;
+            } else {
+                cur.next = rightHead;
+                rightHead = rightHead.next;
+            }
+            cur = cur.next;
+        }
+        if (leftHead != null) {
+            cur.next = leftHead;
+        }
+        if (rightHead != null) {
+            cur.next = rightHead;
+        }
+        return dummy.next;
+    }
+
+    public static ListNode swapInPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode postHead = swapInPairs(head.next.next);
+        head.next.next = head;
+        ListNode newHead = head.next;
+        head.next = postHead;
+        return newHead;
+    }
+
+    public static ListNode reverseInKGroups(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode cur = dummy;
+        ListNode next = dummy;
+        int count = 0;
+
+        while (cur.next != null) {
+            count++;
+            cur = cur.next;
+        }
+
+        while (count >= k) {
+            cur = prev.next;
+            next = cur.next;
+            for (int i = 1; i < k; i++) {
+                cur.next = next.next;
+                next.next = prev.next;
+                prev.next = next;
+                next = cur.next;
+            }
+            prev = cur;
+            count -= k;
+        }
+        return dummy.next;
+    }
+
+    public static ListNode addTwoNumbers(ListNode head1, ListNode head2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        int carry = 0;
+        while (head1 != null || head2 != null || carry != 0) {
+            int x = head1 != null ? head1.val : 0;
+            int y = head2 != null ? head2.val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            if (head1 != null) {
+                head1 = head1.next;
+            }
+            if (head2 != null) {
+                head2 = head2.next;
+            }
+        }
+        return dummy.next;
+    }
     public static void main(String[] args) {
         LinkedListDemo list = new LinkedListDemo();
         list.add(3);
@@ -92,7 +190,24 @@ public class LinkedListDemo {
         list.print();
         list.head = reverseByRange(list.head, 1,4);
         list.print();
-        list.head = insertSortList(list.head);
+        list.head = sortList(list.head);
         list.print();
+        list.add(0);
+        list.head = swapInPairs(list.head);
+        list.print();
+        list.head = reverseInKGroups(list.head, 3);
+        list.print();
+        LinkedListDemo num1 = new LinkedListDemo();
+        LinkedListDemo num2 = new LinkedListDemo();
+        LinkedListDemo num3 = new LinkedListDemo();
+        num1.add(2);
+        num1.add(7);
+        num1.add(2);
+        num2.add(7);
+        num2.add(4);
+        num2.add(1);
+        num2.add(9);
+        num3.head = addTwoNumbers(num1.head, num2.head);
+        num3.print();
     }
 }
