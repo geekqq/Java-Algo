e562f53238bd70c5592940937355979d6f7d11ce
geekqq <jinwei1014@gmail.com>
2023-03-04 09:55:18 -0800

    linked list demo

---
+++ Day183/LinkedListDemo.java
@@ -0,0 +1,98 @@
+package Day183;
+
+import Day174.ListNode;
+
+public class LinkedListDemo {
+    private ListNode head;
+
+    public void add(int val) {
+        ListNode newNode = new ListNode(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            ListNode cur = head;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+    }
+
+    public void print() {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    private static ListNode reverseByOne(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode prev = null;
+        ListNode cur = head;
+        ListNode next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+
+    private static ListNode reverseByRange(ListNode head, int left, int right) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        for (int i = 0; i < left - 1; i++) {
+            prev = prev.next;
+        }
+        ListNode subStart = prev.next;
+        prev.next = null;
+        ListNode subEnd = subStart;
+        for (int i = left; i < right; i++) {
+            subEnd = subEnd.next;
+        }
+        ListNode next = subEnd.next;
+        subEnd.next = null;
+        prev.next = reverseByOne(subStart);
+        subStart.next = next;
+        return dummy.next;
+    }
+
+    private static ListNode insertSortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(0);
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode prev = dummy;
+            while (prev.next != null && cur.val > prev.next.val) {
+                prev = prev.next;
+            }
+            ListNode insertNode = cur;
+            cur = cur.next;
+            insertNode.next = prev.next;
+            prev.next = insertNode;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        LinkedListDemo list = new LinkedListDemo();
+        list.add(3);
+        list.add(1);
+        list.add(2);
+        list.add(6);
+        list.add(4);
+        list.add(5);
+        list.print();
+        list.head = reverseByRange(list.head, 1,4);
+        list.print();
+        list.head = insertSortList(list.head);
+        list.print();
+    }
+}
