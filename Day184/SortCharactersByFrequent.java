96e48a2dc35839bbb3d557cbbb044cee9f105b5c
geekqq <jinwei1014@gmail.com>
2023-03-05 14:24:23 -0800

    sort characters by frequency

---
+++ Day184/SortCharactersByFrequent.java
@@ -0,0 +1,28 @@
+package Day184;
+
+import java.util.*;
+
+public class SortCharactersByFrequent {
+    public static void main(String[] args) {
+        System.out.println(frequentSort("tree"));
+        System.out.println(frequentSort("stonehoo"));
+    }
+
+    public static String frequentSort(String s) {
+        if (s == null || s.length() == 0) return null;
+        Map<Character, Integer> map = new HashMap<>();
+        for (Character c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        List<Character> list = new LinkedList<>(map.keySet());
+        Collections.sort(list, (c1, c2) -> map.get(c2) - map.get(c1));
+        StringBuilder sb = new StringBuilder();
+        for (Character c : list) {
+            int count = map.get(c);
+            for (int i = 0; i < count; i++) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
