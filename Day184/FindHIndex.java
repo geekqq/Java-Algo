e00a75396b127e4f7e8c7fc3a2b6de08386c6f27
geekqq <jinwei1014@gmail.com>
2023-03-05 14:37:58 -0800

    find h index

---
+++ Day184/FindHIndex.java
@@ -0,0 +1,25 @@
+package Day184;
+
+public class FindHIndex {
+    public static void main(String[] args) {
+        int[] citations = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(findHIndex(citations));
+    }
+
+    public static int findHIndex(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) {
+            return nums[0] == 0 ? 0 : 1;
+        }
+        int left = 0;
+        int n = nums.length;
+        int right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == n - mid) return n - mid;
+            else if (nums[mid] < n - mid) left = mid + 1;
+            else right = mid - 1;
+        }
+        return n - left;
+    }
+}
