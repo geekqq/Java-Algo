d08b9aae5e731ebb6f51aafea949ea832040d948
geekqq <jinwei1014@gmail.com>
2023-03-14 15:41:35 -0700

    frequent sort tree

---
+++ Day191/SortCharactersByFrequency.java
@@ -0,0 +1,30 @@
+package Day191;
+
+import java.util.*;
+
+public class SortCharactersByFrequency {
+
+    public static void main(String[] args) {
+        System.out.println(frequentSort("tree"));
+    }
+
+    public static String frequentSort(String s) {
+        if (s == null || s.length() == 0) return null;
+        Map<Character, Integer> map = new HashMap<>();
+        for (Character c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        List<Character> list = new ArrayList<>(map.keySet());
+        Collections.sort(list, (c1, c2) -> map.get(c2) - map.get(c1));
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for (Character c : list) {
+            count = map.get(c);
+            for (int i = 0; i < count; i++) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
