1e6eae0ceb9ce83a94b2ab5bf56d828fc695837a
geekqq <jinwei1014@gmail.com>
2023-03-14 15:43:55 -0700

    find kth largest element

---
+++ Day191/FindKthLargest.java
@@ -0,0 +1,28 @@
+package Day191;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+import static Day138.RandomArray.randomArray;
+
+public class FindKthLargest {
+    public static void main(String[] args) {
+        int[] nums = randomArray(10, 10);
+        System.out.println(Arrays.toString(nums));
+        System.out.println(findKthLargest(nums, 5));
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < nums.length; i++) {
+            pq.add(nums[i]);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+}
