d2a1afe2909efb02dc8003f0a0858dff065469f0
geekqq <jinwei1014@gmail.com>
2023-03-16 15:45:02 -0700

    my queue

---
+++ Day193/MyQueue.java
@@ -0,0 +1,46 @@
+package Day193;
+
+import java.util.Stack;
+
+public class MyQueue<E> {
+    private Stack<E> stackIn;
+    private Stack<E> stackOut;
+
+    public MyQueue() {
+        stackIn = new Stack<E>();
+        stackOut = new Stack<E>();
+    }
+
+    public void offer(E val) {
+        while (!stackIn.isEmpty()) {
+            stackOut.push(stackIn.pop());
+        }
+        stackIn.push(val);
+        while (!stackOut.isEmpty()) {
+            stackIn.push(stackOut.pop());
+        }
+    }
+
+    public E poll() {
+        if (stackIn.isEmpty()) {
+            System.out.println("Underflow!");
+            System.exit(0);
+        }
+        return stackIn.pop();
+    }
+
+    public static void main(String[] args) {
+        MyQueue q = new MyQueue();
+        for (int i = 0; i < 10; i++) {
+            q.offer(i);
+        }
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+    }
+}
