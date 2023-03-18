345d8fb5cdb0f1f099ff1b92584f8dc46411e58e
geekqq <jinwei1014@gmail.com>
2023-03-18 09:24:30 -0700

    linked list reverse by range

---
+++ Day195/LinkedList.java
@@ -0,0 +1,163 @@
+package Day195;
+
+import Day174.ListNode;
+
+public class LinkedList {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        list.print();
+        list.head = swapInPairs(list.head);
+        list.print();
+        list.head = reverseByRange(list.head, 3,7);
+        list.print();
+        list.head = insertSortNode(list.head);
+        list.print();
+        list.head = reverseInKGroups(list.head,4);
+        list.print();
+    }
+
+    private ListNode head;
+    public void add(int val) {
+        ListNode newNode = new ListNode(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            ListNode temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    public void print() {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode prev = null;
+        ListNode cur = head;
+        ListNode next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+
+    public static ListNode insertSortNode(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(0);
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode prev = dummy;
+            while (prev.next != null && cur.val > prev.next.val) {
+                prev = prev.next;
+            }
+            ListNode insertNode = cur;
+            cur = cur.next;
+            insertNode.next = prev.next;
+            prev.next = insertNode;
+        }
+        return dummy.next;
+    }
+
+    public static ListNode swapInPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode postHead = swapInPairs(head.next.next);
+        head.next.next = head;
+        ListNode newHead = head.next;
+        head.next = postHead;
+        return newHead;
+    }
+
+    public static ListNode reverseByRange(ListNode head, int left, int right) {
+        if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        for (int i = 1; i < left; i++) {
+            prev = prev.next;
+        }
+        ListNode subStart = prev.next;
+        prev.next = null;
+        ListNode subEnd = subStart;
+        for (int i = left; i < right; i++) {
+            subEnd = subEnd.next;
+        }
+        ListNode next = subEnd.next;
+        subEnd.next = null;
+        prev.next = reverse(subStart);
+        subStart.next = next;
+        return dummy.next;
+    }
+
+    public static ListNode mergeSortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        ListNode left = mergeSortList(head);
+        ListNode right = mergeSortList(slow);
+        return mergeSort(left, right);
+    }
+
+    private static ListNode mergeSort(ListNode leftHead, ListNode rightHead) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (leftHead != null && rightHead != null) {
+            if (leftHead.val <= rightHead.val) {
+                cur.next = leftHead;
+                leftHead = leftHead.next;
+            } else {
+                cur.next = rightHead;
+                rightHead = rightHead.next;
+            }
+            cur = cur.next;
+            cur.next = leftHead != null ? leftHead : rightHead;
+        }
+        return dummy.next;
+    }
+
+    public static ListNode reverseInKGroups(ListNode head, int k) {
+        if (head == null || head.next == null) return head;
+        ListNode cur = head;
+        int count = 0;
+        while (cur != null) {
+            cur = cur.next;
+            count++;
+        }
+        if (count < k) {
+            return head;
+        }
+        ListNode prev = null;
+        cur = head;
+        ListNode next = null;
+        for (int i = 0; i < k; i++) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        head.next = reverseInKGroups(cur, k);
+        return prev;
+    }
+
+}
