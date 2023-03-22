72e0f6e53c341e14608f32d931409658d81d684f
geekqq <jinwei1014@gmail.com>
2023-03-22 11:26:38 -0700

    preorder traveral

---
+++ Day198/BinaryTree.java
@@ -0,0 +1,80 @@
+package Day198;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTree {
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        List left = preorderTraversal(root.left);
+        List right = preorderTraversal(root.right);
+        res.add((Integer) root.val);
+        res.addAll(left);
+        res.addAll(right);
+        return res;
+    }
+
+    public static List<Integer> preorderTraversalI(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add((Integer)node.val);
+            if (node.right != null) stack.push(node.right);
+            if (node.left != null) stack.push(node.left);
+        }
+        return res;
+    }
+
+    public static List<Integer> preorderTraversalII(TreeNode root) {
+        LinkedList<Integer> res = new LinkedList<>();
+        if (root == null) return res;
+        preorderTraversalII(root, res);
+        return res;
+    }
+
+    private static void preorderTraversalII(TreeNode root, LinkedList<Integer> res) {
+        if (root == null) return;
+        res.add((Integer) root.val);
+        preorderTraversalII(root.left, res);
+        preorderTraversalII(root.right, res);
+    }
+
+
+
+    public static void main(String[] args) {
+        TreeNode a = new TreeNode(4);
+        TreeNode b = new TreeNode(2);
+        TreeNode c = new TreeNode(6);
+        TreeNode d = new TreeNode(1);
+        TreeNode e = new TreeNode(3);
+        TreeNode f = new TreeNode(5);
+        TreeNode g = new TreeNode(7);
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        c.left = f;
+        c.right = g;
+        System.out.println(preorderTraversal(a));
+        System.out.println(preorderTraversalI(a));
+        System.out.println(preorderTraversalII(a));
+    }
+}
+
+class TreeNode<E> {
+    E val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(E val) {
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
