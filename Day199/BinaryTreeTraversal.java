458131c2649880853c7ab3ef5bd5744e6abb283b
geekqq <jinwei1014@gmail.com>
2023-03-23 17:58:07 -0700

    level order traversal

--- Day199/BinaryTreeTraversal.java
+++ Day199/BinaryTreeTraversal.java
@@ -2,10 +2,7 @@ package Day199;
 
 import com.sun.source.tree.Tree;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class BinaryTreeTraversal {
     public static List<Integer> preorderTraversal(TreeNode root) {
@@ -145,7 +142,106 @@ public class BinaryTreeTraversal {
         return res;
     }
 
+    public static int maxHeight(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxHeight(root.left), maxHeight(root.right)) + 1;
+    }
 
+    public static int maxHeightI(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> que = new LinkedList<>();
+        int height = 0;
+        que.offer(root);
+        while (!que.isEmpty()) {
+            int size = que.size();
+            while (size-- > 0) {
+                TreeNode node = que.poll();
+                if (node.left != null) que.offer(node.left);
+                if (node.right != null) que.offer(node.right);
+            }
+            height++;
+        }
+        return height;
+    }
+
+    public static List<Integer> leverOrderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> que = new LinkedList<>();
+        que.offer(root);
+        while (!que.isEmpty()) {
+            int size = que.size();
+            while (size-- > 0) {
+                TreeNode node = que.poll();
+                res.add((Integer) node.val);
+                if (node.left != null) que.offer(node.left);
+                if (node.right != null) que.offer(node.right);
+            }
+        }
+        return res;
+    }
+
+    public static List<Integer> levelOrderTraversalI(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> que = new LinkedList<>();
+        que.offer(root);
+        while (!que.isEmpty()) {
+            TreeNode node = que.poll();
+            res.add((Integer) node.val);
+            if (node.left != null) que.offer(node.left);
+            if (node.right != null) que.offer(node.right);
+        }
+        return res;
+    }
+
+    public static List<Integer> levelOrderTraversalII(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> currentLevel = new LinkedList<>();
+        Queue<TreeNode> nextLevel = new LinkedList<>();
+        currentLevel.offer(root);
+        while (!currentLevel.isEmpty()) {
+            TreeNode node = currentLevel.poll();
+            res.add((Integer) node.val);
+            if (node.left != null) {
+                nextLevel.offer(node.left);
+            }
+            if (node.right != null) {
+                nextLevel.offer(node.right);
+            }
+            if (currentLevel.isEmpty()) {
+                Queue<TreeNode> temp = currentLevel;
+                currentLevel = nextLevel;
+                nextLevel = temp;
+            }
+        }
+        return res;
+    }
+
+    //recursion way: get height of the tree, then print every level from left to right;
+    public static List<Integer> levelOrderTraversalIII(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        int height = getHeight(root);
+        for (int i = 1; i <= height; i++) {
+            printLevel(root, i, res);
+        }
+        return res;
+    }
+
+    private static int getHeight(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+    }
+
+    private static void printLevel(TreeNode node, int level, List<Integer> res) {
+        if (node == null) return;
+        if (level == 1) res.add((Integer) node.val);
+        else if (level > 1) {
+            printLevel(node.left, level - 1, res);
+            printLevel(node.right, level - 1, res);
+        }
+    }
     public static void main(String[] args) {
         TreeNode a = new TreeNode(4);
         TreeNode b = new TreeNode(2);
@@ -173,6 +269,15 @@ public class BinaryTreeTraversal {
         System.out.println(postorderTraversal(a));
         System.out.println(postorderTraversalI(a));
         System.out.println(postorderTraversalII(a));
+        System.out.println("----max height----");
+        System.out.println(maxHeight(a));
+        System.out.println(maxHeightI(a));
+        System.out.println("----level order traversal----");
+        System.out.println(leverOrderTraversal(a));
+        System.out.println(levelOrderTraversalI(a));
+        System.out.println(levelOrderTraversalII(a));
+        System.out.println(levelOrderTraversalIII(a));
+
     }
 }
 
