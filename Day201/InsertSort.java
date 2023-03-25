3a1d76d623c51d0cfe10d3c01e938da74d6d8558
geekqq <jinwei1014@gmail.com>
2023-03-25 11:50:40 -0700

    insert sort

---
+++ Day201/InsertSort.java
@@ -0,0 +1,27 @@
+package Day201;
+
+import java.util.Arrays;
+
+import static Day138.RandomArray.randomArray;
+
+public class InsertSort {
+
+    public static void insertSort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            int key = nums[i];
+            int j = i - 1;
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = randomArray(20, 10);
+        System.out.println(Arrays.toString(nums));
+        insertSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
