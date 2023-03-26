9b6916accb7724576ce8aa548fd500117f968c20
geekqq <jinwei1014@gmail.com>
2023-03-26 10:21:09 -0700

    has cycle

--- Day202/LinkedList.java
+++ Day202/LinkedList.java
@@ -277,6 +277,37 @@ public class LinkedList {
         newNode.next = head;
         head = newNode;
     }
+
+    public static boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null || fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {}
+            return true;
+        }
+        return false;
+    }
+
+    public static ListNode hasCycleII(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) break;
+        }
+        if (slow != fast) return null;
+        ListNode temp = head;
+        while (temp != slow) {
+            temp = temp.next;
+            slow = slow.next;
+        }
+        return temp;
+    }
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
         for (int i = 0; i < 10; i++) {
