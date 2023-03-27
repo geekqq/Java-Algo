159cb68e0cb8ed8b71b915f1402c6a8fd5796377
geekqq <jinwei1014@gmail.com>
2023-03-26 20:22:51 -0700

    search array

---
+++ Day202/SearchArray.java
@@ -0,0 +1,54 @@
+package Day202;
+
+import java.util.Arrays;
+
+public class SearchArray {
+
+    public static int[] searchArray(int[] nums, int x) {
+        int[] res = new int[2];
+        res[0] = findLeftIndex(nums, x);
+        res[1] = findRightIndex(nums, x);
+        return res;
+    }
+
+    private static int findLeftIndex(int[] nums, int x) {
+        int index = -1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= x) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+            if (nums[mid] == x) {
+                index = mid;
+            }
+        }
+        return index;
+    }
+
+    private static int findRightIndex(int[] nums, int x) {
+        int index = -1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left);
+            if (nums[mid] <= x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+            if (nums[mid] == x) {
+                index = mid;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,2,2,3,3,3,3,4,5,6,6,7};
+        System.out.println(Arrays.toString(searchArray(nums, 1)));
+    }
+}
