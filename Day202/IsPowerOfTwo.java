adc732699e831dd8087f102fc0641e8b7d549fed
geekqq <jinwei1014@gmail.com>
2023-03-26 17:30:11 -0700

    power of five

--- Day202/IsPowerOfTwo.java
+++ Day202/IsPowerOfTwo.java
@@ -16,9 +16,28 @@ public class IsPowerOfTwo {
         return powerOfThree(x / 3);
     }
 
+    public static boolean powerOfFour(int x) {
+        if (x <= 0) return false;
+        if (x == 1) return true;
+        if (x % 4 != 0) return false;
+        return powerOfFour(x / 4);
+    }
+
+    public static boolean powerOfFive(int x) {
+        if (x <= 0) return false;
+        if (x == 1) return true;
+        if (x % 5 != 0) return false;
+        return powerOfFive(x / 5);
+    }
     public static void main(String[] args) {
         for (int i = 0; i < 310; i++) {
             if (powerOfThree(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < 1000000; i++) {
+            if (powerOfFive(i)) {
                 System.out.print(i + " ");
             }
         }
