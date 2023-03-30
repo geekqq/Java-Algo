d2266f8d72a69629932259e7e35c7b5dd3f9ce98
geekqq <jinwei1014@gmail.com>
2023-03-29 22:30:29 -0700

    invert tree

--- Day204/BinaryTree.java
+++ Day204/BinaryTree.java
@@ -257,6 +257,29 @@ public class BinaryTree {
         if (leftNode.val != rightNode.val) return false;
         return isSymmetric(leftNode.left, rightNode.right) && isSymmetric(leftNode.right, rightNode.left);
     }
+
+
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) return root;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        return root;
+    }
+
+    public static TreeNode invertTreeI(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) return root;
+        TreeNode leftNode = invertTreeI(root.left);
+        TreeNode rightNode = invertTreeI(root.right);
+        root.left = rightNode;
+        root.right = leftNode;
+        return root;
+    }
     public static void main(String[] args) {
         TreeNode root = new TreeNode(4);
         root.left = new TreeNode(2);
@@ -299,6 +322,8 @@ public class BinaryTree {
         System.out.println("----check symmetric----");
         System.out.println(isSymmetric(root));
         System.out.println(isSymmetric(root1));
+        System.out.println("----invert tree----");
+        System.out.println(invertTreeI(root).left.val);
 
     }
 
