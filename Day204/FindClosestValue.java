1d3fd9a7d67359ad8a29a7a307fb283367af1e75
geekqq <jinwei1014@gmail.com>
2023-04-01 10:45:38 -0700

    closest value

--- Day204/FindClosestValue.java
+++ Day204/FindClosestValue.java
@@ -18,7 +18,20 @@ public class FindClosestValue {
         System.out.println(list.get(2));
         System.out.println(list.get(3));
         System.out.println(list.get(4));
-        System.out.println(findClosestValue(list, 2.5));
+        System.out.println("-------------------");
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        System.out.println(closestValue(root, 3.714286));
+        System.out.println(closestValueI(root, 3.714286));
+        System.out.println(closestValueII(root, 3.714286));
+        TreeNode root1 = new TreeNode(1);
+        System.out.println(closestValue(root1, 4.428571));
+        System.out.println(closestValueI(root1, 4.428571));
+        System.out.println(closestValueII(root1, 4.428571));
+
     }
 
     public static List<Integer> inOrderTraversal(TreeNode root) {
@@ -67,5 +80,30 @@ public class FindClosestValue {
             }
         }
         return closest;
+    }
+
+    //closestValueII 有问题，LC提交错误，不能输出正确答案，暂时搁置
+    static int res = Integer.MAX_VALUE;
+    public static int closestValueII(TreeNode root, double target) {
+        if (root == null) return -1;
+        dfs(root, target);
+        return res;
+    }
+
+    private static void dfs(TreeNode root, double target) {
+        if (root == null) return;
+        if (Math.abs(target - root.val) <= Math.abs(target - res)) {
+            if (Math.abs(target - root.val) == Math.abs(target - res)) {
+                res = Math.min(res, root.val);
+            } else {
+                res = root.val;
+            }
+        }
+        if (root.val == target) return;
+        if (root.val > target) {
+            dfs(root.left, target);
+        } else {
+            dfs(root.right, target);
+        }
     }
 }
---
+++ Day204/MinDistanceInBST.java
@@ -0,0 +1,52 @@
+package Day204;
+
+public class MinDistanceInBST {
+
+    public static int minDiffInBST(TreeNode root) {
+        Integer[] res = new Integer[2];
+        res[1] = Integer.MAX_VALUE;
+        inOrderTraversal(root, res);
+        return res[1];
+    }
+
+    private static void inOrderTraversal(TreeNode root, Integer[] res) {
+        if (root == null) return;
+
+        inOrderTraversal(root.left, res);
+
+        if (res[0] != null) {
+            res[1] = Math.min(res[1], root.val - res[0]);
+        }
+        res[0] = root.val;
+        inOrderTraversal(root.right, res);
+    }
+
+    int res = Integer.MAX_VALUE;
+    TreeNode prev = null;
+    public int minDiffInBSTI(TreeNode root) {
+        inOrderTraverse(root);
+        return res;
+    }
+
+    private void inOrderTraverse(TreeNode root) {
+        if (root == null) return;
+        inOrderTraverse(root.left);
+        if (prev != null) {
+            if (root.val - prev.val < res) {
+                res = root.val - prev.val;
+            }
+        }
+        prev = root;
+        inOrderTraverse(root.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(8);
+        root.left = new TreeNode(4);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(5);
+        root.right = new TreeNode(12);
+        System.out.println(minDiffInBST(root));
+        System.out.println(minDiffInBST(root));
+    }
+}
