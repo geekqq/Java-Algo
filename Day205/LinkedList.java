44138ae0728d85fd0b47d1b4b60b9877c3ed0671
geekqq <jinwei1014@gmail.com>
2023-04-02 15:56:39 -0700

    new change

--- Day205/LinkedList.java
+++ Day205/LinkedList.java
@@ -1,2 +1,172 @@
-package Day205;public class LinkedList {
+package Day205;
+
+import Day174.ListNode;
+
+public class LinkedList {
+    private ListNode head;
+
+    public void add(int val) {
+        ListNode newNode = new ListNode(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            ListNode cur = head;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+    }
+
+    public void print() {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode prev = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+
+    public static ListNode reverseByRange(ListNode head, int left, int right) {
+        if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        for (int i = 1; i < left; i++) {
+            prev = prev.next;
+        }
+        ListNode subStart = prev.next;
+        prev.next = null;
+        ListNode subEnd = subStart;
+        for (int i = left; i < right; i++) {
+            subEnd = subEnd.next;
+        }
+        ListNode next = subEnd.next;
+        subEnd.next = null;
+        prev.next = reverse(subStart);
+        subStart.next = next;
+        return dummy.next;
+    }
+
+    public static ListNode reverseByKGroup(ListNode head, int k) {
+        if (head == null || head.next == null) return head;
+        int count = 0;
+        ListNode cur = head;
+        while (cur != null && count < k) {
+            cur = cur.next;
+            count++;
+        }
+        if (count < k) {
+            return head;
+        }
+        ListNode prev = null;
+        cur = head;
+        for (int i = 0; i < k; i++) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        head.next = reverseByKGroup(cur, k);
+        return prev;
+    }
+
+    public static ListNode swapInPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode postHead = swapInPairs(head.next.next);
+        head.next.next = head;
+        ListNode newHead = head.next;
+        head.next = postHead;
+        return newHead;
+    }
+
+    public static ListNode partitionList(ListNode head, int x) {
+        if (head == null || head.next == null) return head;
+        ListNode small = new ListNode(0);
+        ListNode large = new ListNode(0);
+        ListNode curSmall = small;
+        ListNode curLarge = large;
+        while (head != null) {
+            if (head.val < x) {
+                curSmall.next = head;
+                curSmall = curSmall.next;
+            } else {
+                curLarge.next = head;
+                curLarge = curLarge.next;
+            }
+            head = head.next;
+        }
+        curSmall.next = large.next;
+        return small.next;
+    }
+
+    public static ListNode mergeSortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        ListNode left = mergeSortList(head);
+        ListNode right = mergeSortList(slow);
+        return mergeList(left, right);
+    }
+    private static ListNode mergeList(ListNode leftHead, ListNode rightHead) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (leftHead != null && rightHead != null) {
+            if (leftHead.val < rightHead.val) {
+                cur.next = leftHead;
+                leftHead = leftHead.next;
+            } else {
+                cur.next = rightHead;
+                rightHead = rightHead.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = leftHead != null ? leftHead : rightHead;
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        for (int i = 0; i < 12; i++) {
+            list.add(i);
+        }
+        list.print();
+        System.out.println("----reverse by range----");
+        list.head = reverseByRange(list.head,3,8);
+        list.print();
+        System.out.println("----reverse by k groups----");
+        list.head = reverseByKGroup(list.head, 5);
+        list.print();
+        System.out.println("----swap in pairs----");
+        list.head = swapInPairs(list.head);
+        list.print();
+        System.out.println("----partition list----");
+        list.head = partitionList(list.head, 7);
+        list.print();
+        System.out.println("----merge sort list----");
+        list.head = mergeSortList(list.head);
+        list.print();
+    }
 }
+
+
