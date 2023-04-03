919821660ffb3657f64e13cf24fa01489cdced39
geekqq <jinwei1014@gmail.com>
2023-04-03 11:49:19 -0700

    get lcp

---
+++ Day206/LongestCommonPrefix.java
@@ -0,0 +1,23 @@
+package Day206;
+
+public class LongestCommonPrefix {
+
+    public static String getLCP(String[] strs) {
+        if (strs == null || strs.length == 0) return null;
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return null;
+                }
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"leetcode", "leek", "leegleg", "leemons"};
+        System.out.println(getLCP(strs));
+    }
+}
