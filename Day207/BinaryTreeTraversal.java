a33fb54436677432c968cb039a0b37667288b347
geekqq <jinwei1014@gmail.com>
2023-04-05 13:52:52 -0700

    post order

--- Day207/BinaryTreeTraversal.java
+++ Day207/BinaryTreeTraversal.java
@@ -3,6 +3,7 @@ package Day207;
 import com.sun.source.tree.Tree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
@@ -64,7 +65,86 @@ public class BinaryTreeTraversal {
         return res;
     }
 
+    public static List<Integer> inOrder(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        List left = inOrder(root.left);
+        List right = inOrder(root.right);
+        res.addAll(left);
+        res.add(root.val);
+        res.addAll(right);
+        return res;
+    }
 
+    public static List<Integer> inOrderI(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        inOrderI(root, res);
+        return res;
+    }
+
+    private static void inOrderI(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        inOrderI(root.left, res);
+        res.add(root.val);
+        inOrderI(root.right, res);
+    }
+
+    public static List<Integer> postOrder(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        List left = postOrder(root.left);
+        List right = postOrder(root.right);
+        res.addAll(left);
+        res.addAll(right);
+        res.add(root.val);
+        return res;
+    }
+
+    public static List<Integer> postOrderI(TreeNode root) {
+        List <Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        postOrderI(root, res);
+        return res;
+    }
+
+    private static void postOrderI(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        postOrderI(root.left, res);
+        postOrderI(root.right, res);
+        res.add(root.val);
+    }
+
+    public static List<Integer> postOrderII(TreeNode root) {
+        LinkedList<Integer> res = new LinkedList<>();
+        if (root == null) return res;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.addFirst(node.val);
+            if (node.left != null) stack.push(node.left);
+            if (node.right != null) stack.push(node.right);
+        }
+        return res;
+    }
+    public static List<Integer> inOrderII(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                cur = stack.pop();
+                res.add(cur.val);
+                cur = cur.right;
+            }
+        }
+        return res;
+    }
     public static void main(String[] args) {
         TreeNode root = new TreeNode(4);
         root.left = new TreeNode(2);
@@ -78,6 +158,14 @@ public class BinaryTreeTraversal {
         System.out.println(preOrderI(root));
         System.out.println(preOrderII(root));
         System.out.println(preOrderIII(root));
+        System.out.println("----in order----");
+        System.out.println(inOrder(root));
+        System.out.println(inOrderI(root));
+        System.out.println(inOrderII(root));
+        System.out.println("----post order----");
+        System.out.println(postOrder(root));
+        System.out.println(postOrderI(root));
+        System.out.println(postOrderII(root));
     }
 }
 
