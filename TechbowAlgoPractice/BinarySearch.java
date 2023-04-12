8ada09a597a08fc8b2b11e2202a4d8b1b2650033
geekqq <jinwei1014@gmail.com>
2023-04-11 20:23:09 -0700

    binary search

--- TechbowAlgoPractice/BinarySearch.java
+++ TechbowAlgoPractice/BinarySearch.java
@@ -1,5 +1,9 @@
 package TechbowAlgoPractice;
 
+import java.util.Arrays;
+
+import static Day138.RandomArray.randomArray;
+
 public class BinarySearch {
     public static int binarySearch(int[] nums, int target) {
         if (nums == null || nums.length == 0) return -1;
@@ -15,5 +19,25 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+
+    //s3
+    public static int binarySearchI(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) left = mid;
+            else right = mid;
+        }
+        return nums[left] == target ? left : right;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(binarySearch(nums, 10));
+        System.out.println(binarySearchI(nums, 10));
     }
 }
