6085811dc9ec9a06a1834842b1edd39ef08afbd2
geekqq <jinwei1014@gmail.com>
2023-04-12 15:13:14 -0700

    post order

--- Day212/BinaryTree.java
+++ Day212/BinaryTree.java
@@ -3,6 +3,7 @@ package Day212;
 import com.sun.source.tree.Tree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
@@ -111,6 +112,44 @@ public class BinaryTree {
         return res;
     }
 
+    public static List<Integer> postOrder(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        List left = postOrder(root.left);
+        List right = postOrder(root.right);
+        res.addAll(left);
+        res.addAll(right);
+        res.add(root.val);
+        return res;
+    }
+
+    public static List<Integer> postOrderI(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        postOrderI(root, res);
+        return res;
+    }
+
+    private static void postOrderI(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        postOrderI(root.left, res);
+        postOrderI(root.right, res);
+        res.add(root.val);
+    }
+
+    public static List<Integer> postOrderII(TreeNode root) {
+        LinkedList<Integer> res = new LinkedList<>();
+        if (root == null) return res;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.addFirst(node.val);
+            if (node.left != null) stack.push(node.left);
+            if (node.right != null) stack.push(node.right);
+        }
+        return res;
+    }
     public static void main(String[] args) {
         TreeNode root = new TreeNode(4);
         root.left = new TreeNode(2);
@@ -128,6 +167,10 @@ public class BinaryTree {
         System.out.println(inOrder(root));
         System.out.println(inOrderI(root));
         System.out.println(inOrderII(root));
+        System.out.println("----post order----");
+        System.out.println(postOrder(root));
+        System.out.println(postOrderI(root));
+        System.out.println(postOrderII(root));
     }
 }
 
