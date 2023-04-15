6caf5dad4060eec7813b51f5f33b5d3f49ac9dcf
geekqq <jinwei1014@gmail.com>
2023-04-15 10:48:42 -0700

    validate BST

---
+++ Day213/ValidBST.java
@@ -0,0 +1,24 @@
+package Day213;
+
+public class ValidBST {
+
+    public static boolean validBST(TreeNode root) {
+        if (root == null || root.left == null && root.right == null) return true;
+
+        return isValidBST(root.left, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    private static boolean isValidBST(TreeNode root, long lower, long upper) {
+        if(root == null) return true;
+        if (root.val <= lower || root.val >= upper) return false;
+        return isValidBST(root.left, lower, root.val) && isValidBST(root.right, upper, root.val);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+        root.right.right = new TreeNode(4);
+        root.right.right.right = new TreeNode(5);
+        System.out.println(validBST(root));
+    }
+}
