7d4dc8a63114d62a5672ebeac761620590942a7c
geekqq <jinwei1014@gmail.com>
2023-04-16 17:13:02 -0700

    is valid parentheses

--- Day214/GenerateParentheses.java
+++ Day214/GenerateParentheses.java
@@ -2,6 +2,9 @@ package Day214;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
+
+import static Day152.ValidParentheses.MAP;
 
 public class GenerateParentheses {
     public static List<String> generateParentheses(int n) {
@@ -29,8 +32,36 @@ public class GenerateParentheses {
         }
     }
 
+    public static boolean isValidParentheses(String s) {
+        if (s == null || s.length() == 0) return false;
+        Stack<Character> stack = new Stack<>();
+        for (Character c : s.toCharArray()) {
+            if (MAP.containsKey(c)) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    Character open = stack.pop();
+                    if (MAP.get(open) != c) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
     public static void main(String[] args) {
         System.out.println(generateParentheses(3));
         System.out.println(generateParentheses(5));
+        List<String> res1 = generateParentheses(3);
+        List<String> res2= generateParentheses(5);
+        for (String s : res1) {
+            System.out.println(isValidParentheses(s));
+        }
+        for (String s : res2) {
+            System.out.println(isValidParentheses(s));
+        }
     }
 }
