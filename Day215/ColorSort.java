6b40cc3452ad42ccd5837c46b022c3083e20bc62
geekqq <jinwei1014@gmail.com>
2023-04-18 13:59:09 -0700

    color sort

---
+++ Day215/ColorSort.java
@@ -0,0 +1,29 @@
+package Day215;
+
+import java.util.Arrays;
+
+import static Day138.RandomArray.randomArray;
+import static Day44.SelectSort.swap;
+
+public class ColorSort {
+
+    public static void colorSort(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        int left = 0;
+        int right = nums.length - 1;
+        for (int i = 0; i <= right; i++) {
+            if (nums[i] == 0) {
+                swap(nums, left++, i);
+            } else if (nums[i] == 2) {
+                swap(nums, right--, i--);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = randomArray(3, 10);
+        System.out.println(Arrays.toString(nums));
+        colorSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
