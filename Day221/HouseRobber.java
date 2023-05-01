ae480ab8d5194957faf9434a5a1108a72020b1c2
geekqq <jinwei1014@gmail.com>
2023-04-30 21:18:13 -0700

    rob house

---
+++ Day221/HouseRobber.java
@@ -0,0 +1,16 @@
+package Day221;
+
+
+public class HouseRobber {
+    public static int rob(TreeNode root) {
+        if (root == null) return 0;
+        int val = 0;
+        if (root.left != null) {
+            val += rob(root.left.left) + rob(root.left.right);
+        }
+        if (root.right != null) {
+            val += rob(root.right.left) + rob(root.right.right);
+        }
+        return Math.max(val + root.val, rob(root.left) + rob(root.right));
+    }
+}
---
+++ Day221/TreeNode.java
@@ -0,0 +1,13 @@
+package Day221;
+
+public class TreeNode {
+    int val;
+    TreeNode right;
+    TreeNode left;
+
+    public TreeNode(int val) {
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
