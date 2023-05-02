ded92260b3589a656a87514c1125dd927a5b6e5d
geekqq <jinwei1014@gmail.com>
2023-05-02 13:31:59 -0700

    find all anagrams

--- Day223/FindAllAnagrams.java
+++ Day223/FindAllAnagrams.java
@@ -1,2 +1,36 @@
-package Day223;public class FindAllAnagrams {
+package Day223;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindAllAnagrams {
+
+    public static List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+        if (s == null || s.length() == 0 || p == null || p.length() > s.length()) {
+            return res;
+        }
+        int m = s.length();
+        int n = p.length();
+        int[] sc = new int[26];
+        int[] pc = new int[26];
+
+        for (int i = 0; i < n; i++) {
+            char c = p.charAt(i);
+            pc[c - 'a']++;
+        }
+
+        for (int i = 0; i < m; i++) {
+            sc[s.charAt(i) - 'a']++;
+            if (i >= n) sc[s.charAt(i - n) - 'a']--;
+            if (Arrays.equals(sc, pc)) res.add(i - n + 1);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findAnagrams("cbaebabacd", "abc"));
+        System.out.println(findAnagrams("abab", "ba"));
+    }
 }
