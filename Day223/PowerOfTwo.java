37e1a47083e8b44a5adf4d00caacf111e4cf91a2
geekqq <jinwei1014@gmail.com>
2023-05-02 22:23:45 -0700

    is power of two

---
+++ Day223/PowerOfTwo.java
@@ -0,0 +1,19 @@
+package Day223;
+
+public class PowerOfTwo {
+
+    public static boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        if (n == 1) return true;
+        if (n % 2 != 0) return false;
+        return isPowerOfTwo(n / 2);
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100000; i++) {
+            if (isPowerOfTwo(i)) {
+                System.out.print(i + "," );
+            }
+        }
+    }
+}
