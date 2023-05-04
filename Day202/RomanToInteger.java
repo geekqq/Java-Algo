1cec2ded779c5df4e80fb560a82690958b6567d8
geekqq <jinwei1014@gmail.com>
2023-05-04 14:39:14 -0700

    add integer to roman

--- Day202/RomanToInteger.java
+++ Day202/RomanToInteger.java
@@ -48,5 +48,6 @@ public class RomanToInteger {
     public static void main(String[] args) {
         System.out.println(romanToInt("CMXCIV"));
         System.out.println(romanToIntII("CMXCIV"));
+        System.out.println(romanToInt("XCIX"));
     }
 }
--- Day224/IntegerToRoman.java
+++ Day224/IntegerToRoman.java
@@ -1,2 +1,24 @@
-package Day224;public class IntegerToRoman {
+package Day224;
+
+public class IntegerToRoman {
+    public static String intToRoman(int num) {
+        int[] vals = {100, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < vals.length; i++) {
+            while (num >= vals[i]) {
+                num -= vals[i];
+                sb.append(romans[i]);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.print(intToRoman(i) + ",");
+        }
+    }
 }
