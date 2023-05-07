0ef6ea5c4a5bd74ff6e1c14e36e93f25141a2256
geekqq <jinwei1014@gmail.com>
2023-05-06 23:00:44 -0700

    count of primes

---
+++ Day225/CountPrimes.java
@@ -0,0 +1,31 @@
+package Day225;
+
+import java.util.Arrays;
+
+public class CountPrimes {
+    public static int countPrimes(int n) {
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countPrimes(2));
+        System.out.println(countPrimes(10));
+        System.out.println(countPrimes(100));
+        System.out.println(countPrimes(10000));
+    }
+}
