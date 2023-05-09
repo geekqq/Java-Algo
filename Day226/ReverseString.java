5cb30179be677041ab174aca0c6e42d095f4d05b
geekqq <jinwei1014@gmail.com>
2023-05-09 13:19:45 -0700

    reverse a string

---
+++ Day226/ReverseString.java
@@ -0,0 +1,25 @@
+package Day226;
+
+public class ReverseString {
+    public static String reverseString(String s) {
+        if (s == null || s.length() == 0) return null;
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            swap(chars, left++, right--);
+        }
+        return new String(chars);
+    }
+    private static void swap(char[] chars, int left, int right) {
+        //char[] chars = s.toCharArray();
+        char temp = chars[left];
+        chars[left] = chars[right];
+        chars[right] = temp;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(reverseString("hello"));
+    }
+}
