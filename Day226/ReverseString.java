346d634e459b1b63c006bf1daa62fabafe41ae3c
geekqq <jinwei1014@gmail.com>
2023-05-09 15:02:00 -0700

    reverse string

--- Day226/ReverseString.java
+++ Day226/ReverseString.java
@@ -18,8 +18,18 @@ public class ReverseString {
         chars[right] = temp;
     }
 
+    public static String reverseStringI(String s) {
+        if (s == null || s.length() == 0) return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
 
         System.out.println(reverseString("hello"));
+        System.out.println(reverseStringI("hello"));
     }
 }
