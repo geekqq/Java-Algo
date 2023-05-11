25aeae3acb50a06980187eefb2951a1f10f3cc68
geekqq <jinwei1014@gmail.com>
2023-05-11 11:41:59 -0700

    regexmatch

---
+++ Day228/RegExMatch.java
@@ -0,0 +1,32 @@
+package Day228;
+
+public class RegExMatch {
+
+    public static boolean isMatch(String s, String p) {
+        if (s == null && p == null) return true;
+        if (s == null || p == null) return false;
+        if (p.length() == 0) return s.length() == 0;
+
+        if (p.length() > 1 && p.charAt(1) == '*') {
+            if (isMatch(s, p.substring(2))) {
+                return true;
+            } else if (s.length() > 0 && (p.charAt(0) == '.' || s.charAt(0) == p.charAt(0))) {
+                return isMatch(s.substring(1), p);
+            } else {
+                return false;
+            }
+        } else {
+            if (s.length() > 0 && (p.charAt(0) == '.' || s.charAt(0) == p.charAt(0))) {
+                return isMatch(s.substring(1), p.substring(1));
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isMatch("aa", "a"));
+        System.out.println(isMatch("aa", "a*"));
+        System.out.println(isMatch("aa", ".*"));
+    }
+}
