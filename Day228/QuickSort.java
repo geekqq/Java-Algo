8e1703c71d078328d2fc73bf58a2c95df343dc98
geekqq <jinwei1014@gmail.com>
2023-05-12 10:42:52 -0700

    quick sort

---
+++ Day228/QuickSort.java
@@ -0,0 +1,36 @@
+package Day228;
+
+import java.util.Arrays;
+
+import static Day138.RandomArray.randomArray;
+import static Day37.SortedArray.swap;
+
+public class QuickSort {
+
+    public static int partition(int[] nums, int low, int high) {
+        int pivot = nums[high];
+        int i = low - 1;
+        for (int j = low; j < nums.length - 1; j++) {
+            if (nums[j] < pivot) {
+                i++;
+                swap(nums, i, j);
+            }
+        }
+        swap(nums, i + 1, high);
+        return i + 1;
+    }
+
+    private static void sort(int[] nums, int low, int high) {
+        if (low < high) {
+            int pi = partition(nums, low, high);
+            sort(nums, low, pi - 1);
+            sort(nums, pi + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = randomArray(15, 10);
+        sort(nums, 0, nums.length - 1);
+        System.out.println(Arrays.toString(nums));
+    }
+}
