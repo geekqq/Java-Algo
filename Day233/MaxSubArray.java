0d4d89906f35ed3021639a535efd3aeb2120f928
geekqq <jinwei1014@gmail.com>
2023-05-31 11:36:03 -0700

    dp max sum of subarray

--- Day233/MaxSubArray.java
+++ Day233/MaxSubArray.java
@@ -1,5 +1,7 @@
 package Day233;
 
+import java.util.Arrays;
+
 public class MaxSubArray {
 
     //LC53
@@ -20,9 +22,26 @@ public class MaxSubArray {
         return max;
     }
 
+    public static int maxSumSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+
+        int len = nums.length;
+        int[] dp = new int[len];
+
+        dp[0] = nums[0];
+        int max = dp[0];
+        for (int i = 1; i < len; i++) {
+            dp[i] = Math.max(nums[i] + dp[i - 1], nums[i]);
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         System.out.println(maxSubArray(nums));
+        System.out.println(maxSumSubArray(nums));
 
     }
 }
