456eec83a368206e17568a13ef9ebfcd120a2ecb
geekqq <jinwei1014@gmail.com>
2023-06-01 11:08:13 -0700

    find the max sum of subarray

---
+++ Day234/MaxSubArray.java
@@ -0,0 +1,24 @@
+package Day234;
+
+public class MaxSubArray {
+
+    public static int maxSumSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSumSubArray(nums));
+    }
+}
