a5973476681adb2305fd0d16aaf7481ae5115067
geekqq <jinwei1014@gmail.com>
2023-06-05 22:55:29 -0700

    get the length of the longest substring without repearing characters

---
+++ Day238/LongestSubstringWithoutDuplicates.java
@@ -0,0 +1,30 @@
+package Day238;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstringWithoutDuplicates {
+
+    public static int getLength(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int left = 0;
+        int right = 0;
+        int maxLen = 0;
+        Set<Character> set = new HashSet<>();
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            if (set.add(c)) {
+                maxLen = Math.max(maxLen, set.size());
+                right++;
+            } else {
+                set.remove(s.charAt(left));
+                left++;
+            }
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getLength("jinweihustonehoo"));
+    }
+}
