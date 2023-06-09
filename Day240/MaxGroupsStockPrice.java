1c53ec4ce6b9edc8ca8162673bc046cd15001fb6
geekqq <jinwei1014@gmail.com>
2023-06-09 14:58:39 -0700

    max group of stock price

---
+++ Day240/MaxGroupsStockPrice.java
@@ -0,0 +1,32 @@
+package Day240;
+
+import java.util.Stack;
+
+public class MaxGroupsStockPrice {
+
+    public static int getMaxLength(int[] stockPrice) {
+        if (stockPrice == null || stockPrice.length == 0) return -1;
+        int res = 0;
+        int max = stockPrice[0];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(stockPrice[0]);
+        for (int i = 1; i < stockPrice.length; i++) {
+            if (max <= stockPrice[i]) {
+                res += i;
+                max = stockPrice[i];
+            } else {
+                res += stack.size();
+            }
+            while (!stack.isEmpty() && stack.peek() < stockPrice[i]) {
+                stack.pop();
+            }
+            stack.push(stockPrice[i]);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] stockPrice = {7,2,6,4,8,3};
+        System.out.println(getMaxLength(stockPrice));
+    }
+}
