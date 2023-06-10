1995d54eb3e93c65a39407a71c2cba5fe07cbe3a
geekqq <jinwei1014@gmail.com>
2023-06-10 10:13:57 -0700

    get diffrent formatted currency for different country

---
+++ Day240/GerCurrency.java
@@ -0,0 +1,32 @@
+package Day240;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class GerCurrency {
+
+    public static void main(String[] args) {
+        double amount = 12345.67;
+        Locale locale = new Locale("en", "US");
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(locale);
+
+        String formattedAmount = currencyFormatter.format(amount);
+        System.out.println("US Currency: " + formattedAmount);
+
+        Locale locale1 = new Locale("fr", "FR");
+        NumberFormat currencyFormatter1 = NumberFormat.getCurrencyInstance(locale1);
+
+        String formattedAmountFr = currencyFormatter1.format(amount);
+        System.out.println("France Currency: " + formattedAmountFr);
+
+        Locale localIndia = new Locale("en", "IN");
+        NumberFormat currencyFormatterIn = NumberFormat.getCurrencyInstance(localIndia);
+        String formattedAmountIn = currencyFormatterIn.format(amount);
+        System.out.println("INDIA Currency: " + formattedAmountIn);
+
+        Locale localChina = new Locale("zh", "CN");
+        NumberFormat currencyFormatterCn = NumberFormat.getCurrencyInstance(localChina);
+        String formattedAmountCn = currencyFormatterCn.format(amount);
+        System.out.println("CHINA Currency: " + formattedAmountCn);
+    }
+}
