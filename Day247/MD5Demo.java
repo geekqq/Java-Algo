81e8f4982a9fc34d2e6ffedf81855d496d4d1ba5
geekqq <jinwei1014@gmail.com>
2023-06-19 22:11:17 -0700

    demo of MD5 algrithom

---
+++ Day247/MD5Demo.java
@@ -0,0 +1,20 @@
+package Day247;
+
+import java.security.MessageDigest;
+import java.util.Scanner;
+
+public class MD5Demo {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        byte[] hash = md5.digest(s.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hash) {
+            sb.append(String.format("%02x", b));
+        }
+        System.out.println(sb.toString());
+    }
+}
