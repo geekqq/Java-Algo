90bb1ebf2258301a6df7e7b0f663bcd7b4e048bf
geekqq <jinwei1014@gmail.com>
2023-06-23 13:34:43 -0700

    duo null check singleton class

--- Day250/Singleton.java
+++ Day250/Singleton.java
@@ -1,2 +1,16 @@
-package Day250;public class Singleton {
+package Day250;
+
+public class Singleton {
+    private Singleton instance = null;
+    private Singleton() {};
+    public Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
 }
