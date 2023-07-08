a3ae4fd759f42f14a54268bc3ab60c5035ab1fb3
geekqq <jinwei1014@gmail.com>
2023-07-07 22:18:34 -0700

    arr with no duplicates

---
+++ Day255/GenerateRandomArray.java
@@ -0,0 +1,26 @@
+package Day255;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class GenerateRandomArray {
+    public static int[] generateArray(int size, int min, int max) {
+        if (max - min + 1 < size) throw new IllegalArgumentException();
+        int[] arr = new int[size];
+        boolean[] used = new boolean[max - min + 1];
+        int index = 0;
+        Random rd = new Random();
+        while (index < size) {
+            int num = rd.nextInt(max - min + 1) + min;
+            if (!used[num - min]) {
+                arr[index++] = num;
+                used[num - min] = true;
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(generateArray(5, 10, 16)));
+    }
+}
