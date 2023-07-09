558ae5e090a163765b7fca216d816736312fce61
geekqq <jinwei1014@gmail.com>
2023-07-09 09:43:09 -0700

    average number

---
+++ Day256/AverageNumber.java
@@ -0,0 +1,12 @@
+package Day256;
+
+public class AverageNumber {
+
+    public static double average(int a, int b,int c) {
+        return (double) (a + b + c) / 3;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(average(1,2,3));
+    }
+}
