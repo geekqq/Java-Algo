33b821112245c60132fede59201ce35a7c7cf7a7
geekqq <jinwei1014@gmail.com>
2023-07-14 13:35:58 -0700

    quick sort

---
+++ Day260/GenerateRandomArrayWithoutDuplicate.java
@@ -0,0 +1,54 @@
+package Day260;
+
+import java.util.Random;
+
+import static Day256.PrintArray.printArray;
+import static Day37.SortedArray.swap;
+
+public class GenerateRandomArrayWithoutDuplicate {
+
+    public static int[] generateArray(int size, int min, int max) {
+        if (max - min + 1 < size) {
+            throw new IllegalArgumentException();
+        }
+        int[] arr = new int[size];
+        boolean[] used = new boolean[max - min + 1];
+        int index = 0;
+        Random rd = new Random();
+        while (index < size) {
+            int num = rd.nextInt(max - min + 1) + min;
+            if (!used[num - min]) {
+                arr[index++] = num;
+                used[num - min] = true;
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = generateArray(10, 2, 22);
+        printArray(nums);
+        sort(nums, 0, nums.length - 1);
+        printArray(nums);
+    }
+
+    public static int partition(int[] nums, int low, int high) {
+        int i = low - 1;
+        int pivot = nums[high];
+        for (int j = low; j < nums.length - 1; j++) {
+            if (nums[j] < pivot) {
+                i++;
+                swap(nums, i, j);
+            }
+        }
+        swap(nums, i + 1, high);
+        return i + 1;
+    }
+    private static void sort(int[] nums, int low, int high) {
+        if (low < high) {
+            int pi = partition(nums, low, high);
+            sort(nums, low, pi - 1);
+            sort(nums, pi + 1, high);
+        }
+    }
+}
