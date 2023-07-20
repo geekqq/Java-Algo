dad74b2901680e9501dcffc415358f20d544aa4a
geekqq <jinwei1014@gmail.com>
2023-07-19 23:03:41 -0700

    listNode demo delete node in pos

--- Day261/LinkedListDemo.java
+++ Day261/LinkedListDemo.java
@@ -53,14 +53,50 @@ public class LinkedListDemo {
         return cur;
     }
 
+    public int countNodes() {
+        ListNode cur = head;
+        int count = 1;
+        while (cur.next != null) {
+            cur = cur.next;
+            count++;
+        }
+        return count;
+    }
+
+    public void deleteNode(int pos) {
+        if (pos > countNodes()) {
+            throw new IllegalArgumentException("position out of bounds");
+        }
+        if (pos == 1) head = head.next;
+        else {
+            ListNode prev = head;
+            int count = 1;
+            while (count < pos - 1) {
+                prev = prev.next;
+                count++;
+            }
+            ListNode cur = prev.next;
+            prev.next = cur.next;
+        }
+    }
+
     public static void main(String[] args) {
         LinkedListDemo list  =new LinkedListDemo();
         list.add(2);
         list.add(5);
         list.add(1);
         list.add(3);
+        list.add(10);
+        list.add(4);
+        list.add(7);
+        list.add(9);
         list.printList();
+        System.out.println(list.countNodes());
         list.deleteLast();
+        list.deleteLast();
+        list.printList();
+        System.out.println(list.countNodes());
+        list.deleteNode(2);
         list.printList();
     }
 }
