d9e63fd4a60593816e10549b5c713dc4527a2ddc
geekqq <jinwei1014@gmail.com>
2023-07-23 22:26:14 -0700

    add two numbers

--- Day261/MergeLinkedList.java
+++ Day261/MergeLinkedList.java
@@ -53,6 +53,28 @@ public class MergeLinkedList {
         return dummy.next;
     }
 
+    public static ListNode addTwoNumbers(ListNode head1, ListNode head2) {
+        if (head1 == null ) return head2;
+        if (head2 == null) return head1;
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        int carry = 0;
+        while (head1 != null || head2 != null) {
+            int x = head1 == null ? 0 : head1.val;
+            int y = head2 == null ? 0 : head2.val;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            if (head1 != null) head1 = head1.next;
+            if (head2 != null) head2 = head2.next;
+        }
+        if (carry > 0) {
+            cur.next = new ListNode(carry);
+        }
+        return dummy.next;
+    }
+
 
     public static void main(String[] args) {
         MergeLinkedList list1 = new MergeLinkedList();
@@ -61,7 +83,7 @@ public class MergeLinkedList {
         list1.add(2);
         list1.add(4);
         list1.add(6);
-        list1.add(10);
+        list1.add(9);
         list2.add(1);
         list2.add(3);
         list2.add(5);
@@ -71,6 +93,12 @@ public class MergeLinkedList {
         list2.print();
         result.head = mergeLinkedList(list1.head, list2.head);
         result.print();
+        MergeLinkedList sum = new MergeLinkedList();
+        System.out.println("-----------add two numbers-----------------");
+        list1.print();
+        list2.print();
+        sum.head = addTwoNumbers(list1.head, list2.head);
+        sum.print();
     }
 }
 
