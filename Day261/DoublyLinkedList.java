a20afd85e05ca52bf414e1f02a3c4d6ed8597ad6
geekqq <jinwei1014@gmail.com>
2023-07-23 23:29:19 -0700

    doubly linked list

--- Day261/DoublyLinkedList.java
+++ Day261/DoublyLinkedList.java
@@ -29,4 +29,48 @@ public class DoublyLinkedList {
     public int length() {
         return length;
     }
+
+    public void addLast(int val) {
+        ListNode newNode = new ListNode(val);
+        if (isEmpty()) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+
+        }
+        newNode.prev = tail;
+        tail = newNode;
+        length++;
+    }
+
+    public void printForward() {
+        if (head == null) return;
+        ListNode cur = head;
+        while (cur != null) {
+            System.out.print(cur.val + " --> ");
+            cur = cur.next;
+        }
+        System.out.println("null\n");
+    }
+
+    public void printBackward() {
+        if (tail == null) return;
+        ListNode cur = tail;
+        while (cur != null) {
+            System.out.print(cur.val + " --> ");
+            cur = cur.prev;
+        }
+        System.out.println("null\n");
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.addLast(4);
+        dll.addLast(6);
+        dll.addLast(2);
+        dll.addLast(8);
+        dll.addLast(5);
+        dll.printForward();
+        dll.printBackward();
+    }
 }
