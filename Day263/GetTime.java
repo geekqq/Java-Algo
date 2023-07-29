be18f2f5a1b7e16f781c920d1ae38748f1d68245
geekqq <jinwei1014@gmail.com>
2023-07-29 00:06:09 -0700

    local date and time demo

--- Day263/GetTime.java
+++ Day263/GetTime.java
@@ -1,9 +1,14 @@
 package Day263;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class GetTime {
     public static void main(String[] args) {
         System.out.println(Instant.now());
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now= LocalDateTime.now();
+        System.out.println(dtf.format(now));
     }
 }
