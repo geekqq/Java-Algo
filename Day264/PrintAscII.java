4a4041f9cd17dc159253dd42d779184e63124352
geekqq <jinwei1014@gmail.com>
2023-08-01 13:25:15 -0700

    print ascii table

---
+++ Day264/PrintAscII.java
@@ -0,0 +1,19 @@
+package Day264;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintAscII {
+
+    public static void main(String[] args) {
+        List<Character> characterList = new ArrayList<>();
+        Character c = ' ';
+        for (int i = 32; i < 128; i++) {
+            characterList.add(c);
+            c++;
+        }
+        for (Character x : characterList) {
+            System.out.print(x);
+        }
+    }
+}
