2432add8ee99f51d8297e0595cf7789c26afd11e
geekqq <jinwei1014@gmail.com>
2023-08-02 11:56:21 -0700

    is valid parentheses

---
+++ Day265/ValidParentheses.java
@@ -0,0 +1,33 @@
+package Day265;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean isValidParentheses(String s) {
+        if (s == null || s.length() == 0) {
+            throw new IllegalArgumentException();
+        }
+        Stack<Character> stack = new Stack<>();
+        for (Character c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                else {
+                    Character top = stack.peek();
+                    if (top == '(' && c == ')'
+                    || top == '[' && c == ']'
+                    || top == '{' && c == '}') {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValidParentheses("[]{}())"));
+    }
+}
