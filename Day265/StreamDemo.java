9c2c2080422791e966f20905f9f66ed6589d78ce
geekqq <jinwei1014@gmail.com>
2023-08-02 22:14:52 -0700

    anyMatch, noneMatch, allMatch demo in stream

--- Day265/StreamDemo.java
+++ Day265/StreamDemo.java
@@ -1,7 +1,6 @@
 package Day265;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class StreamDemo {
@@ -11,5 +10,30 @@ public class StreamDemo {
         List<Integer> integers = list.stream().sorted().collect(Collectors.toList());
         System.out.println(integers);
         list.stream().sorted().forEach(n -> System.out.print(n + " "));
+        System.out.println();
+        list.stream().sorted(Comparator.reverseOrder()).forEach(n -> System.out.print(n + " "));
+
+        System.out.println("============================");
+        List<String> list1 = Arrays.asList("Stone", "John", "Gary", "Ming", "Hao");
+        list1.stream().sorted().forEach(n -> System.out.print(n + " "));
+        System.out.println();
+        list1.stream().sorted(Comparator.reverseOrder()).forEach(n -> System.out.print(n + " "));
+
+        System.out.println("============================");
+        Set<String> fruits = new HashSet<>();
+        fruits.add("one apple");
+        fruits.add("one banana");
+        fruits.add("one apple");
+        fruits.add("two grapes");
+        fruits.add("more guavas");
+
+        boolean one = fruits.stream().anyMatch(value -> value.startsWith("one"));
+        System.out.println(one);
+
+        boolean one1 = fruits.stream().allMatch(value -> value.startsWith("one"));
+        System.out.println(one1);
+
+        boolean one2 = fruits.stream().noneMatch(value -> value.startsWith("n"));
+        System.out.println(one2);
     }
 }
