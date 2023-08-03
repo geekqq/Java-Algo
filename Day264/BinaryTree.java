abe43ef0da7341dab02bf89643f35a5868392953
geekqq <jinwei1014@gmail.com>
2023-08-03 12:09:34 -0700

    post order traversal

--- Day264/BinaryTree.java
+++ Day264/BinaryTree.java
@@ -1,6 +1,7 @@
 package Day264;
 
 import java.util.*;
+import java.util.LinkedList;
 
 public class BinaryTree {
     private TreeNode root;
@@ -43,6 +44,7 @@ public class BinaryTree {
 
     public static void levelOrder(TreeNode root) {
         if (root == null) return;
+
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
         while (!q.isEmpty()) {
@@ -51,6 +53,75 @@ public class BinaryTree {
             if (node.left != null) q.offer(node.left);
             if (node.right != null) q.offer(node.right);
         }
+    }
+
+    public static void inOrder(TreeNode root) {
+        if (root == null) return;
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    public static void inOrderUsingStack(TreeNode root) {
+        if (root == null) return;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (!stack.isEmpty() || cur != null) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                cur = stack.pop();
+                System.out.print(cur.data + " ");
+                cur = cur.right;
+            }
+        }
+    }
+
+    public static void postOrder(TreeNode root) {
+        if (root == null) return;
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void postOrderUsingStack(TreeNode root) {
+        if (root == null) return;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                TreeNode temp = stack.peek().right;
+                if (temp == null) {
+                    temp = stack.pop();
+                    System.out.print(temp.data + " ");
+                    while (!stack.isEmpty() && temp == stack.peek().right) {
+                        temp = stack.pop();
+                        System.out.print(temp.data + " ");
+                    }
+                } else {
+                    cur = temp;
+                }
+
+            }
+        }
+    }
+
+    public static List<Integer> postOrderStack(TreeNode root) {
+        if (root == null) return null;
+        LinkedList<Integer> res = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            res.addFirst(temp.data);
+            if (temp.left != null) stack.push(temp.left);
+            if (temp.right != null) stack.push(temp.right);
+        }
+        return res;
     }
     public void createBinaryTree() {
         TreeNode first = new TreeNode(1);
@@ -74,6 +145,17 @@ public class BinaryTree {
         preOrderI(bt.root);
         System.out.println();
         levelOrder(bt.root);
-
+        System.out.println();
+        System.out.println("===in order===");
+        inOrder(bt.root);
+        System.out.println();
+        inOrderUsingStack(bt.root);
+        System.out.println();
+        System.out.println("==== post order====");
+        postOrder(bt.root);
+        System.out.println();
+        postOrderUsingStack(bt.root);
+        System.out.println();
+        System.out.println(postOrderStack(bt.root));
     }
 }
