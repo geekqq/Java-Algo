69c1b18fef951aee4f4b35e94ad804164d229d36
geekqq <jinwei1014@gmail.com>
2023-08-05 22:15:25 -0700

    max heap insert node:

--- Day266/MaxPriorityQueue.java
+++ Day266/MaxPriorityQueue.java
@@ -3,7 +3,7 @@ package Day266;
 public class MaxPriorityQueue {
 
     Integer[] heap;
-    int n;
+    int n;  //size of max heap
 
     public MaxPriorityQueue(int capacity) {
         heap = new Integer[capacity + 1];
@@ -18,9 +18,54 @@ public class MaxPriorityQueue {
         return n;
     }
 
+    public void insert(int x) {
+        if (n == heap.length - 1) {
+            resize(2 * heap.length);
+        }
+        n++;
+        heap[n] = x;
+        swim(n);
+    }
+
+    private void swim(int k) {
+        while (k > 1 && heap[k / 2] < heap[k]) {
+            swap(heap, k / 2, k);
+            k = k / 2;
+        }
+    }
+
+    private void swap(Integer[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void resize(int capacity) {
+        Integer[] temp = new Integer[capacity];
+        for (int i = 0; i < heap.length; i++) {
+            temp[i] = heap[i];
+        }
+        heap = temp;
+    }
+
+    public void printMaxHeap() {
+        for (int i = 1; i <= n; i++) {
+            System.out.print(heap[i] + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         MaxPriorityQueue pq = new MaxPriorityQueue(3);
         System.out.println(pq.size());
         System.out.println(pq.isEmpty());
+        pq.insert(4);
+        pq.insert(1);
+        pq.insert(3);
+        pq.insert(8);
+        pq.insert(5);
+        pq.insert(9);
+        System.out.println(pq.size());
+        pq.printMaxHeap();
     }
 }
