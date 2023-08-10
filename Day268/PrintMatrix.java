5e24f568b21de2cc542b669ae4f7c52873caa578
geekqq <jinwei1014@gmail.com>
2023-08-09 22:36:35 -0700

    counter clock wise print a matrix

--- Day268/PrintMatrix.java
+++ Day268/PrintMatrix.java
@@ -55,5 +55,43 @@ public class PrintMatrix {
         printMatrix(matrix);
         printMatrix(matrix1);
         printMatrix(matrix2);
+        printMatrixCounterClock(matrix);
+        printMatrixCounterClock(matrix1);
+        printMatrixCounterClock(matrix2);
     }
+
+    public static void printMatrixCounterClock(int[][] matrix) {
+        //cc
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int k = 0;
+        int l = 0;
+        while (k < row && l < col) {
+            for (int i = k; i < row; i++) {
+                System.out.print(matrix[i][l] + " ");
+            }
+            l++;
+            for (int i = l; i < col; i++) {
+                System.out.print(matrix[row - 1][i] + " ");
+            }
+            row--;
+
+            if (l < col) {
+                for (int i = row - 1; i >= k; i--) {
+                    System.out.print(matrix[i][col - 1] + " ");
+                }
+                col--;
+            }
+            if (k < row) {
+                for (int i = col - 1; i >= l ; i--) {
+                    System.out.print(matrix[k][i] + " ");
+                }
+                k++;
+            }
+        }
+        System.out.println();
+    }
+
+
 }
