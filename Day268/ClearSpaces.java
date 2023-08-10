8067e53d307e2bb1be43cad70d45a42c49b05468
geekqq <jinwei1014@gmail.com>
2023-08-10 12:01:30 -0700

    clear spaces

---
+++ Day268/ClearSpaces.java
@@ -0,0 +1,22 @@
+package Day268;
+
+public class ClearSpaces {
+
+    public static String clearSpaces(String s) {
+        int i = 0;
+        int j = 0;
+        int n = s.length();
+        char[] chars = s.toCharArray();
+        while (j < n) {
+            while (j < n && chars[j] == ' ') j++;
+            while (j < n && chars[j] != ' ') chars[i++] = chars[j++];
+            while (j < n && chars[j] == ' ') j++;
+            if (j < n) chars[i++] = ' ';
+        }
+        return new String(chars).substring(0, i);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(clearSpaces("  hello  world   "));
+    }
+}
