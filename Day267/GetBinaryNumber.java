7711ee30d0f3fc93bae93dd745ab62c12e3f52f3
geekqq <jinwei1014@gmail.com>
2023-08-10 20:39:57 -0700

    get binary number

---
+++ Day267/GetBinaryNumber.java
@@ -0,0 +1,52 @@
+package Day267;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class GetBinaryNumber {
+
+    public static String[] getBinaryNumber(int n) {
+        if (n <= 0) return null;
+        Queue<String> q = new LinkedList<>();
+        q.offer("1");
+        String[] res = new String[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = q.poll();
+            q.offer(res[i] + "0");
+            q.offer(res[i] + "1");
+        }
+        return res;
+    }
+
+    public static String getBinary(int n) {
+        if (n == 0) return "0";
+        String res = "";
+        while (n > 0) {
+            String remainder = String.valueOf(n % 2);
+            res = remainder + res;
+            n = n / 2;
+        }
+        return res;
+    }
+
+    public static String getBinaryI(int n) {
+        if (n == 0) return "0";
+        String res = "";
+        while (n > 0) {
+            int remainder = n % 2;
+            res = remainder + res;
+            n /= 2;
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            System.out.println(Arrays.toString(getBinaryNumber(i)));
+        }
+        System.out.println();
+        for (int i = 0; i < 11; i++) {
+            System.out.println(getBinaryI(i));
+        }
+    }
+}
