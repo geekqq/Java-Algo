62b2bed58d0bce66266640f0e13046491b9e3878
geekqq <jinwei1014@gmail.com>
2023-08-15 22:41:37 -0700

    get insert sort

---
+++ Day273/InsertSort.java
@@ -0,0 +1,26 @@
+package Day273;
+
+import static Day256.PrintArray.printArray;
+import static Day256.RandomArray.randomArray;
+
+public class InsertSort {
+
+    public static void insertSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            while (j >= 0 && temp < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = randomArray(10, 20);
+        printArray(arr);
+        insertSort(arr);
+        printArray(arr);
+    }
+}
