5fd3022b75b8634f64a7f6c2ed1be6a1ec0c50b0
geekqq <jinwei1014@gmail.com>
2023-08-19 11:47:27 -0700

    get 2nd max int

--- Day275/GetSecondLargest.java
+++ Day275/GetSecondLargest.java
@@ -1,5 +1,9 @@
 package Day275;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Random;
+
 import static Day256.PrintArray.printArray;
 import static Day256.RandomArray.randomArray;
 
@@ -20,9 +24,43 @@ public class GetSecondLargest {
         return secondMax;
     }
 
+    public static int[] randomArrayNoDuplicates(int size, int min, int max) {
+        if (size > max - min + 1) {
+            throw new IllegalArgumentException();
+        }
+
+        int[] arr = new int[size];
+        boolean[] used = new boolean[max - min + 1];
+        int index = 0;
+        Random rd = new Random();
+        while ( index < size) {
+            int num = rd.nextInt(max - min + 1) + min;
+            if (!used[num - min]) {
+                arr[index++] = num;
+                used[num - min] = true;
+            }
+        }
+        return arr;
+    }
+
+    public static int get2ndMax(int[] arr) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < arr.length; i++) {
+            pq.offer(arr[i]);
+        }
+        pq.poll();
+        return pq.peek();
+    }
+
     public static void main(String[] args) {
         int[] arr = randomArray(10, 18);
         printArray(arr);
         System.out.println(getSecondMax(arr));
+        System.out.println(get2ndMax(arr));
+
+        int[] newArr = randomArrayNoDuplicates(10, 5, 29);
+        printArray(newArr);
+        System.out.println(getSecondMax(newArr));
+        System.out.println(get2ndMax(newArr));
     }
 }
