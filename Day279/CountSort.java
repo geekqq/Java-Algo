bf2ae63457babdaa40a6118d547653c642f9e6b5
geekqq <jinwei1014@gmail.com>
2023-08-23 11:36:04 -0700

    get color sorted

---
+++ Day279/CountSort.java
@@ -0,0 +1,33 @@
+package Day279;
+
+import java.util.Arrays;
+
+import static Day256.PrintArray.printArray;
+import static Day256.RandomArray.randomArray;
+
+public class CountSort {
+
+    public static int[] countSort(int[] arr) {
+        int[] newArr = Arrays.copyOf(arr, arr.length);
+        int len = Arrays.stream(newArr).max().getAsInt() + 1;
+        int[] bucket = new int[len];
+        for (int i : newArr) {
+            bucket[i]++;
+        }
+        int index = 0;
+        for (int i = 0; i < len; i++) {
+            while (bucket[i] > 0) {
+                newArr[index++] = i;
+                bucket[i]--;
+            }
+        }
+        return newArr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = randomArray(10, 20);
+        printArray(arr);
+        int[] newArr = countSort(arr);
+        printArray(newArr);
+    }
+}
