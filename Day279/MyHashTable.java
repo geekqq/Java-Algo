236c205fdbef1f3e8ae31252c5a13f1d76cc0f06
geekqq <jinwei1014@gmail.com>
2023-08-22 21:11:17 -0700

    my hashtable

--- Day279/MyHashTable.java
+++ Day279/MyHashTable.java
@@ -1,10 +1,20 @@
-package Day278;
+package Day279;
 
 public class MyHashTable {
 
     private HashNode[] buckets;
     private int numOfBuckets;
     private int size;
+
+    public MyHashTable(int capacity) {
+        buckets = new HashNode[capacity];
+        numOfBuckets = capacity;
+        size = 0;
+    }
+
+    public MyHashTable() {
+        this(10);
+    }
 
     class HashNode {
         private Integer key;
@@ -14,21 +24,11 @@ public class MyHashTable {
         public HashNode(Integer key, String value) {
             this.key = key;
             this.value = value;
-            this.next = null;
         }
 
     }
-    public MyHashTable(int capacity) {
-        this.buckets = new HashNode[capacity];
-        this.numOfBuckets = capacity;
-        this.size = 0;
-    }
 
-    public MyHashTable() {
-        this(10);
-    }
-
-    public int getSize() {
+    public int size() {
         return size;
     }
 
@@ -37,11 +37,13 @@ public class MyHashTable {
     }
 
     public int getBucketIndex(int key) {
-        return key % numOfBuckets; // buckets.length;
+        return key % numOfBuckets; //buckets.length()
     }
 
     public void put(Integer key, String value) {
-        if (key == null || value == null) throw new IllegalArgumentException();
+        if (key == null || value == null) {
+            throw new IllegalArgumentException();
+        }
         int bucketIndex = getBucketIndex(key);
         HashNode head = buckets[bucketIndex];
         while (head != null) {
@@ -51,14 +53,17 @@ public class MyHashTable {
             }
             head = head.next;
         }
-        size++;
         head = buckets[bucketIndex];
         HashNode node = new HashNode(key, value);
+        size++;
         buckets[bucketIndex] = node;
         node.next = head;
     }
 
     public String get(Integer key) {
+        if (key == null) {
+            throw new IllegalArgumentException();
+        }
         int bucketIndex = getBucketIndex(key);
         HashNode head = buckets[bucketIndex];
         while (head != null) {
@@ -67,13 +72,11 @@ public class MyHashTable {
             }
             head = head.next;
         }
-        if (head == null) {
-            return "The key is not present!";
-        }
-        return null;
+        return "The key is not present!";
     }
 
     public String remove(Integer key) {
+        if (key == null) throw new IllegalArgumentException();
         int bucketIndex = getBucketIndex(key);
         HashNode head = buckets[bucketIndex];
         HashNode prev = null;
@@ -84,9 +87,7 @@ public class MyHashTable {
             prev = head;
             head = head.next;
         }
-        if (head == null) {
-            return "the key is not present!";
-        }
+        if (head == null) return "The key is not present!";
         if (prev != null) {
             prev.next = head.next;
         } else {
@@ -98,19 +99,21 @@ public class MyHashTable {
 
     public static void main(String[] args) {
         MyHashTable table = new MyHashTable();
+        table.put(12, "stone");
+        table.put(23, "jay");
+        table.put(16, "gary");
+        table.put(12, "tom");
+        table.put(16, "jin");
+        table.put(26, "hoo");
+        table.put(46, "ruin");
+        table.put(66, "tim");
+        table.put(56, "min");
         System.out.println(table.size);
-        table.put(102, "taiwan");
-        table.put(32, "hayward");
-        table.put(15, "San Jose");
-        table.put(15, "jay");
+        System.out.println(table.isEmpty());
+        System.out.println(table.get(18));
+        System.out.println(table.remove(12));
         System.out.println(table.size);
-        System.out.println(table.get(16));
-        System.out.println(table.remove(30));
-        System.out.println(table.size);
-        Integer a = 1200;
-        Integer b = 1200;
-        System.out.println(a.equals(b));
+        System.out.println(table.remove(46));
+        System.out.println(table.remove(14));
     }
 }
-
-
