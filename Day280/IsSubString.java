1048bd497a3e3494f8091ae5fd926c4a723e41d2
geekqq <jinwei1014@gmail.com>
2023-08-26 08:32:09 -0700

    check is a string is a substring of another string and return the beginning index otherwise return -1

--- Day280/IsSubString.java
+++ Day280/IsSubString.java
@@ -3,11 +3,23 @@ package Day280;
 public class IsSubString {
 
     public static int isSubString(String longStr, String shortStr) {
+        if (shortStr == null || longStr == null || longStr.length() < shortStr.length()) return -1;
         int m = longStr.length();
         int n = shortStr.length();
-        for (int i = 0; i < m - n; i++) {
-            return -1;
+        for (int i = 0; i <= m - n; i++) {
+            boolean isSubstring = true;
+            for (int j = 0; j < n; j++) {
+                if (longStr.charAt(i + j) != shortStr.charAt(j)) {
+                    isSubstring = false;
+                    break;
+                }
+            }
+            if (isSubstring) return i;
         }
         return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isSubString("lovetone", "tone"));
     }
 }
