9d809c5d924ce9226fbe5046f34157b72080fcc7
geekqq <jinwei1014@gmail.com>
2023-08-28 15:31:41 -0700

    longest substring

--- Day282/LongestSubStringWithoutDuplicates.java
+++ Day282/LongestSubStringWithoutDuplicates.java
@@ -1,6 +1,8 @@
 package Day282;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class LongestSubStringWithoutDuplicates {
@@ -38,8 +40,25 @@ public class LongestSubStringWithoutDuplicates {
         return maxLen;
     }
 
+    public static int getLongest(String s) {
+        if (s == null || s.length() == 0) throw new IllegalArgumentException();
+        int maxLen = 0;
+        int start = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int end = 0; end < s.length(); end++) {
+            char c = s.charAt(end);
+            if (map.containsKey(c)) {
+                start = Math.max(start, map.get(c) + 1);
+            }
+            map.put(c, end);
+            maxLen = Math.max(maxLen, end - start + 1);
+        }
+        return maxLen;
+    }
+
     public static void main(String[] args) {
         System.out.println(getLongestSubString("jinweihustonehoo"));
         System.out.println(getLongestSubStringI("jinweihustonehoo"));
+        System.out.println(getLongest("jinweihustonehoo"));
     }
 }
