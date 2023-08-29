ed95362225cf4d9817eeb605cf218ba000fc2e6c
geekqq <jinwei1014@gmail.com>
2023-08-29 12:06:30 -0700

    is index of

---
+++ Day283/IsIndexOf.java
@@ -0,0 +1,28 @@
+package Day283;
+
+public class IsIndexOf {
+
+    public static int isIndexOf(String longStr, String shortStr) {
+        if (longStr == null || shortStr == null) {
+            throw new IllegalArgumentException();
+        }
+        int m = longStr.length();
+        int n = shortStr.length();
+
+        for (int i = 0; i <= m - n; i++) {
+            boolean isSubstring = true;
+            for (int j = 0; j < n; j++) {
+                if (longStr.charAt(i + j) != shortStr.charAt(j)) {
+                    isSubstring = false;
+                    break;
+                }
+            }
+            if (isSubstring) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isIndexOf("stonehoo", "hoo"));
+    }
+}
