da90c4f842fdb385860832c52bca6349f69e32d1
geekqq <jinwei1014@gmail.com>
2023-08-30 11:36:26 -0700

    get the longest substring with no repeating letters

---
+++ Day284/GetLongestSubstring.java
@@ -0,0 +1,28 @@
+package Day284;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetLongestSubstring {
+
+    public static int getLongest(String s) {
+        if (s == null || s.length() == 0) return -1;
+        Map<Character, Integer> map = new HashMap<>();
+        int start = 0;
+        int maxLen = 0;
+        for (int end = 0; end < s.length() - 1; end++) {
+            char c = s.charAt(end);
+            if (map.containsKey(c)) {
+                start = Math.max(start, map.get(c) + 1);
+            } else {
+                map.put(c, end);
+                maxLen = Math.max(maxLen, end - start + 1);
+            }
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getLongest("jinweihustonehoo"));
+    }
+}
