414f65b430f3fc6ce550ab1a7b41825ee30d9674
geekqq <jinwei1014@gmail.com>
2023-08-29 22:08:14 -0700

    post order

--- Day283/TreeTraversal.java
+++ Day283/TreeTraversal.java
@@ -2,6 +2,8 @@ package Day283;
 
 import Day266.TreeNode;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Stack;
 
 public class TreeTraversal {
@@ -26,6 +28,74 @@ public class TreeTraversal {
         System.out.println();
     }
 
+    public static void inOrder(TreeNode root) {
+        if (root == null) return;
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    public static void inOrderStack(TreeNode root) {
+        if (root == null) return;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (!stack.isEmpty() || cur != null) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                cur = stack.pop();
+                System.out.print(cur.data + " ");
+                cur = cur.right;
+            }
+        }
+    }
+
+    public static void postOrder(TreeNode root) {
+        if (root == null) return;
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+
+    public static void postOrderI(TreeNode root) {
+        if (root == null) return;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (!stack.isEmpty() || cur != null) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                TreeNode temp = stack.peek().right;
+                if (temp == null) {
+                    temp = stack.pop();
+                    System.out.print(temp.data + " ");
+                    while (!stack.isEmpty() && temp == stack.peek().right) {
+                        temp = stack.pop();
+                        System.out.print(temp.data + " ");
+                    }
+                } else {
+                    cur = temp;
+                }
+            }
+        }
+    }
+    public static LinkedList<Integer> postOrderStack(TreeNode root) {
+        if (root == null) return null;
+        LinkedList<Integer> res = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.addFirst(node.data);
+            if (node.left != null) stack.push(node.left);
+            if (node.right != null) stack.push(node.right);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(5);
         root.left = new TreeNode(2);
@@ -37,5 +107,13 @@ public class TreeTraversal {
         preOrder(root);
         System.out.println();
         preOrderI(root);
+        inOrder(root);
+        System.out.println();
+        inOrderStack(root);
+        System.out.println();
+        postOrder(root);
+        System.out.println();
+        System.out.println(postOrderStack(root));
+        postOrderI(root);
     }
 }
