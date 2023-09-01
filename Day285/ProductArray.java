fc6e3622228e42ab1d87bb4b1fec057ca95b68b7
geekqq <jinwei1014@gmail.com>
2023-09-01 14:51:36 -0700

    product array

---
+++ Day285/ProductArray.java
@@ -0,0 +1,32 @@
+package Day285;
+
+import static Day256.PrintArray.printArray;
+import static Day256.RandomArray.randomArray;
+
+public class ProductArray {
+
+
+    public static int[] productArray(int[] arr) {
+        if (arr == null || arr.length == 0) return null;
+        int[] res = new int[arr.length];
+        int temp = 1;
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = temp;
+            temp *= arr[i];
+        }
+
+        temp = 1;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            res[i] *= temp;
+            temp *= arr[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = randomArray(5,6);
+        printArray(arr);
+        int[] res = productArray(arr);
+        printArray(res);
+    }
+}
