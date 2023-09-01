78b3ca2167a2234502d62846ac8ff626702b0706
geekqq <jinwei1014@gmail.com>
2023-09-01 14:45:25 -0700

    reverse integer

---
+++ Day285/ReverseInteger.java
@@ -0,0 +1,21 @@
+package Day285;
+
+public class ReverseInteger {
+
+    public static long reverseInteger(int n) {
+        boolean isNeg = n < 0;
+        if (isNeg) n = -n;
+        int rev = 0;
+        while (n > 0) {
+            int lastDig = n % 10;
+            rev = rev * 10 + lastDig;
+            n /= 10;
+        }
+        return isNeg ? rev * -1 : rev;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseInteger(56234));
+        System.out.println(reverseInteger(-56234));
+    }
+}
