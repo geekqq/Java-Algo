33bcd35b593baa56a470a7114f51900c602bb3a6
geekqq <jinwei1014@gmail.com>
2023-09-02 23:12:05 -0700

    add two numbers

--- Day280/LinkedList.java
+++ Day280/LinkedList.java
@@ -272,6 +272,29 @@ public class LinkedList {
         head = newNode;
     }
 
+    public ListNode addTwoNumbers(ListNode head1, ListNode head2) {
+        if (head1 == null) return head2;
+        if (head2 == null) return head1;
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        int carry = 0;
+        while (head1 != null || head2 != null || carry != 0) {
+            int x = head1 != null ? head1.val : 0;
+            int y = head2 != null ? head2.val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            if (head1 != null) {
+                head1 = head1.next;
+            }
+            if (head1 != null) {
+                head2 = head2.next;
+            }
+        }
+        return dummy.next;
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
         for (int i = 0; i < 11; i++) {
