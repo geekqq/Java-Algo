5aa60b6809166355f74e9fb814b4c153a891b42e
geekqq <jinwei1014@gmail.com>
2023-09-05 15:42:53 -0700

    three sum

---
+++ Day288/ThreeSum.java
@@ -0,0 +1,44 @@
+package Day288;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import static Day256.PrintArray.printArray;
+import static Day256.RandomArray.randomArray;
+
+public class ThreeSum {
+
+    public static List<List<Integer>> threeSum(int[] arr, int target) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (arr == null || arr.length == 0) return res;
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length - 2; i++) {
+            int j = i + 1;
+            int k = arr.length - 1;
+            while (j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                List<Integer> list = new LinkedList<>();
+                if (sum == target) {
+                    list.add(arr[i]);
+                    list.add(arr[j]);
+                    list.add(arr[k]);
+                    res.add(list);
+                    j++;
+                    k--;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = randomArray(10, 15);
+        printArray(arr);
+        System.out.println(threeSum(arr, 25));
+    }
+}
