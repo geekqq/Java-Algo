6e178e1552ef7f665dd6ee643ea81a44d5536fa4
geekqq <jinwei1014@gmail.com>
2023-09-06 13:21:45 -0700

    max sum window

---
+++ Day288/MaxSumWindow.java
@@ -0,0 +1,29 @@
+package Day288;
+
+import static Day256.PrintArray.printArray;
+import static Day256.RandomArray.randomArray;
+
+public class MaxSumWindow {
+
+    public static int maxSumWindow(int[] arr, int k) {
+        if (arr == null || arr.length == 0) throw new IllegalArgumentException("Invalid input!");
+        int maxSum = Integer.MIN_VALUE;
+        int maxSumWindow = 0;
+        int start = 0;
+        for (int end = 0; end < arr.length; end++) {
+            maxSumWindow += arr[end];
+            if (end >= k - 1) {
+                maxSum = Math.max(maxSum, maxSumWindow);
+                maxSumWindow -= arr[start++];
+
+            }
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = randomArray(10, 20);
+        printArray(arr);
+        System.out.println(maxSumWindow(arr, 3));
+    }
+}
