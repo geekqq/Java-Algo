ec3971dcd323ecc21602f8f2aee3aee51dc76b24
geekqq <jinwei1014@gmail.com>
2023-09-06 11:57:44 -0700

    valid sudoku

--- Day288/ValidSudoku.java
+++ Day288/ValidSudoku.java
@@ -13,14 +13,58 @@ public class ValidSudoku {
                 char cur = board[i][j];
                 if (cur == '.') continue;
                 if (!set.add(cur + " found in row " + i)
-                || !set.add(cur + " found in column " + j)
-            || !set.add(cur + " found in sub box " + i / 3 + "-" + j / 3)) return false;
+                        || !set.add(cur + " found in column " + j)
+                        || !set.add(cur + " found in sub box " + i / 3 + "-" + j / 3)) return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static boolean validSudoku(char[][] board) {
+        //cc
+
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] square = new boolean[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+                int cur = board[i][j] - '1'; // map char to index
+                if (rows[i][cur] || cols[j][cur] || square[i / 3 * 3 + j / 3][cur]) return false;
+                rows[i][cur] = true;
+                cols[j][cur] = true;
+                square[i / 3 * 3 + j / 3][cur] = true;
             }
         }
         return true;
     }
 
     public static void main(String[] args) {
+        char[][] board1 =
+                {{'5', '3', '.', '.', '7', '.', '.', '.', '.'}
+                        , {'6', '.', '.', '1', '9', '5', '.', '.', '.'}
+                        , {'.', '9', '8', '.', '.', '.', '.', '6', '.'}
+                        , {'8', '.', '.', '.', '6', '.', '.', '.', '3'}
+                        , {'4', '.', '.', '8', '.', '3', '.', '.', '1'}
+                        , {'7', '.', '.', '.', '2', '.', '.', '.', '6'}
+                        , {'.', '6', '.', '.', '.', '.', '2', '8', '.'}
+                        , {'.', '.', '.', '4', '1', '9', '.', '.', '5'}
+                        , {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+        System.out.println(isValidSudoku(board1));
+        System.out.println(validSudoku(board1));
 
+        char[][] board2 =
+                {{'8', '3', '.', '.', '7', '.', '.', '.', '.'}
+                        , {'6', '.', '.', '1', '9', '5', '.', '.', '.'}
+                        , {'.', '9', '8', '.', '.', '.', '.', '6', '.'}
+                        , {'8', '.', '.', '.', '6', '.', '.', '.', '3'}
+                        , {'4', '.', '.', '8', '.', '3', '.', '.', '1'}
+                        , {'7', '.', '.', '.', '2', '.', '.', '.', '6'}
+                        , {'.', '6', '.', '.', '.', '.', '2', '8', '.'}
+                        , {'.', '.', '.', '4', '1', '9', '.', '.', '5'}
+                        , {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+        System.out.println(validSudoku(board2));
+        System.out.println(isValidSudoku(board2));
     }
 }
