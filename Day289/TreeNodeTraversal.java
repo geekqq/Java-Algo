6c443bce0d369091711ddd3c35ef01217128be8d
geekqq <jinwei1014@gmail.com>
2023-09-06 22:04:19 -0700

    pre order

---
+++ Day289/TreeNodeTraversal.java
@@ -0,0 +1,50 @@
+package Day289;
+
+import Day266.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class TreeNodeTraversal {
+
+    public static void preOrder(TreeNode root) {
+        if (root == null) return;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            System.out.print(node.data + " ");
+            if (node.right != null) stack.push(node.right);
+            if (node.left != null) stack.push(node.left);
+        }
+    }
+    public static List<Integer> preOrderList(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+        if (root == null) return res;
+        preOrderList(root, res);
+        return res;
+    }
+
+    private static void preOrderList(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        res.add(root.data);
+        preOrderList(root.left, res);
+        preOrderList(root.right, res);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(4);
+        root.right = new TreeNode(8);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+
+        System.out.println("==== pre order ====");
+        preOrder(root);
+        System.out.println();
+        System.out.println(preOrderList(root));
+    }
+}
