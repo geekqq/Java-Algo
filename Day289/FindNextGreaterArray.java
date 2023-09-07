f5cf02236a8368cd5ecd937ed3624c9eefda0278
geekqq <jinwei1014@gmail.com>
2023-09-07 12:14:11 -0700

    get the longest substring

---
+++ Day289/FindNextGreaterArray.java
@@ -0,0 +1,36 @@
+package Day289;
+
+import java.util.Stack;
+
+import static Day256.PrintArray.printArray;
+import static Day256.RandomArray.randomArray;
+
+public class FindNextGreaterArray {
+
+    public static int[] nextGreaterElementArray(int[] arr) {
+        int[] res = new int[arr.length];
+        if (arr == null || arr.length == 0) return res;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (!stack.isEmpty()) {
+                while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                    stack.pop();
+                }
+            }
+            if (stack.isEmpty()) {
+                res[i] = -1;
+            } else {
+                res[i] = stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = randomArray(10, 15);
+        printArray(arr);
+        int[] res = nextGreaterElementArray(arr);
+        printArray(res);
+    }
+}
