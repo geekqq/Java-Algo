52405d67111fd563cd0a4a3e1143feee0f5d21de
geekqq <jinwei1014@gmail.com>
2023-09-08 11:22:50 -0700

    find median in two sorted array

--- Day290/GetLongestSubstring.java
+++ Day290/GetLongestSubstring.java
@@ -1,2 +1,63 @@
-package Day290;public class GetLongestSubstring {
+package Day290;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class GetLongestSubstring {
+
+    public static int getLongest(String s) {
+        if (s == null || s.length() == 0) return 0;
+        Map<Character, Integer> map = new HashMap<>();
+        int start = 0;
+        int maxLen = 0;
+        for (int end = 0; end < s.length(); end++) {
+            char c = s.charAt(end);
+            if (map.containsKey(c)) {
+                start = Math.max(start, map.get(c) + 1);
+            }
+            map.put(c, end);
+            maxLen = Math.max(maxLen, end - start + 1);
+        }
+        return maxLen;
+    }
+
+    public static int getLongestI(String s) {
+        //cc
+        int left = 0;
+        int right = 0;
+        int maxLen = 0;
+        Set<Character> set = new HashSet<>();
+        while (right < s.length()) {
+            if (set.add(s.charAt(right))) {
+                maxLen = Math.max(maxLen, set.size());
+                right++;
+            } else {
+                set.remove(s.charAt(left++));
+            }
+        }
+        return maxLen;
+    }
+
+    public static int getLongestII(String s) {
+        //cc
+        String test = "";
+        int maxLen = 0;
+        for (char c : s.toCharArray()) {
+            String cur = String.valueOf(c);
+            if (test.contains(cur)) {
+                test = test.substring(test.indexOf(cur) + 1);
+            }
+            test += cur;
+            maxLen = Math.max(maxLen, test.length());
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getLongest("jinweihustonehoo"));
+        System.out.println(getLongestI("jinweihustonehoo"));
+        System.out.println(getLongestII("jinweihustonehoo"));
+    }
 }
