542c0708d067944f04490c9dc9603c9621b3fc85
geekqq <jinwei1014@gmail.com>
2023-09-08 12:16:08 -0700

    get the longest consective sequence

---
+++ Day290/LongestConsecutiveSequence.java
@@ -0,0 +1,36 @@
+package Day290;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+
+    public static int getLongestSequence(int[] arr) {
+        if (arr == null || arr.length == 0) return 0;
+        Set<Integer> set = new HashSet<>();
+        for (int i : arr) {
+            set.add(i);
+        }
+        int max = 1;
+        for (int i : set) {
+            if (!set.contains(i - 1)) {
+                int cur = i;
+                int count = 0;
+                while (set.contains(cur)) {
+                    cur++;
+                    count++;
+                }
+                max = Math.max(max, count);
+            }
+        }
+        return max;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        int[] nums1 = {0,3,7,2,5,8,4,6,0,1};
+        System.out.println(getLongestSequence(nums));
+        System.out.println(getLongestSequence(nums1));
+    }
+}
