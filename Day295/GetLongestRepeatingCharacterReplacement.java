abc2a0967b30e859e9cfbcea3d731bc7c41a716a
geekqq <jinwei1014@gmail.com>
2023-09-13 22:31:08 -0700

    get the max char replacement

---
+++ Day295/GetLongestRepeatingCharacterReplacement.java
@@ -0,0 +1,37 @@
+package Day295;
+
+public class GetLongestRepeatingCharacterReplacement {
+    public static int characterReplacement(String s, int k) {
+        if (s == null || s.length() == 0) return 0;
+        int[] count = new int[26];
+        char maxChar = '\0';
+        int start = 0;
+        int max = 0;
+        for (int end = 0; end < s.length(); end++) {
+            char ch = s.charAt(end);
+            count[ch - 'A']++;
+            if (maxChar == '\0' || count[ch - 'A'] > count[maxChar - 'A']) maxChar = ch;
+
+            while ((end - start + 1) - count[maxChar - 'A'] > k) {
+                char startChar = s.charAt(start);
+                start++;
+                count[startChar - 'A']--;
+
+                if (startChar == maxChar) {
+                    for (char c = 'A'; c <= 'Z'; c++) {
+                        if (count[c - 'A'] > count[maxChar - 'A']) {
+                            maxChar = c;
+                        }
+                    }
+                }
+            }
+            max = Math.max(max, end - start + 1);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(characterReplacement("ABAB", 2));
+        System.out.println(characterReplacement("AABABBA", 1));
+    }
+}
