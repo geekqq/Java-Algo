ceef0d8a71ff58ae8a6e23de216f74d95e1e4935
geekqq <jinwei1014@gmail.com>
2023-09-15 11:44:20 -0700

    palindrome number

---
+++ Day297/PalindromeNumber.java
@@ -0,0 +1,21 @@
+package Day297;
+
+public class PalindromeNumber {
+
+    public static boolean palindromeNumber(int n) {
+        if (n < 0 || (n % 10 == 0 && n != 0)) return false;
+        int x = 0;
+        while (n > x) {
+            x = x * 10 + n % 10;
+            n /= 10;
+        }
+        return x ==n || x / 10 == n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(palindromeNumber(0));
+        System.out.println(palindromeNumber(10));
+        System.out.println(palindromeNumber(-1010));
+        System.out.println(palindromeNumber(12321));
+    }
+}
