cab8b6168a3f8f11cfdc6f33212344271c897c19
geekqq <jinwei1014@gmail.com>
2023-09-14 23:15:53 -0700

    get the max length of substring with character replacement

--- Day296/CharacterReplacement.java
+++ Day296/CharacterReplacement.java
@@ -1,5 +1,9 @@
 package Day296;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CharacterReplacement {
 
     public static int characterReplacement(String s, int k) {
@@ -30,8 +34,30 @@ public class CharacterReplacement {
         return max;
     }
 
+    public static int characterReplacementCount(String s, int k) {
+        int res = 0;
+        int l = 0;
+        int r = 0;
+        int[] count = new int[26];
+        int maxFreq = 0;
+        while (r < s.length()){
+            char c = s.charAt(r);
+            count[c -'A']++;
+            maxFreq = Math.max(maxFreq, count[c -'A']);
+            while ((r - l + 1) - maxFreq > k) {
+                char ch = s.charAt(l++);
+                count[ch - 'A']--;
+            }
+            res = Math.max(res, r - l + 1);
+            r++;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         System.out.println(characterReplacement("ABAB", 2));
         System.out.println(characterReplacement("AABABBA", 1));
+        System.out.println(characterReplacementCount("ABAB", 2));
+        System.out.println(characterReplacementCount("AABABBA", 1));
     }
 }
