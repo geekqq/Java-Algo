63608902f2fc5f2f5c18604e759a6d2e4e792562
geekqq <jinwei1014@gmail.com>
2023-09-16 18:18:23 -0700

    get my hash map

---
+++ Day298/MyHashMap.java
@@ -0,0 +1,103 @@
+package Day298;
+
+public class MyHashMap {
+
+    private HashNode[] buckets;
+    private int numOfBuckets;
+    private int size; // num of key-value pairs
+
+    class HashNode {
+        private Integer key;
+        private String value;
+        private HashNode next;
+
+        public HashNode(Integer key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public MyHashMap(int capacity) {
+        this.buckets = new HashNode[capacity];
+        this.numOfBuckets = capacity;
+        this.size = 0;
+    }
+
+    public MyHashMap() {
+        this(10);
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int getBucketIndex(Integer key) {
+        return key % numOfBuckets;
+    }
+
+    public void put(Integer key, String value) {
+        if (key == null || value == null) {
+            throw new IllegalArgumentException("Invalid input!");
+        }
+
+        int bucketIndex = getBucketIndex(key);
+        HashNode head = buckets[bucketIndex];
+        while (head != null) {
+            if (head.key.equals(key)) {
+                head.value = value;
+                return;
+            }
+            head = head.next;
+        }
+        size++;
+        head = buckets[bucketIndex];
+        HashNode node = new HashNode(key, value);
+        buckets[bucketIndex] = node;
+        node.next = head;
+    }
+
+    public String get(Integer key) {
+        if (key == null) throw new IllegalArgumentException("Invalid input!");
+        int bucketIndex = getBucketIndex(key);
+        HashNode head = buckets[bucketIndex];
+        while (head!= null && !head.key.equals(key)) {
+            head = head.next;
+        }
+        if (head == null) return "the key is not present!";
+        else return head.value;
+    }
+
+    public String remove(Integer key) {
+        if (key == null) throw new IllegalArgumentException("Invalid input!");
+        int bucketIndex = getBucketIndex(key);
+        HashNode head = buckets[bucketIndex];
+        HashNode prev = null;
+        while (head != null && !head.key.equals(key)) {
+            prev = head;
+            head = head.next;
+        }
+        if (head == null) return "The key is not present!";
+        if (prev == null) buckets[bucketIndex] = head.next; // removing the first node;
+        else {
+            prev.next = head.next;
+        }
+        size--;
+        return head.value;
+    }
+
+    public static void main(String[] args) {
+        MyHashMap map = new MyHashMap();
+        map.put(1, "stone");
+        map.put(11, "cae");
+        map.put(12, "joe");
+        map.put(23, "home");
+        map.put(104, "kate");
+        map.put(104, "jay");
+        map.put(101, "jack");
+
+        System.out.println(map.get(101));
+        System.out.println(map.size);
+        System.out.println(map.remove(12));
+        System.out.println(map.size);
+    }
+}
