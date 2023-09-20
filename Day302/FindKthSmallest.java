c82e0da9562f38b39442d09214e32a3734588d8d
geekqq <jinwei1014@gmail.com>
2023-09-20 12:03:45 -0700

    find kth smallest

---
+++ Day302/FindKthSmallest.java
@@ -0,0 +1,44 @@
+package Day302;
+
+public class FindKthSmallest {
+
+    public static int findKthSmallest(int[] arr1, int[] arr2, int k) {
+        int l1 = arr1.length;
+        int l2 = arr2.length;
+        if (l1 + l2 < k) throw new IllegalArgumentException();
+        int i = 0;
+        int j = 0;
+        int kIdx = 0;
+        while (i < l1 && j < l2) {
+            if (arr1[i] < arr2[j]) {
+                kIdx++;
+                if (kIdx == k) return arr1[i];
+                i++;
+            } else {
+                kIdx++;
+                if (kIdx == k) return arr2[j];
+                j++;
+            }
+        }
+        while (i < l1) {
+            kIdx++;
+            if (kIdx == k) return arr1[i];
+            i++;
+        }
+        while (j < l2) {
+            kIdx++;
+            if (kIdx == k) return arr2[j];
+            j++;
+        }
+        return Integer.MAX_VALUE;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {2,4,5,7,9};
+        int[] nums2 = {1,3,4,5};
+        System.out.println(findKthSmallest(nums1, nums2, 3));
+        System.out.println(findKthSmallest(nums1, nums2, 5));
+        System.out.println(findKthSmallest(nums1, nums2, 6));
+        System.out.println(findKthSmallest(nums1, nums2, 7));
+    }
+}
