acdbff093bc3f3902b4c48633a011b4913a5a513
geekqq <jinwei1014@gmail.com>
2023-09-20 13:06:55 -0700

    get the longest substring

---
+++ Day302/GetLongestSubstring.java
@@ -0,0 +1,27 @@
+package Day302;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetLongestSubstring {
+
+    public static int getLongestSubstring(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int start = 0;
+        int max = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int end = 0; end < s.length(); end++) {
+            char c = s.charAt(end);
+            if (map.containsKey(c)) {
+                start = Math.max(start, map.get(c) + 1);
+            }
+            map.put(c, end);
+            max = Math.max(max, end - start + 1);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getLongestSubstring("jinweihustonehoo"));
+    }
+}
