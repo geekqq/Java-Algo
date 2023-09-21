fe8b6b83c05de9d3127969cccd8a4bbb1c08fd63
geekqq <jinwei1014@gmail.com>
2023-09-20 21:37:08 -0700

    shuffle array

---
+++ Day302/ShuffleArray.java
@@ -0,0 +1,25 @@
+package Day302;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static Day256.PrintArray.printArray;
+import static Day65.BubbleSort.swap;
+
+public class ShuffleArray {
+
+    public static void shuffleArray(int[] arr) {
+        if (arr == null || arr.length == 0) return;
+        for (int i = arr.length - 1; i > 0; i--) {
+            Random rd = new Random();
+            int j = rd.nextInt(i);
+            swap(arr, i, j);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        shuffleArray(arr);
+        printArray(arr);
+    }
+}
