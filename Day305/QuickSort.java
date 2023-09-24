13d10927cff10ab56c58af1f969e78f593dd1fe9
geekqq <jinwei1014@gmail.com>
2023-09-23 21:37:29 -0700

    quick sort

---
+++ Day305/QuickSort.java
@@ -0,0 +1,35 @@
+package Day305;
+
+import static Day256.PrintArray.printArray;
+import static Day256.RandomArray.randomArray;
+import static Day65.BubbleSort.swap;
+
+public class QuickSort {
+
+    public static int partition(int[] arr, int low, int high) {
+        int i = low;
+        int j = low;
+        int pivot = arr[high];
+        while (i <= high) {
+            if (arr[i] <= pivot) {
+                swap(arr, i++, j++);
+            } else i++;
+        }
+        return j - 1;
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = randomArray(10, 20);
+        printArray(arr);
+        quickSort(arr, 0, arr.length - 1);
+        printArray(arr);
+    }
+}
