9f5843f2ce4ab87d55c126b1ee50181f9a77b0de
geekqq <jinwei1014@gmail.com>
2023-09-23 21:59:57 -0700

    permutation substring

---
+++ Day305/PermutationString.java
@@ -0,0 +1,31 @@
+package Day305;
+
+import java.util.Arrays;
+
+public class PermutationString {
+
+    public static boolean permutationString(String s1, String s2) {
+        if (s1 == null || s2 == null || s1.length() > s2.length()) return false;
+        int l1 = s1.length();
+        int l2 = s2.length();
+        int[] countS1 = new int[26];
+        int[] countS2 = new int[26];
+        for (int i = 0; i < l1; i++) {
+            countS1[s1.charAt(i) - 'a']++;
+            countS2[s2.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i <= l2 - l1; i++) {
+            if (i != 0) {
+                countS2[s2.charAt(i + l1 - 1) - 'a']++;
+                countS2[s2.charAt(i - 1) - 'a']--;
+            }
+            if (Arrays.equals(countS1, countS2)) return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(permutationString("ab", "eibaooo"));
+        System.out.println(permutationString("ab", "eiboaooo"));
+    }
+}
