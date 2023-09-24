317db52f2e0e4c45248a093d075c984f9bee82bc
geekqq <jinwei1014@gmail.com>
2023-09-23 17:15:02 -0700

    max water container

---
+++ Day305/MaxWaterContainer.java
@@ -0,0 +1,23 @@
+package Day305;
+
+public class MaxWaterContainer {
+
+    public static int waterContainer(int[] height) {
+        if (height == null || height.length == 0) return 0;
+        int l = 0;
+        int r = height.length - 1;
+        int res = 0;
+        while (l < r) {
+            res = height[l] < height[r] ? Math.max(res, (r - l) * height[l++]) : Math.max(res, (r- l) * height[r--]);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        int[] height2 = {1,1};
+        System.out.println(waterContainer(height2));
+        System.out.println(waterContainer(height));
+    }
+}
