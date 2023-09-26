d74b9dcaf5b7de0c1f9f07a9d230775d3f641008
geekqq <jinwei1014@gmail.com>
2023-09-26 11:52:08 -0700

    next warmer day

---
+++ Day308/DailyTemperatures.java
@@ -0,0 +1,27 @@
+package Day308;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class DailyTemperatures {
+
+    public static int[] nextWarmerDay(int[] temperatures) {
+        if (temperatures == null || temperatures.length == 0) return null;
+        Stack<Integer> stack = new Stack<>();
+        int[] res = new int[temperatures.length];
+        for (int i = temperatures.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]) {
+                stack.pop();
+            }
+            res[i] = stack.isEmpty() ? 0 : stack.peek() - i;
+            stack.push(i);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] temperatures = {73,74,75,71,69,72,76,73};
+        System.out.println(Arrays.toString(nextWarmerDay(temperatures)));
+    }
+}
