00fdaaa815251bcbcf0e8d7e6f14e16f55f41a75
geekqq <jinwei1014@gmail.com>
2023-09-26 12:04:19 -0700

    permutation substring

--- Day308/PermutationSubstring.java
+++ Day308/PermutationSubstring.java
@@ -1,8 +1,6 @@
-package Day306;
+package Day308;
 
 import java.util.Arrays;
-
-import static Day286.ValidAnagram.isAnagram;
 
 public class PermutationSubstring {
 
@@ -16,11 +14,9 @@ public class PermutationSubstring {
             countS1[s1.charAt(i) - 'a']++;
             countS2[s2.charAt(i) - 'a']++;
         }
-        for (int i = 0; i <= l2 - l1; i++) {
-            if (i != 0) {
-                countS2[s2.charAt(i + l1 - 1) - 'a']++;
-                countS2[s2.charAt(i - 1) - 'a']--;
-            }
+        for (int i = 1; i <= l2 - l1; i++) {
+            countS2[s2.charAt(i + l1 - 1) - 'a']++;
+            countS2[s2.charAt(i - 1) - 'a']--;
             if (Arrays.equals(countS1, countS2)) return true;
         }
         return false;
@@ -31,10 +27,25 @@ public class PermutationSubstring {
         int l1 = s1.length();
         int l2 = s2.length();
         for (int i = 0; i <= l2 - l1; i++) {
-            String sub = s2.substring(i, i + l1);
-            if (isAnagram(s1, sub)) return true;
+            String subStr = s2.substring(i, i + l1);
+            if (isAnagram(s1, subStr)) return true;
         }
         return false;
+    }
+
+    public static boolean isAnagram(String s1, String s2) {
+        if (s1 == null || s2 == null || s1.length() != s2.length()) return false;
+        int[] count = new int[26];
+        for (char c : s1.toCharArray()) {
+            count[c - 'a']++;
+        }
+        for (char c : s2.toCharArray()) {
+            count[c - 'a']--;
+        }
+        for (int i : count) {
+            if (i != 0) return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
@@ -42,5 +53,6 @@ public class PermutationSubstring {
         System.out.println(permutationSubstring("ab", "eiboaooo"));
         System.out.println(permutationSubstringI("ab", "eibaooo"));
         System.out.println(permutationSubstringI("ab", "eiboaooo"));
+
     }
 }
