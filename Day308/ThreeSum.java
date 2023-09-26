938b1741fedd450d956bc3c6025b3808f9a79659
geekqq <jinwei1014@gmail.com>
2023-09-26 15:45:32 -0700

    valid sudoku:

---
+++ Day308/ThreeSum.java
@@ -0,0 +1,33 @@
+package Day308;
+
+import java.util.*;
+
+import static Day256.PrintArray.printArray;
+import static Day256.RandomArray.randomArray;
+
+public class ThreeSum {
+
+    public static List<List<Integer>> threeSum(int[] arr, int target) {
+        if (arr == null || arr.length == 0) return null;
+        Set<List<Integer>> res = new HashSet<>();
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length - 2; i++) {
+            int j = i + 1;
+            int k = arr.length - 1;
+            while (j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (sum == target) {
+                    res.add(Arrays.asList(arr[i], arr[j++], arr[k--]));
+                } else if (sum < target) j++;
+                else  k--;
+            }
+        }
+        return new LinkedList<>(res);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = randomArray(10, 15);
+        printArray(arr);
+        System.out.println(threeSum(arr, 20));
+    }
+}
