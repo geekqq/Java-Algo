119935f7198f0b5643e47df28fd44c4a2441ab5a
geekqq <jinwei1014@gmail.com>
2023-09-27 13:45:36 -0700

    back tracking problems

---
+++ Day307/BackTrack.java
@@ -0,0 +1,26 @@
+package Day307;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class BackTrack {
+
+    public static List<String> generateParentheses(int n) {
+        List<String> res = new LinkedList<>();
+        backTracking(res, "", 0, 0, n);
+        return res;
+    }
+
+    private static void backTracking(List<String> res, String cur, int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            res.add(cur);
+            return;
+        }
+        if (open < max) backTracking(res, cur + "(", open + 1, close, max);
+        if (close < open) backTracking(res, cur + ")", open, close + 1, max);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generateParentheses(4));
+    }
+}
