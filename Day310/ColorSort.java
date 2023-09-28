fbf1bde350a8d65e124a328f651d5e7b6555b8d3
geekqq <jinwei1014@gmail.com>
2023-09-28 12:02:26 -0700

    color sort

---
+++ Day310/ColorSort.java
@@ -0,0 +1,27 @@
+package Day310;
+
+import static Day256.PrintArray.printArray;
+import static Day256.RandomArray.randomArray;
+import static Day37.SortedArray.swap;
+
+public class ColorSort {
+    public static void colorSort(int[] color) {
+        if (color == null || color.length == 0) return;
+        int i = 0;
+        int j = 0;
+        int k = color.length - 1;
+        while (i <= k) {
+            if (color[i] == 0) {
+                swap(color, i++, j++);
+            } else if (color[i] == 1) i++;
+            else if (color[i] == 2) swap(color, i, k--);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = randomArray(10, 2);
+        printArray(arr);
+        colorSort(arr);
+        printArray(arr);
+    }
+}
