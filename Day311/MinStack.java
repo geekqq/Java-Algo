f2c94dc14d9d9485588dbf1eec7fde77ae574194
geekqq <jinwei1014@gmail.com>
2023-09-29 20:46:19 -0700

    get min stack implementation

---
+++ Day311/MinStack.java
@@ -0,0 +1,46 @@
+package Day311;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class MinStack {
+    private Stack<Integer> stackStr;
+    private Stack<Integer> stackMin;
+
+    public MinStack() {
+        stackStr = new Stack<>();
+        stackMin = new Stack<>();
+    }
+    public void push(int val) {
+        stackStr.push(val);
+        if (stackMin.isEmpty() || stackMin.peek() > val) stackMin.push(val);
+        else stackMin.push(stackMin.peek());
+    }
+
+    public int top() {
+        if (stackStr.isEmpty()) throw new EmptyStackException();
+        return stackStr.peek();
+    }
+
+    public void pop() {
+        if (stackStr.isEmpty()) throw new EmptyStackException();
+        stackStr.pop();
+        stackMin.pop();
+    }
+
+    public int getMin() {
+        if (stackStr.isEmpty()) throw new EmptyStackException();
+        return stackMin.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.top());
+        System.out.println(minStack.getMin());
+    }
+}
