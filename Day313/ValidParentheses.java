5798c5a220c5508f50e93629f86a1f39469bce57
geekqq <jinwei1014@gmail.com>
2023-10-02 22:02:22 -0700

    valid parentheses

---
+++ Day313/ValidParentheses.java
@@ -0,0 +1,31 @@
+package Day313;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean validParentheses(String s) {
+        if (s == null || s.length() == 0) return false;
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') stack.push(c);
+            else {
+                if (stack.isEmpty()) return false;
+                else {
+                    char top = stack.peek();
+                    if (top == '(' && c == ')'
+                    || top == '[' && c == ']'
+                    || top == '{' && c == '}') {
+                        stack.pop();
+                    } else stack.push(c);
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(validParentheses("()()[]{}"));
+        System.out.println(validParentheses("(){[}"));
+    }
+}
