1a629fb023e3d1b1190e765e3db860ab539f3baa
geekqq <jinwei1014@gmail.com>
2023-10-03 12:21:41 -0700

    max window sum in k

---
+++ Day313/MaxWindowSum.java
@@ -0,0 +1,28 @@
+package Day313;
+
+import static Day256.PrintArray.printArray;
+import static Day256.RandomArray.randomArray;
+
+public class MaxWindowSum {
+
+    public static int maxWindowSum(int[] arr, int k) {
+        if (arr == null || arr.length == 0) throw new IllegalArgumentException();
+        int max = Integer.MIN_VALUE;
+        int start = 0;
+        int maxWindow = 0;
+        for (int end = 0; end < arr.length; end++) {
+            maxWindow += arr[end];
+            if (end >= k - 1) {
+                max = Math.max(max, maxWindow);
+                maxWindow -= arr[start++];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = randomArray(10, 20);
+        printArray(arr);
+        System.out.println(maxWindowSum(arr, 3));
+    }
+}
