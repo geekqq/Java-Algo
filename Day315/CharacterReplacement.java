2b63d72bd4fde2a723c25c1ce7aa4f53f7b36380
geekqq <jinwei1014@gmail.com>
2023-10-04 14:54:13 -0700

    character replacement

---
+++ Day315/CharacterReplacement.java
@@ -0,0 +1,27 @@
+package Day315;
+
+public class CharacterReplacement {
+
+    public static int characterReplacement(String s, int k) {
+        if (s == null || s.length() == 0) throw new IllegalArgumentException();
+        int l = 0;
+        int r = 0;
+        int[] count = new int[26];
+        int maxFreq = 0;
+        int res = 0;
+        while (r < s.length()) {
+            maxFreq = Math.max(maxFreq, ++count[s.charAt(r) - 'A']);
+            while (r - l + 1 - maxFreq > k) {
+                count[s.charAt(l++) - 'A']--;
+            }
+            res = Math.max(res, r - l + 1);
+            r++;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(characterReplacement("ABAB", 2));
+        System.out.println(characterReplacement("AABBABA", 2));
+    }
+}
