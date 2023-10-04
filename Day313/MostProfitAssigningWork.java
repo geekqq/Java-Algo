795b5337b3d2f30308e9127c42c766a53125ab09
geekqq <jinwei1014@gmail.com>
2023-10-04 14:50:58 -0700

    most profit assiging work

--- Day313/MostProfitAssigningWork.java
+++ Day313/MostProfitAssigningWork.java
@@ -1,17 +1,17 @@
-package Day314;
+package Day313;
 
 import java.util.Arrays;
 
-public class LC826MostProfitAssigningWork {
-    public static int maxProfit(int[] difficulty, int[] profit, int[] worker) {
+public class MostProfitAssigningWork {
+
+    public static int mostProfitAssigningWork(int[] difficulty, int[] profit, int[] worker) {
         int len = difficulty.length;
         int[][] pairs = new int[len][2];
         for (int i = 0; i < len; i++) {
             pairs[i][0] = difficulty[i];
             pairs[i][1] = profit[i];
         }
-
-        Arrays.sort(pairs, (a, b) -> a[0] - b[0]);
+        Arrays.sort(pairs, (a, b) -> (a[0] - b[0]));
         Arrays.sort(worker);
 
         int maxProfit = 0;
@@ -33,7 +33,7 @@ public class LC826MostProfitAssigningWork {
         int[] difficulty1 = {85, 47, 57};
         int[] profit1 = {24, 66, 99};
         int[] worker1 = {40, 25, 25};
-        System.out.println(maxProfit(difficulty, profit, worker));
-        System.out.println(maxProfit(difficulty1, profit1, worker1));
+        System.out.println(mostProfitAssigningWork(difficulty, profit, worker));
+        System.out.println(mostProfitAssigningWork(difficulty1, profit1, worker1));
     }
 }
