aa6172d26db13dc5aff61e9ea3622d4cf5998376
geekqq <jinwei1014@gmail.com>
2023-10-04 21:28:27 -0700

    is index of

---
+++ Day315/IsIndexOf.java
@@ -0,0 +1,27 @@
+package Day315;
+
+public class IsIndexOf {
+
+    public static int indexOf(String s1, String s2) {
+        if (s1 == null || s2 == null) return -1;
+        if (s1.length() < s2.length()) return indexOf(s2, s1);
+        int m = s1.length();
+        int n = s2.length();
+        for (int i = 0; i <= m - n; i++) {
+            boolean isSubstring = true;
+            for (int j = 0; j < n; j++) {
+                if (s1.charAt(i + j) != s2.charAt(j)) {
+                    isSubstring = false;
+                    break;
+                }
+            }
+            if (isSubstring) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(indexOf("stonehoo", "hoo"));
+        System.out.println(indexOf("hoo", "stonehoo"));
+    }
+}
