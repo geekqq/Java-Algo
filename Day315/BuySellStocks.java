d23841adf3e6f05e02411530642450ed86ab1e05
geekqq <jinwei1014@gmail.com>
2023-10-04 21:15:45 -0700

    buy and sell stocks

---
+++ Day315/BuySellStocks.java
@@ -0,0 +1,28 @@
+package Day315;
+
+public class BuySellStocks {
+
+    public static int maxProfit(int[] prices) {
+        if (
+                prices == null || prices.length == 0
+        ) throw new IllegalArgumentException();
+        int buy = 0;
+        int sell = 1;
+        int max = 0;
+        while (sell < prices.length) {
+            if (prices[buy] < prices[sell]) {
+                int profit = prices[sell] - prices[buy];
+                max = Math.max(max, profit);
+            } else {
+                buy = sell;
+            }
+            sell++;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+}
