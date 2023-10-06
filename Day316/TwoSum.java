003fd977745b7da42770c325cf500490b67b3fab
geekqq <jinwei1014@gmail.com>
2023-10-06 15:44:55 -0700

    get two sum

---
+++ Day316/TwoSum.java
@@ -0,0 +1,55 @@
+package Day316;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] arr, int target) {
+        if (arr == null || arr.length == 0) return null;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) return new int[]{i, j};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static int[] twoSumI(int[] arr, int target) {
+        //cc
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            int diff = target - arr[i];
+            if (map.containsKey(diff)) {
+                return new int[]{i, map.get(diff)};
+            }
+            map.put(arr[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static int[] twoSumII(int[] arr, int target) {
+        //cc
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[] {-1, -1};
+        for (int i = 0; i < arr.length; i++) {
+            int diff = target - arr[i];
+            if (!map.containsKey(diff)) {
+                map.put(arr[i], i);
+            } else {
+                res[0] = i;
+                res[1] = map.get(diff);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 5, 3, 8};
+        int target = 14;
+        System.out.println(Arrays.toString(twoSum(arr, target)));
+        System.out.println(Arrays.toString(twoSumI(arr, target)));
+        System.out.println(Arrays.toString(twoSumII(arr, target)));
+    }
+}
