a2359c1ab4da05c7a380906ddddb752688845209
geekqq <jinwei1014@gmail.com>
2023-10-06 11:52:57 -0700

    longest commong prefix in words

--- Day316/LongestCommonPrefix.java
+++ Day316/LongestCommonPrefix.java
@@ -1,23 +1,20 @@
-package Day140;
+package Day316;
 
 public class LongestCommonPrefix {
-    public static void main(String[] args) {
-        String[] strs = {"leetcode", "leets", "leeds", "leekcity"};
-        System.out.println(lcp(strs));
-    }
 
-    private static String lcp(String[] strs) {
+    public static String longestPrefix(String[] strs) {
         if (strs == null || strs.length == 0) return null;
         String prefix = strs[0];
         for (int i = 1; i < strs.length; i++) {
             while (strs[i].indexOf(prefix) != 0) {
                 prefix = prefix.substring(0, prefix.length() - 1);
-
-            }
-            if (prefix.isEmpty()) {
-                return null;
             }
         }
-        return prefix;
+        return prefix.isEmpty() ? null : prefix;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"leet", "leetcode", "lee"};
+        System.out.println(longestPrefix(strs));
     }
 }
