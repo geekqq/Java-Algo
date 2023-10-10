7f5a7fe90d4715ceacc618bb20d736f210cd61a5
geekqq <jinwei1014@gmail.com>
2023-10-10 14:45:14 -0700

    character replacement

---
+++ Day320/CharacterReplacement.java
@@ -0,0 +1,26 @@
+package Day320;
+
+public class CharacterReplacement {
+
+    public static int characterReplacement(String s, int k) {
+        if (s == null || s.length() == 0) return 0;
+        int l = 0;
+        int maxFreq = 0;
+        int res =0 ;
+        int[] count = new int[26];
+        for (int r = 0; r < s.length(); r++) {
+            maxFreq = Math.max(maxFreq, ++count[s.charAt(r) - 'A']);
+            while (r - l + 1 - maxFreq > k) {
+                count[s.charAt(l++) - 'A']--;
+            }
+            res = Math.max(res, r - l + 1);
+            r++;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(characterReplacement("ABAB", 2));
+        System.out.println(characterReplacement("AABBABA", 2));
+    }
+}
