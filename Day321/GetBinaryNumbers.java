48c9dc449c470a4f767b8256492663950c702431
geekqq <jinwei1014@gmail.com>
2023-10-12 13:49:09 -0700

    get the bianry numbers to String List

--- Day321/GetBinaryNumbers.java
+++ Day321/GetBinaryNumbers.java
@@ -1,8 +1,6 @@
 package Day321;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class GetBinaryNumbers {
     public static String[] binaryNumbers(int n) {
@@ -18,7 +16,21 @@ public class GetBinaryNumbers {
         return res;
     }
 
+    public static List<String> binaryNumberStringList(int n) {
+        List<String> res = new ArrayList<>();
+        Queue<String> q = new LinkedList<>();
+        q.offer("1");
+        for (int i = 0; i < n; i++) {
+            String cur = q.poll();
+            res.add(cur);
+            q.offer(cur + "0");
+            q.offer(cur + "1");
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         System.out.println(Arrays.toString(binaryNumbers(8)));
+        System.out.println(binaryNumberStringList(8));
     }
 }
