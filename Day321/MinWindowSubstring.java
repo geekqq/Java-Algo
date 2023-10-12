7ee0028b070434cd7e677898dda9de9407f1f141
geekqq <jinwei1014@gmail.com>
2023-10-11 22:38:44 -0700

    car fleet

--- Day321/MinWindowSubstring.java
+++ Day321/MinWindowSubstring.java
@@ -1,7 +1,6 @@
-package Day320;
+package Day321;
 
 public class MinWindowSubstring {
-
     public static String minWindowSubstring(String s1, String s2) {
         if (s1 == null || s2 == null) return "";
         int[] freq = new int[128];
@@ -10,8 +9,8 @@ public class MinWindowSubstring {
         }
         int l = 0;
         int start = 0;
-        int minLen = Integer.MAX_VALUE;
         int total = s2.length();
+        int minLen = Integer.MAX_VALUE;
         for (int r = 0; r < s1.length(); r++) {
             if (freq[s1.charAt(r)]-- > 0) total--;
             while (total == 0) {
@@ -24,6 +23,7 @@ public class MinWindowSubstring {
             }
         }
         return minLen == Integer.MAX_VALUE ? "" : s1.substring(start, start + minLen);
+
     }
 
     public static void main(String[] args) {
