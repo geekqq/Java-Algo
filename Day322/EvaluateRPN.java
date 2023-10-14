0c8b80e49ed2a7b5e7b5cb58469686121a44fdf6
geekqq <jinwei1014@gmail.com>
2023-10-14 16:51:25 -0700

    evaluate reverse polish notation

---
+++ Day322/EvaluateRPN.java
@@ -0,0 +1,35 @@
+package Day322;
+
+
+import java.util.Stack;
+
+public class EvaluateRPN {
+
+    public static int evalRPN(String[] tokens) {
+        if (tokens == null || tokens.length == 0) throw new IllegalArgumentException();
+        Stack<Integer> stack = new Stack();
+        for (String t : tokens) {
+            if (t.equals("+")) stack.push(stack.pop() + stack.pop());
+            else if (t.equals("*")) stack.push(stack.pop() * stack.pop());
+            else if (t.equals("-")) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b - a);
+            } else if (t.equals("/")) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b / a);
+            } else {
+                stack.push(Integer.parseInt(t));
+            }
+        }
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        String[] tokens = {"2","1","+","3","*"};
+        String[] tokens1 = {"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
+        System.out.println(evalRPN(tokens));
+        System.out.println(evalRPN(tokens1));
+    }
+}
