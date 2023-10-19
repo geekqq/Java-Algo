a57770bc7426a28ad5abe9e6ef458789b04a4a29
geekqq <jinwei1014@gmail.com>
2023-10-19 10:57:14 -0700

    count sort

---
+++ Day324/CountSort.java
@@ -0,0 +1,32 @@
+package Day324;
+
+import java.util.Arrays;
+
+import static Day256.PrintArray.printArray;
+import static Day256.RandomArray.randomArray;
+
+public class CountSort {
+    public static int[] countSort(int[] arr) {
+        int[] newArr = Arrays.copyOf(arr, arr.length);
+        int len = Arrays.stream(newArr).max().getAsInt() + 1;
+        int[] bucket = new int[len];
+        for (int i : newArr) {
+            bucket[i]++;
+        }
+        int index = 0;
+        for (int i = 0; i < len; i++) {
+            while (bucket[i] > 0) {
+                newArr[index++] = i;
+                bucket[i]--;
+            }
+        }
+        return newArr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = randomArray(10, 20);
+        printArray(arr);
+        int[] res = countSort(arr);
+        printArray(res);
+    }
+}
