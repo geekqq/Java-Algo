f78634d1cdff9013d722da4a88440820f2fd668e
geekqq <jinwei1014@gmail.com>
2023-10-19 11:01:09 -0700

    get the longest consecutive sequence

---
+++ Day324/LongestConsecutiveSequence.java
@@ -0,0 +1,41 @@
+package Day324;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static Day256.PrintArray.printArray;
+import static Day256.RandomArray.randomArray;
+
+public class LongestConsecutiveSequence {
+
+    public static int getLongestSequence(int[] arr) {
+        if (arr == null || arr.length == 0) return 0;
+        Set<Integer> set = new HashSet<>();
+        for (int i : arr) {
+            set.add(i);
+        }
+        int max = 1;
+        for (int i : set) {
+            if (!set.contains(i - 1)) {
+                int count = 0;
+                int cur = i;
+                while (set.contains(cur)) {
+                    count++;
+                    cur = cur+ 1;
+                }
+                max = Math.max(max, count);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = randomArray(10, 20);
+        printArray(arr);
+        System.out.println(getLongestSequence(arr));
+        int[] nums = {100, 4, 3, 7, 2};
+        int[] nums1 = {0,3,7,2,5,8,4,6,0,10};
+        System.out.println(getLongestSequence(nums));
+        System.out.println(getLongestSequence(nums1));
+    }
+}
