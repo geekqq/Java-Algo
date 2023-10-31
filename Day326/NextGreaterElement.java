f5f2c1247c847be76833336536c672acaaa5b8b3
geekqq <jinwei1014@gmail.com>
2023-10-31 11:06:16 -0700

    next greater element array

---
+++ Day326/NextGreaterElement.java
@@ -0,0 +1,30 @@
+package Day326;
+
+import java.util.Stack;
+
+import static Day256.PrintArray.printArray;
+import static Day256.RandomArray.randomArray;
+
+public class NextGreaterElement {
+
+    public static int[] nextGreater(int[] arr) {
+        if (arr == null || arr.length == 0) return null;
+        int[] res = new int[arr.length];
+        Stack<Integer> stack = new Stack();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            res[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(arr[i]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = randomArray(10, 20);
+        printArray(arr);
+        int[] res = nextGreater(arr);
+        printArray(res);
+    }
+}
