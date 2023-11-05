8a1aeece3f107b411412cf2d787b6ff828154ae4
geekqq <jinwei1014@gmail.com>
2023-11-05 15:05:59 -0800

    merge sort

---
+++ Day327/MergeSort.java
@@ -0,0 +1,44 @@
+package Day327;
+
+import static Day256.PrintArray.printArray;
+import static Day256.RandomArray.randomArray;
+
+public class MergeSort {
+
+    public static void mergeSort(int[] arr, int[] temp, int low, int high) {
+        if (low < high) {
+            int mid = low + (high - low) / 2;
+            mergeSort(arr, temp, low, mid);
+            mergeSort(arr, temp, mid + 1, high);
+            merge(arr, temp, low, mid, high);
+        }
+    }
+    private static void merge(int[] arr, int[] temp, int low, int mid, int high) {
+        for (int i = low; i <= high; i++) {
+            temp[i] = arr[i];
+        }
+        int i = low;
+        int j = mid + 1;
+        int k = low;
+        while (i <= mid && j <= high) {
+            if (temp[i] < temp[j]) {
+                arr[k++] = temp[i++];
+            } else {
+                arr[k++] = temp[j++];
+            }
+        }
+        while (i <= mid) {
+            arr[k++] = temp[i++];
+        }
+        while (j <= high) {
+            arr[k++] = temp[j++];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = randomArray(10, 20);
+        printArray(arr);
+        mergeSort(arr, new int[arr.length],0, arr.length - 1);
+        printArray(arr);
+    }
+}
