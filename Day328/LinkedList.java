f30222ff47976ba32e2a06f0ecec3497abb75627
geekqq <jinwei1014@gmail.com>
2023-12-03 10:17:02 -0800

    review new linked list

---
+++ Day328/LinkedList.java
@@ -0,0 +1,89 @@
+package Day328;
+
+import Day266.ListNode;
+
+public class LinkedList {
+    private ListNode head;
+
+    public void add(int val) {
+        ListNode node = new ListNode(val);
+        if (head == null) head = node;
+        else {
+            ListNode cur = head;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = node;
+        }
+    }
+
+    public void print(ListNode head) {
+        if(head == null) return;
+        ListNode cur = head;
+        while (cur != null) {
+            System.out.print(cur.data + "--");
+            cur = cur.next;
+        }
+        System.out.println("null");
+    }
+
+    public ListNode reverse(ListNode head) {
+        if (head == null) return null;
+        ListNode prev = null;
+        ListNode next = null;
+        ListNode cur = head;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+
+    public ListNode reverseRecursion(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode rest = reverseRecursion(head.next);
+        head.next.next = head;
+        head.next = null;
+        return rest;
+    }
+
+    public ListNode reverseByRange(ListNode head, int left, int right) {
+        if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode prev = dummy;
+        for (int i = 1; i < left; i++) {
+            prev = prev.next;
+        }
+        ListNode subStart = prev.next;
+        prev.next = null;
+        ListNode subEnd = subStart;
+        for (int i = left; i < right; i++) {
+            subEnd = subEnd.next;
+        }
+        ListNode next = subEnd.next;
+        subEnd.next = null;
+        prev.next = reverseRecursion(subStart);
+        subStart.next = next;
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        for (int i = 0; i < 15; i++) {
+            list.add(i);
+        }
+        list.print(list.head);
+        System.out.println("====reverse list====");
+        list.head = list.reverse(list.head);
+        list.print(list.head);
+        list.head = list.reverseRecursion(list.head);
+        list.print(list.head);
+        System.out.println("====reverse by range====");
+        list.head = list.reverseByRange(list.head, 5, 10);
+        list.print(list.head);
+    }
+
+}
