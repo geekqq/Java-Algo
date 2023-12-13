82130b14a95ff0cc330a4893403891c7b6b3e8a1
geekqq <jinwei1014@gmail.com>
2023-12-13 14:08:08 -0800

    get binary number, return string array or list of string

--- Day328/GetBinaryNumber.java
+++ Day328/GetBinaryNumber.java
@@ -1,8 +1,7 @@
 package Day328;
 
-import java.util.Arrays;
+import java.util.*;
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class GetBinaryNumber {
 
@@ -19,7 +18,20 @@ public class GetBinaryNumber {
         return res;
     }
 
+    public static List<String> getBinList(int n) {
+        if (n <= 0) return null;
+        List<String> list = new ArrayList<>();
+        Queue<String> q = new LinkedList<>();
+        q.offer("1");
+        for (int i = 0; i < n; i++) {
+            list.add(q.poll());
+            q.offer(list.get(i) + "0");
+            q.offer(list.get(i) + "1");
+        }
+        return list;
+    }
     public static void main(String[] args) {
         System.out.println(Arrays.toString(getBin(5)));
+        System.out.println(getBinList(5));
     }
 }
