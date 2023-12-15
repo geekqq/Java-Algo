22e128644a83cfaa77b67e89d4089b121a74e923
geekqq <jinwei1014@gmail.com>
2023-12-15 13:26:38 -0800

    get the random array generated with out duplicates

--- Day328/GenerateRandomArray.java
+++ Day328/GenerateRandomArray.java
@@ -11,16 +11,53 @@ public class GenerateRandomArray {
         int[] res = new int[size];
         Set<Integer> set = new HashSet<>();
         Random rand = new Random();
-        for (int i = 0; i < size; i++) {
-            int num = rand.nextInt(max - min) + 1;
+        int index = 0;
+        while (index < size){
+            int num = rand.nextInt(max - min + 1) + min;
             if (set.add(num)) {
-                res[i] = num;
+                res[index++] = num;
             }
         }
         return res;
     }
 
+    public static int[] generateArray(int min, int max, int size) {
+        if (size <= 0 || max - min < size) throw new IllegalArgumentException("Invalid input!");
+        int[] res = new int[size];
+        boolean[] used = new boolean[max - min + 1];
+        Random rand = new Random();
+        int index = 0;
+        while (index < size){
+            int num = rand.nextInt(max - min + 1) + min;
+            if (!used[num - min]) {
+                res[index++] = num;
+                used[num - min] = true;
+            }
+        }
+        return res;
+    }
+
+    public static int[] generateArrayI(int size, int min, int max) {
+        if (max - min + 1 < size) {
+            throw new IllegalArgumentException();
+        }
+        int[] arr = new int[size];
+        boolean[] used = new boolean[max - min + 1];
+        Arrays.fill(used, true);
+        Random rd = new Random();
+        int index = 0;
+        while (index < size) {
+            int num = rd.nextInt(max - min + 1) + min;
+            if (used[num - min]) {
+                arr[index++] = num;
+                used[num - min] = false;
+            }
+        }
+        return arr;
+    }
     public static void main(String[] args) {
         System.out.println(Arrays.toString(generateRandomArray(5, 25, 10)));
+        System.out.println(Arrays.toString(generateArray(5,25,10)));
+        System.out.println(Arrays.toString(generateArrayI(5,10,25)));
     }
 }
