121f96edf02b28f5e34001ec40dc0fb4c2e8ff5e
geekqq <jinwei1014@gmail.com>
2023-12-21 21:46:50 -0800

    is this valid parenthese

---
+++ Day328/ValidParentheses.java
@@ -0,0 +1,32 @@
+package Day328;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean isValidParentheses(String s) {
+        if (s == null || s.length() == 0) return false;
+        Stack<Character> stack = new Stack<>();
+        for (Character c : s.toCharArray() ){
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    char top = stack.peek();
+                    if (top == '(' && c == ')'
+                    || top == '[' && c == ']'
+                    || top == '{' && c == '}') stack.pop();
+                    else stack.push(c);
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValidParentheses("()[]{}"));
+        System.out.println(isValidParentheses("()[]{)}"));
+    }
+}
